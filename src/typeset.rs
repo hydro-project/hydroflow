bbb7b1f69b2d136a686380185d2515e41febeb52
Mingwei Samuel <mingwei.samuel@gmail.com>
2021-09-20 13:14:34 -0700

    Typeset wip

--- src/typeset.rs
+++ src/typeset.rs
@@ -19,8 +19,9 @@ trait Exclude<Item> {
     type Output;
 }
 
+
 impl<ItemToRemove, Item, Next: Exclude<ItemToRemove>> Exclude<ItemToRemove> for (Item, Next) {
-    default type Output = &'static str; //(Item, <Next as Exclude<ItemToRemove>>::Output);
+    default type Output = (Item, <Next as Exclude<ItemToRemove>>::Output);
 }
 impl<ItemToRemove, Item, Next: Exclude<ItemToRemove>> Exclude<ItemToRemove> for (Item, Next)
 where
@@ -39,7 +40,7 @@ type XYZ = <ZZZ as Exclude<i16>>::Output;
 fn _test() {
     let z: <() as Exclude<i16>>::Output = ();
     let z: <(i16, ()) as Exclude<i16>>::Output = ();
-    let z: <(u16, ()) as Exclude<i16>>::Output = "hello"; //(0_u16, ()); //(0_u16, ());
+    let z: <(u16, ()) as Exclude<i16>>::Output = (0_u16, ());
     let xyz: XYZ = (0_u8, (0_u16, (0_u32, ())));
 }
 
@@ -76,3 +77,28 @@ fn _test() {
 
 
 // type
+
+
+/*
+[gen box]->  Ord set  --[remove prop box]->  Unord set  --[sort box]->  Ord set
+
+
+Simplest case, very easy:
+- List<Int> -> Set<Int> -> List<Int>
+Next, easy:
+- List<T> -> Set<T> -> List<T>
+
+Numbering, odd, even:
+- List<Int> -> List<(Int [odd], Int [even])> -> List<Int>
+  - Still ordered
+  - Still dense
+  - Still ordered lattice of prefixes (but only of even length!)
+
+TODO: find smallest collection of examples that isn't generic.
+
+
+
+Hard expressive version:
+Current: <InputProps as Exclude<OrderProp>>::Output
+Wrappers: ExcludeWrapper<OrderProp, InputProps>
+*/
