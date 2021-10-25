561a418d41782cdd6e2423310c9420fbc74b0d36
Justin Jaffray <justin.jaffray@gmail.com>
2021-10-25 13:44:54 -0700

    working layout for compiled layer

--- hydroflow/src/compiled/mod.rs
+++ hydroflow/src/compiled/mod.rs
@@ -1,10 +1,6 @@
 use std::marker::PhantomData;
 
-// trait Iterator {
-//     type Item;
-//     fn next(&mut self) -> Option<Self::Item>;
-// }
-trait Pusherator: Sized {
+pub trait Pusherator: Sized {
     type Item;
     fn give(&mut self, item: Self::Item);
 
@@ -16,13 +12,40 @@ trait Pusherator: Sized {
     }
 }
 
-// trait Botherator {
-//     type In = ();
-//     type Out = ();
-//     fn give(&mut self, item: Self::In) -> Option<Self::Out>;
-// }
+pub struct Pivot<T, I, P>
+where
+    I: Iterator<Item = T>,
+    P: Pusherator<Item = T>,
+{
+    pull: I,
+    push: P,
+}
+impl<T, I, P> Pivot<T, I, P>
+where
+    I: Iterator<Item = T>,
+    P: Pusherator<Item = T>,
+{
+    pub fn new(pull: I, push: P) -> Self {
+        Self { pull, push }
+    }
 
-struct ForEach<T, F>
+    pub fn step(&mut self) -> bool {
+        if let Some(v) = self.pull.next() {
+            self.push.give(v);
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn run(mut self) {
+        while let Some(v) = self.pull.next() {
+            self.push.give(v);
+        }
+    }
+}
+
+pub struct ForEach<T, F>
 where
     F: FnMut(T),
 {
@@ -42,7 +65,7 @@ impl<T, F> ForEach<T, F>
 where
     F: FnMut(T),
 {
-    fn new(f: F) -> Self {
+    pub fn new(f: F) -> Self {
         ForEach {
             f,
             _marker: PhantomData,
@@ -50,7 +73,7 @@ where
     }
 }
 
-struct Map<T, U, F, O>
+pub struct Map<T, U, F, O>
 where
     F: Fn(T) -> U,
     O: Pusherator<Item = U>,
@@ -74,7 +97,7 @@ where
     F: Fn(T) -> U,
     O: Pusherator<Item = U>,
 {
-    fn new(f: F, out: O) -> Self {
+    pub fn new(f: F, out: O) -> Self {
         Map {
             out,
             f,
@@ -83,7 +106,7 @@ where
     }
 }
 
-struct Filter<T, F, O>
+pub struct Filter<T, F, O>
 where
     F: Fn(&T) -> bool,
     O: Pusherator<Item = T>,
@@ -109,7 +132,7 @@ where
     F: Fn(&T) -> bool,
     O: Pusherator<Item = T>,
 {
-    fn new(f: F, out: O) -> Self {
+    pub fn new(f: F, out: O) -> Self {
         Filter {
             out,
             f,
@@ -118,7 +141,7 @@ where
     }
 }
 
-struct Partition<T, F, O1, O2>
+pub struct Partition<T, F, O1, O2>
 where
     F: Fn(&T) -> bool,
     O1: Pusherator<Item = T>,
@@ -150,7 +173,7 @@ where
     O1: Pusherator<Item = T>,
     O2: Pusherator<Item = T>,
 {
-    fn new(f: F, out1: O1, out2: O2) -> Self {
+    pub fn new(f: F, out1: O1, out2: O2) -> Self {
         Partition {
             out1,
             out2,
@@ -160,7 +183,7 @@ where
     }
 }
 
-struct Tee<T, O1, O2>
+pub struct Tee<T, O1, O2>
 where
     T: Clone,
     O1: Pusherator<Item = T>,
@@ -188,7 +211,7 @@ where
     O1: Pusherator<Item = T>,
     O2: Pusherator<Item = T>,
 {
-    fn new(out1: O1, out2: O2) -> Self {
+    pub fn new(out1: O1, out2: O2) -> Self {
         Tee {
             out1,
             out2,
@@ -197,89 +220,102 @@ where
     }
 }
 
-// struct RefTee<T, O1, O2>
-// where
-//     T: Clone,
-//     O1: Pusherator<Item = &T>,
-//     O2: Pusherator<Item = T>,
-// {
-//     out1: O1,
-//     out2: O2,
-//     _marker: PhantomData<T>,
-// }
-// impl<T, O1, O2> Pusherator for RefTee<T, O1, O2>
-// where
-//     T: Clone,
-//     O1: Pusherator<Item = &T>,
-//     O2: Pusherator<Item = T>,
-// {
-//     type Item = T;
-//     fn give(&mut self, item: Self::Item) {
-//         self.out1.give(&item);
-//         self.out2.give(item);
-//     }
-// }
-// impl<T, O1, O2> RefTee<T, O1, O2>
-// where
-//     T: Clone,
-//     O1: Pusherator<Item = T>,
-//     O2: Pusherator<Item = T>,
-// {
-//     fn new(out1: O1, out2: O2) -> Self {
-//         RefTee {
-//             out1,
-//             out2,
-//             _marker: PhantomData,
-//         }
-//     }
-// }
+#[cfg(test)]
+mod tests {
+    use std::rc::Rc;
 
-#[test]
-fn linear_chains() {
-    let mut v = Vec::new();
-    let mut pusher = Map::new(
-        |x| x * 2,
-        Filter::new(|x| *x > 5, ForEach::new(|x| v.push(x))),
-    );
+    use crate::compiled::{Filter, ForEach, Map, Partition, Pivot, Pusherator, Tee};
 
-    for i in 0..5 {
-        pusher.give(i);
+    #[test]
+    fn linear_chains() {
+        let mut v = Vec::new();
+        let mut pusher = Map::new(
+            |x| x * 2,
+            Filter::new(|x| *x > 5, ForEach::new(|x| v.push(x))),
+        );
+
+        for i in 0..5 {
+            pusher.give(i);
+        }
+
+        assert_eq!(v, vec![6, 8]);
     }
 
-    assert_eq!(v, vec![6, 8]);
-}
+    #[test]
+    fn partition() {
+        let mut evens = Vec::new();
+        let mut odds = Vec::new();
+        let mut pusher = Partition::new(
+            |x| x % 2 == 0,
+            ForEach::new(|x| evens.push(x)),
+            ForEach::new(|x| odds.push(x)),
+        );
 
-#[test]
-fn partition() {
-    let mut evens = Vec::new();
-    let mut odds = Vec::new();
-    let mut pusher = Partition::new(
-        |x| x % 2 == 0,
-        ForEach::new(|x| evens.push(x)),
-        ForEach::new(|x| odds.push(x)),
-    );
+        for i in 0..5 {
+            pusher.give(i);
+        }
 
-    for i in 0..5 {
-        pusher.give(i);
+        assert_eq!(evens, vec![0, 2, 4]);
+        assert_eq!(odds, vec![1, 3]);
     }
 
-    assert_eq!(evens, vec![0, 2, 4]);
-    assert_eq!(odds, vec![1, 3]);
-}
+    #[test]
+    fn tee() {
+        let mut left = Vec::new();
+        let mut right = Vec::new();
+        let mut pusher = Tee::new(
+            ForEach::new(|x| left.push(x)),
+            ForEach::new(|x| right.push(x)),
+        );
 
-#[test]
-fn tee() {
-    let mut left = Vec::new();
-    let mut right = Vec::new();
-    let mut pusher = Tee::new(
-        ForEach::new(|x| left.push(x)),
-        ForEach::new(|x| right.push(x)),
-    );
+        for i in 0..5 {
+            pusher.give(i);
+        }
 
-    for i in 0..5 {
-        pusher.give(i);
+        assert_eq!(left, vec![0, 1, 2, 3, 4]);
+        assert_eq!(right, vec![0, 1, 2, 3, 4]);
     }
 
-    assert_eq!(left, vec![0, 1, 2, 3, 4]);
-    assert_eq!(right, vec![0, 1, 2, 3, 4]);
+    #[test]
+    fn tee_rcs() {
+        let mut left = Vec::new();
+        let mut right = Vec::new();
+        let mut pusher = Map::new(
+            |x| Rc::new(x),
+            Tee::new(
+                ForEach::new(|x: Rc<i32>| left.push(*x)),
+                ForEach::new(|x: Rc<i32>| right.push(*x)),
+            ),
+        );
+
+        for i in 0..5 {
+            pusher.give(i);
+        }
+
+        assert_eq!(left, vec![0, 1, 2, 3, 4]);
+        assert_eq!(right, vec![0, 1, 2, 3, 4]);
+    }
+
+    #[test]
+    fn pivot() {
+        let a = 0..10;
+        let b = 10..20;
+
+        let mut left = Vec::new();
+        let mut right = Vec::new();
+
+        let pivot = Pivot::new(
+            a.into_iter().chain(b.into_iter()),
+            Partition::new(
+                |x| x % 2 == 0,
+                ForEach::new(|x| left.push(x)),
+                ForEach::new(|x| right.push(x)),
+            ),
+        );
+
+        pivot.run();
+
+        assert_eq!(left, vec![0, 2, 4, 6, 8, 10, 12, 14, 16, 18]);
+        assert_eq!(right, vec![1, 3, 5, 7, 9, 11, 13, 15, 17, 19]);
+    }
 }
