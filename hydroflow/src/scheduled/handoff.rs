6a47e919bba73e101dea8d3c6f2756edac21d62c
Mingwei Samuel <mingwei.samuel@gmail.com>
2021-11-01 12:06:53 -0700

    Cleanup old comments

--- hydroflow/src/scheduled/handoff.rs
+++ hydroflow/src/scheduled/handoff.rs
@@ -83,20 +83,6 @@ impl<T> CanReceive<VecDeque<T>> for VecHandoff<T> {
     }
 }
 
-// /**
-//  * A trait specifying a handoff point between compiled subgraphs.
-//  */
-// pub trait Handoff {
-//     type Item;
-
-//     fn new() -> Self;
-
-//     #[allow(clippy::result_unit_err)]
-//     fn try_give(&mut self, item: Self::Item) -> Result<(), ()>;
-
-//     fn is_bottom(&self) -> bool;
-// }
-
 /**
  * A handle onto the metadata part of a [Handoff], with no element type.
  */
@@ -105,53 +91,11 @@ pub trait HandoffMeta {
     fn is_bottom(&self) -> bool;
 }
 
-// /**
-//  * A null handoff which will panic when called.
-//  *
-//  * This is used in sources and sinks as the unused read or write handoff respectively.
-//  */
-// pub struct NullHandoff;
-// impl Handoff for NullHandoff {
-//     type Item = ();
-
-//     fn new() -> Self {
-//         NullHandoff
-//     }
-
-//     fn try_give(&mut self, _item: Self::Item) -> Result<(), ()> {
-//         panic!("Tried to write to null handoff.");
-//     }
-
-//     fn is_bottom(&self) -> bool {
-//         true
-//     }
-// }
 impl HandoffMeta for NullHandoff {
     fn is_bottom(&self) -> bool {
         true
     }
 }
-
-// /**
-//  * A [VecDeque]-based FIFO handoff.
-//  */
-// pub struct VecHandoff<T>(pub(crate) VecDeque<T>);
-// impl<T> Handoff for VecHandoff<T> {
-//     type Item = T;
-
-//     fn new() -> Self {
-//         VecHandoff(VecDeque::new())
-//     }
-
-//     fn try_give(&mut self, t: Self::Item) -> Result<(), ()> {
-//         self.0.push_back(t);
-//         Ok(())
-//     }
-
-//     fn is_bottom(&self) -> bool {
-//         self.0.is_empty()
-//     }
-// }
 
 impl<T> HandoffMeta for VecHandoff<T> {
     fn is_bottom(&self) -> bool {
