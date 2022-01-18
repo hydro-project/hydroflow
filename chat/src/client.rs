f569072d948cb69366353c2ba4f2637689725ba8
Joe Hellerstein <jmh@berkeley.edu>
2022-01-18 10:43:03 -0800

    msgs sent to server

--- chat/src/client.rs
+++ chat/src/client.rs
@@ -1,18 +1,43 @@
-use crate::{Decode, Encode, Opts};
-
-use std::time::Duration;
-
-use hydroflow::compiled::{pull::SymmetricHashJoin, IteratorToPusherator, PusheratorBuild};
-use hydroflow::lang::collections::Iter;
-use hydroflow::scheduled::ctx::{RecvCtx, SendCtx};
+use crate::{Encode,Opts};
+use hydroflow::builder::prelude::*;
 use hydroflow::scheduled::{handoff::VecHandoff, net::Message};
-use hydroflow::tokio::net::TcpListener;
-use hydroflow::{
-    scheduled::{graph::Hydroflow, graph_ext::GraphExt},
-    tl, tt,
-};
-use rand::Rng;
+use hydroflow::tokio::net::{TcpStream, TcpListener};
 
 pub(crate) async fn run_client(opts: Opts) {
-    let mut df = Hydroflow::new();
+    let mut df = HydroflowBuilder::default();
+
+    let stream = TcpStream::connect(format!("localhost:{}", opts.port))
+        .await
+        .unwrap();
+    let network_out = df.add_write_tcp_stream(stream);
+
+    let (text_in, text_out) = df.add_channel_input::<Option<_>, VecHandoff<(String, String)>>();
+    hydroflow::tokio::spawn(async move {
+        // TODO(mingwei): temp, need to integrate stream into surface API.
+        use hydroflow::tokio::io::AsyncBufReadExt;
+
+        let mut reader = hydroflow::tokio::io::BufReader::new(hydroflow::tokio::io::stdin());
+        let mut buf = String::new();
+        while let Ok(_num_chars) = reader.read_line(&mut buf).await {
+            text_in.give(Some(("localhost:1111".to_owned(), buf.clone())));
+            text_in.flush();
+            buf.clear(); // TODO(mingwei): Maybe not needed?
+        }
+    });
+
+    let sg = text_out
+        .map(|vec_deque| {
+            let mut buf = Vec::new();
+            vec_deque.encode(&mut buf);
+            Some(Message {
+                address: 0,
+                batch: buf.into(),
+            })
+        })
+        .pivot()
+        .reverse(network_out);
+    df.add_subgraph(sg);
+
+    let mut df = df.build();
+    df.run_async().await.unwrap();
 }
--- chat/src/server.rs
+++ chat/src/server.rs
@@ -1,54 +1,49 @@
-use crate::{Decode, Encode, Opts};
+use crate::{Decode, Opts};
 
-use hydroflow::lang::collections::Iter;
-use hydroflow::scheduled::{ctx::RecvCtx, graph::Hydroflow, handoff::VecHandoff, net::Message};
+use hydroflow::scheduled::{handoff::VecHandoff};
 use hydroflow::builder::prelude::*;
-use hydroflow::tokio::net::TcpStream;
-use hydroflow::{
-    compiled::{pull::SymmetricHashJoin, InputBuild, IteratorToPusherator, PusheratorBuild},
-    scheduled::graph_ext::GraphExt,
-    tl, tt,
-};
+use hydroflow::tokio::net::TcpListener;
+
 
 pub(crate) async fn run_server(opts: Opts) {
+    // let stream = TcpListener::bind(format!("localhost:{}", opts.port))
+    // .await
+    // .unwrap();
+    // let (stream, _) = stream.accept().await.unwrap();
+
     let mut hf = HydroflowBuilder::default();
 
-    // let members:Vec<(String, String)>; // (id, nickname) pair
-    // let msgs:Vec:String; 
+    let stream = TcpListener::bind(format!("localhost:{}", opts.port))
+        .await
+        .unwrap();
+    let (stream, _) = stream.accept().await.unwrap();
 
+    // inbound msgs stuff
+    let msgs_out = hf.add_read_tcp_stream(stream);
+    
     let (members_in, members_out) = hf.add_channel_input::<Option<_>, VecHandoff<(String, String)>>();
-    let (msgs, msgs_out) = hf.add_channel_input::<Option<_>, VecHandoff<(String, String)>>();
-
-    // let members_out = members_out.flat_map(std::convert::identity).map(|x| ((), x));
-    // let msgs_out = msgs_out.flat_map(std::convert::identity).map(|x| ((), x));
-
-    // let sg = members_out.join(msgs_out).pivot().for_each(|x| {
-    //    println!("{:?}", x); 
-    // });
+    // let (msgs, msgs_out) = hf.add_channel_input::<Option<_>, VecHandoff<(String, String)>>();
 
     let members_out = members_out.flat_map(std::convert::identity);
-    let msgs_out = msgs_out.flat_map(std::convert::identity);
+    let msgs_out = 
+        msgs_out
+        .flat_map(std::convert::identity)
+        .flat_map(|message| <Vec<(String, String)>>::decode(message.batch).into_iter());
+    // let msgs_out = msgs_out.flat_map(std::convert::identity);
 
-    let sg = members_out.cross_join(msgs_out).pivot().for_each(|x| {
+    let sg = members_out.cross_join(msgs_out)
+                                                      .pivot()
+                                                      .for_each(|x| {
        println!("{:?}", x); 
     });
 
     hf.add_subgraph(sg);
 
-    {
-        let mut hf = hf.build();
+    let mut hf = hf.build();
 
-        members_in.give(Some(("localhost:1234".to_string(), "Joe".to_string())));
-        members_in.give(Some(("localhost:1235".to_string(), "Mingwei".to_string())));
-        members_in.flush();
-        hf.tick();
+    members_in.give(Some(("localhost:1234".to_string(), "Joe".to_string())));
+    members_in.give(Some(("localhost:1235".to_string(), "Mingwei".to_string())));
+    members_in.flush();
 
-        msgs.give(Some(("localhost:1234".to_string(), "Do you hear me?".to_string())));
-        msgs.flush();
-        hf.tick();
-
-        msgs.give(Some(("localhost:1235".to_string(), "Ack!".to_string())));
-        msgs.flush();
-        hf.tick();
-    }
+    hf.run_async().await.unwrap();
 }
