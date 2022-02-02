2d31d7efd0e9594f9fe8f7e04efab138c334bfe5
Mingwei Samuel <mingwei.samuel@gmail.com>
2022-02-02 12:57:16 -0800

    Re-add add_(channel_)input methods

--- hydroflow/src/scheduled/graph_ext.rs
+++ hydroflow/src/scheduled/graph_ext.rs
@@ -1,4 +1,5 @@
 use core::task;
+use std::sync::mpsc::SyncSender;
 use std::{pin::Pin, task::Poll};
 
 use futures::Stream;
@@ -6,7 +7,9 @@ use futures::Stream;
 use super::context::Context;
 use super::graph::Hydroflow;
 use super::handoff::{CanReceive, Handoff};
+use super::input::Input;
 use super::port::{InputPort, OutputPort, RecvCtx, SendCtx};
+use super::SubgraphId;
 
 macro_rules! subgraph_ext {
     (
@@ -16,6 +19,7 @@ macro_rules! subgraph_ext {
     ) => {
         fn $fn_name <F, $($recv_generic,)* $($send_generic),*>
             (&mut self, $($recv_param : OutputPort< $recv_generic >,)* $($send_param : InputPort< $send_generic >,)* subgraph: F)
+            -> SubgraphId
         where
             F: 'static + FnMut(&Context<'_>, $(&RecvCtx< $recv_generic >,)* $(&SendCtx< $send_generic >),*),
             $($recv_generic : 'static + Handoff,)*
@@ -29,6 +33,7 @@ macro_rules! subgraph_ext {
     ) => {
         fn $fn_name <F, $($recv_generic,)* $($send_generic),*>
             (&mut self, $($recv_param : OutputPort< $recv_generic >,)* $($send_param : InputPort< $send_generic >,)* subgraph: F)
+            -> SubgraphId
         where
             F: 'static + FnMut(&Context<'_>, $(&RecvCtx< $recv_generic >,)* $(&SendCtx< $send_generic >),*),
             $($recv_generic : 'static + Handoff,)*
@@ -40,7 +45,7 @@ macro_rules! subgraph_ext {
                 crate::tl!($($send_param),*),
                 move |ctx, crate::tl!($($recv_param),*), crate::tl!($($send_param),*)|
                     (subgraph)(ctx, $($recv_param,)* $($send_param),*),
-            );
+            )
         }
     };
 }
@@ -67,6 +72,18 @@ pub trait GraphExt {
         (recv_port_1: R1, recv_port_2: R2),
         (send_port_1: W1, send_port_2: W2)
     );
+
+    fn add_channel_input<T, W>(&mut self) -> (Input<T, SyncSender<T>>, OutputPort<W>)
+    where
+        T: 'static,
+        W: 'static + Handoff + CanReceive<T>;
+
+    /// Adds an "input" operator, returning a handle to insert data into it.
+    /// TODO(justin): make this thing work better
+    fn add_input<T, W>(&mut self) -> (Input<T, super::input::Buffer<T>>, OutputPort<W>)
+    where
+        T: 'static,
+        W: 'static + Handoff + CanReceive<T>;
 
     fn add_input_from_stream<T, W, S>(&mut self, send_port: InputPort<W>, stream: S)
     where
@@ -95,6 +112,39 @@ impl GraphExt for Hydroflow {
         (recv_port_1: R1, recv_port_2: R2),
         (send_port_1: W1, send_port_2: W2)
     );
+
+    fn add_channel_input<T, W>(&mut self) -> (Input<T, SyncSender<T>>, OutputPort<W>)
+    where
+        T: 'static,
+        W: 'static + Handoff + CanReceive<T>,
+    {
+        use std::sync::mpsc;
+
+        let (sender, receiver) = mpsc::sync_channel(8000);
+        let (send_port, recv_port) = self.make_handoff();
+        let sg_id = self.add_subgraph_source::<_, W>(send_port, move |_ctx, send| {
+            for x in receiver.try_iter() {
+                send.give(x);
+            }
+        });
+        (Input::new(self.reactor(), sg_id, sender), recv_port)
+    }
+
+    fn add_input<T, W>(&mut self) -> (Input<T, super::input::Buffer<T>>, OutputPort<W>)
+    where
+        T: 'static,
+        W: 'static + Handoff + CanReceive<T>,
+    {
+        let input = super::input::Buffer::default();
+        let inner_input = input.clone();
+        let (send_port, recv_port) = self.make_handoff::<W>();
+        let sg_id = self.add_subgraph_source::<_, W>(send_port, move |_ctx, send| {
+            for x in (*inner_input.0).borrow_mut().drain(..) {
+                send.give(x);
+            }
+        });
+        (Input::new(self.reactor(), sg_id, input), recv_port)
+    }
 
     fn add_input_from_stream<T, W, S>(&mut self, send_port: InputPort<W>, stream: S)
     where
