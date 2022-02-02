4fb3d05026526db029ca0f520f8448e92c4fe189
Mingwei Samuel <mingwei.samuel@gmail.com>
2022-02-02 14:19:11 -0800

    Update covid_tracing_dist to new core API

--- covid_tracing_dist/src/database.rs
+++ covid_tracing_dist/src/database.rs
@@ -4,12 +4,11 @@ use std::time::Duration;
 
 use hydroflow::compiled::{pull::SymmetricHashJoin, IteratorToPusherator, PusheratorBuild};
 use hydroflow::lang::collections::Iter;
-use hydroflow::scheduled::port::{RecvCtx, SendCtx};
 use hydroflow::scheduled::{handoff::VecHandoff, net::Message};
 use hydroflow::tokio::net::TcpListener;
 use hydroflow::{
     scheduled::{graph::Hydroflow, graph_ext::GraphExt},
-    tl, tt,
+    tl,
 };
 use rand::Rng;
 
@@ -18,9 +17,16 @@ pub(crate) async fn run_database(opts: Opts) {
 
     let mut df = Hydroflow::new();
 
-    let (contacts_in, contacts_out) = df.add_channel_input();
-    let (diagnoses_in, diagnoses_out) = df.add_channel_input();
-    let (people_in, people_out) = df.add_channel_input();
+    let (notifs, notif_sink) = df.make_handoff::<VecHandoff<(String, usize)>>();
+    let (encode_contacts_out, contacts_merge) = df.make_handoff::<VecHandoff<Message>>();
+    let (encode_diagnoses_out, diagnoses_merge) = df.make_handoff::<VecHandoff<Message>>();
+
+    let (contacts_in, contacts_out) =
+        df.add_channel_input::<_, VecHandoff<(&'static str, &'static str, usize)>>();
+    let (diagnoses_in, diagnoses_out) =
+        df.add_channel_input::<_, VecHandoff<(&'static str, (usize, usize))>>();
+    let (people_in, people_out) =
+        df.add_channel_input::<_, VecHandoff<(String, (String, String))>>();
 
     let stream = TcpListener::bind(format!("localhost:{}", opts.port))
         .await
@@ -29,16 +35,13 @@ pub(crate) async fn run_database(opts: Opts) {
     let (stream, _) = stream.accept().await.unwrap();
     let (network_in, network_out) = df.add_tcp_stream(stream);
 
-    let (encoded_notifs_in, notifs) =
-        df.add_inout(|_ctx, recv: &RecvCtx<VecHandoff<Message>>, send| {
-            for message in recv.take_inner().into_iter() {
-                send.give(Iter(
-                    <Vec<(String, usize)>>::decode(message.batch).into_iter(),
-                ));
-            }
-        });
-
-    df.add_edge(network_out, encoded_notifs_in);
+    df.add_subgraph_in_out(network_out, notifs, |_ctx, recv, send| {
+        for message in recv.take_inner().into_iter() {
+            send.give(Iter(
+                <Vec<(String, usize)>>::decode(message.batch).into_iter(),
+            ));
+        }
+    });
 
     std::thread::spawn(move || {
         let mut t = 0;
@@ -75,56 +78,40 @@ pub(crate) async fn run_database(opts: Opts) {
         }
     });
 
-    let (mut ins, mut out) = df.add_n_in_m_out(
-        2,
-        1,
-        |recvs: &[&RecvCtx<VecHandoff<Message>>], sends: &[&SendCtx<VecHandoff<_>>]| {
-            for recv in recvs {
-                sends[0].give(recv.take_inner());
-            }
+    df.add_subgraph_2in_out(
+        contacts_merge,
+        diagnoses_merge,
+        network_in,
+        |_ctx, recv1, recv2, send| {
+            send.give(Iter(
+                recv1.take_inner().into_iter().chain(recv2.take_inner()),
+            ));
         },
     );
 
-    df.add_edge(out.pop().unwrap(), network_in);
+    df.add_subgraph_in_out(contacts_out, encode_contacts_out, |_ctx, recv, send| {
+        let mut buf = Vec::new();
+        recv.take_inner().encode(&mut buf);
+        send.give(Some(Message {
+            address: CONTACTS_ADDR,
+            batch: buf.into(),
+        }));
+    });
 
-    let (contacts_merge, diagnoses_merge) = (ins.pop().unwrap(), ins.pop().unwrap());
-
-    let (encode_contacts_in, encode_contacts_out) = df.add_inout(
-        |_ctx, recv: &RecvCtx<VecHandoff<(&'static str, &'static str, usize)>>, send| {
-            let mut buf = Vec::new();
-            recv.take_inner().encode(&mut buf);
-            send.give(Some(Message {
-                address: CONTACTS_ADDR,
-                batch: buf.into(),
-            }));
-        },
-    );
-
-    df.add_edge(contacts_out, encode_contacts_in);
-    df.add_edge(encode_contacts_out, contacts_merge);
-
-    let (encode_diagnoses_in, encode_diagnoses_out) = df.add_inout(
-        |_ctx, recv: &RecvCtx<VecHandoff<(&'static str, (usize, usize))>>, send| {
-            let mut buf = Vec::new();
-            recv.take_inner().encode(&mut buf);
-            send.give(Some(Message {
-                address: DIAGNOSES_ADDR,
-                batch: buf.into(),
-            }));
-        },
-    );
-
-    df.add_edge(diagnoses_out, encode_diagnoses_in);
-    df.add_edge(encode_diagnoses_out, diagnoses_merge);
-
-    type SubgraphIn = tt!(
-        VecHandoff::<(String, usize)>,
-        VecHandoff::<(String, (String, String))>,
-    );
+    df.add_subgraph_in_out(diagnoses_out, encode_diagnoses_out, |_ctx, recv, send| {
+        let mut buf = Vec::new();
+        recv.take_inner().encode(&mut buf);
+        send.give(Some(Message {
+            address: DIAGNOSES_ADDR,
+            batch: buf.into(),
+        }));
+    });
 
     let mut join_state = Default::default();
-    let (tl!(notif_sink, people_sink), tl!()) =
-        df.add_subgraph::<_, SubgraphIn, ()>(move |_ctx, tl!(notifs, people), tl!()| {
+    df.add_subgraph(
+        tl!(notif_sink, people_out),
+        tl!(),
+        move |_ctx, tl!(notifs, people), tl!()| {
             let pivot = SymmetricHashJoin::new(
                 notifs.take_inner().into_iter(),
                 people.take_inner().into_iter(),
@@ -135,10 +122,8 @@ pub(crate) async fn run_database(opts: Opts) {
             .for_each(|(name, phone, t)| println!("notifying {}, {}@{}", name, phone, t));
 
             pivot.run();
-        });
-
-    df.add_edge(notifs, notif_sink);
-    df.add_edge(people_out, people_sink);
+        },
+    );
 
     df.run_async().await.unwrap();
 }
