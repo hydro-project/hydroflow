af3e990605bb88ecad25ab86ba1b48c1f93047ff
Mingwei Samuel <mingwei.samuel@gmail.com>
2022-02-09 15:40:12 -0800

    Simplify surface pull_iter generics

--- hydroflow/src/builder/build/pull_iter.rs
+++ hydroflow/src/builder/build/pull_iter.rs
@@ -2,32 +2,32 @@ use super::{PullBuild, PullBuildBase};
 
 use crate::scheduled::{context::Context, handoff::handoff_list::PortList, port::RECV};
 
-pub struct IterPullBuild<I, T>
+pub struct IterPullBuild<I>
 where
-    I: Iterator<Item = T>,
+    I: Iterator,
 {
     it: I,
 }
-impl<I, T> IterPullBuild<I, T>
+impl<I> IterPullBuild<I>
 where
-    I: Iterator<Item = T>,
+    I: Iterator,
 {
     pub fn new(it: I) -> Self {
         Self { it }
     }
 }
 
-impl<I, T> PullBuildBase for IterPullBuild<I, T>
+impl<I> PullBuildBase for IterPullBuild<I>
 where
-    I: 'static + Iterator<Item = T>,
+    I: 'static + Iterator,
 {
-    type ItemOut = T;
+    type ItemOut = I::Item;
     type Build<'slf, 'hof> = &'slf mut I;
 }
 
-impl<I, T> PullBuild for IterPullBuild<I, T>
+impl<I> PullBuild for IterPullBuild<I>
 where
-    I: 'static + Iterator<Item = T>,
+    I: 'static + Iterator,
 {
     type InputHandoffs = ();
 
