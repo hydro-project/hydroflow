a4d94a4bad2483eb11bbe86bc9e7c05f93a30bc8
Justin Jaffray <justin.jaffray@gmail.com>
2022-03-03 12:06:44 -0800

    Rewrite KVS in dataflow (#91)
    * add stream join operator
    
    * Add KVS in dataflow
    
    This is a first pass at a dataflow based KVS. It is missing a bunch of clock
    stuff, but I was getting anxious about not merging this so here's an early
    version of it.
    
    The structure of the program is roughly as follows:
    
    ```mermaid
    graph BT
        Y[Read Requests] --> Z[Streaming Join]
        X[Received Batches] --> Z
        Z --> Q[Respond to Read Request]
    
        F[Write Requests] --> B
        A[Epoch Timer] --> B[Batcher / Merger]
        B --> C[Join]
        G[Ownership] --> C
        C --> E[Network Shuffle]
    ```

--- hydroflow/src/compiled/pull.rs
+++ hydroflow/src/compiled/pull.rs
@@ -11,6 +11,82 @@ where
 {
     state: L::Repr,
 }
+pub struct HalfHashJoinState<K, L>
+where
+    L: LatticeRepr,
+{
+    tab: HashMap<K, L::Repr>,
+}
+
+impl<K, L> Default for HalfHashJoinState<K, L>
+where
+    L: LatticeRepr,
+{
+    fn default() -> Self {
+        Self {
+            tab: HashMap::new(),
+        }
+    }
+}
+
+pub struct HalfHashJoin<'a, K, Buf, L, Update, Stream, StreamV>
+where
+    K: Eq + std::hash::Hash,
+    L: LatticeRepr + Merge<Update>,
+    Update: LatticeRepr,
+    Buf: Iterator<Item = (K, Update::Repr)>,
+    Stream: Iterator<Item = (K, StreamV)>,
+{
+    buf: Buf,
+    stream: Stream,
+    state: &'a mut HalfHashJoinState<K, L>,
+    _marker: PhantomData<Update>,
+}
+
+impl<'a, K, Buf, L, Update, Stream, StreamV> Iterator
+    for HalfHashJoin<'a, K, Buf, L, Update, Stream, StreamV>
+where
+    K: Eq + std::hash::Hash + Clone,
+    L: LatticeRepr + Merge<Update>,
+    L::Repr: Default,
+    Update: LatticeRepr,
+    StreamV: Clone,
+    Buf: Iterator<Item = (K, Update::Repr)>,
+    Stream: Iterator<Item = (K, StreamV)>,
+{
+    type Item = (K, StreamV, L::Repr);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        for (k, v) in &mut self.buf {
+            <L as Merge<Update>>::merge(self.state.tab.entry(k).or_default(), v);
+        }
+
+        for (k, v) in &mut self.stream {
+            if let Some(vals) = self.state.tab.get(&k) {
+                return Some((k, v, vals.clone()));
+            }
+        }
+        None
+    }
+}
+impl<'a, K, Buf, L, Update, Stream, StreamV> HalfHashJoin<'a, K, Buf, L, Update, Stream, StreamV>
+where
+    K: Eq + std::hash::Hash,
+    Buf: Iterator<Item = (K, Update::Repr)>,
+    Stream: Iterator<Item = (K, StreamV)>,
+    Update: LatticeRepr,
+    L: LatticeRepr + Merge<Update>,
+    L::Repr: Clone,
+{
+    pub fn new(buf: Buf, stream: Stream, state: &'a mut HalfHashJoinState<K, L>) -> Self {
+        Self {
+            buf,
+            stream,
+            state,
+            _marker: PhantomData,
+        }
+    }
+}
 
 impl<L> Default for BatchJoinState<L>
 where
