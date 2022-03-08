e3503c1bd877655bb4e44430ff7736951bc37b9a
Mingwei Samuel <mingwei.samuel@gmail.com>
2022-03-08 13:02:34 -0800

    Make groupby tests more relational-looking

--- hydroflow/tests/groupby.rs
+++ hydroflow/tests/groupby.rs
@@ -1,5 +1,5 @@
-use std::cell::{Cell, RefCell};
-use std::collections::HashMap;
+use std::cell::RefCell;
+use std::collections::{BinaryHeap, HashMap};
 use std::rc::Rc;
 
 use hydroflow::lang::collections::Iter;
@@ -7,22 +7,94 @@ use hydroflow::scheduled::graph::Hydroflow;
 use hydroflow::scheduled::graph_ext::GraphExt;
 use hydroflow::scheduled::handoff::VecHandoff;
 
-/// First batch of items for monotonic threshold test.
-const BATCH_A: [&'static str; 7] = ["megan", "davis", "mingwei", "john", "justin", "joe", "mae"];
-/// Second batch.
-const BATCH_B: [&'static str; 7] = [
-    "mingwei", "lauren", "justin", "mae", "mingwei", "justin", "pierce",
+#[derive(Clone, Copy, Debug)]
+pub struct Employee {
+    pub name: &'static str,
+    pub department: &'static str,
+    pub salary: u32,
+}
+const BATCH_A: &[Employee] = &[
+    Employee {
+        name: "Davis",
+        department: "hr",
+        salary: 7000,
+    },
+    Employee {
+        name: "Megan",
+        department: "accounting",
+        salary: 6500,
+    },
+    Employee {
+        name: "Zach",
+        department: "sales",
+        salary: 8000,
+    },
+    Employee {
+        name: "Pierce",
+        department: "accounting",
+        salary: 7100,
+    },
+    Employee {
+        name: "Alexa",
+        department: "sales",
+        salary: 9000,
+    },
 ];
-/// Third & final batch.
-const BATCH_C: [&'static str; 3] = ["joe", "mae", "zach"];
 
-/// Basic monotonic threshold: release a value once after it has been seen three times.
+const BATCH_B: &[Employee] = &[
+    Employee {
+        name: "Joanne",
+        department: "engineering",
+        salary: 6800,
+    },
+    Employee {
+        name: "Suzanne",
+        department: "hr",
+        salary: 5900,
+    },
+    Employee {
+        name: "Raul",
+        department: "engineering",
+        salary: 12000,
+    },
+    Employee {
+        name: "Buffy",
+        department: "sales",
+        salary: 8800,
+    },
+    Employee {
+        name: "Chaitali",
+        department: "accounting",
+        salary: 6900,
+    },
+];
+
+const BATCH_C: &[Employee] = &[
+    Employee {
+        name: "Rene",
+        department: "engineering",
+        salary: 7000,
+    },
+    Employee {
+        name: "Miles",
+        department: "hr",
+        salary: 6100,
+    },
+    Employee {
+        name: "Phillip",
+        department: "sales",
+        salary: 5800,
+    },
+];
+
+/// Basic monotonic threshold: find all departments with total salary at least 20_000.
 /// Uses the core API.
+/// SQL: SELECT department FROM employees WHERE 20_000 <= SUM(salary) GROUP BY department
 #[test]
 fn groupby_monotonic_core() {
     let mut hf = Hydroflow::new();
 
-    let (source_send, source_recv) = hf.make_edge::<_, VecHandoff<&'static str>>("source handoff");
+    let (source_send, source_recv) = hf.make_edge::<_, VecHandoff<Employee>>("source handoff");
     let input = hf.add_input("source", source_send);
 
     let (sink_send, sink_recv) = hf.make_edge::<_, VecHandoff<&'static str>>("sink handoff");
@@ -33,11 +105,12 @@ fn groupby_monotonic_core() {
         source_recv,
         sink_send,
         move |_ctx, recv, send| {
-            for item in recv.take_inner() {
-                let count = groups.entry(item).or_default();
-                *count += 1;
-                if 3 == *count {
-                    send.give(Some(item));
+            for employee in recv.take_inner() {
+                let cost = groups.entry(employee.department).or_default();
+                let old_cost = *cost;
+                *cost += employee.salary;
+                if old_cost < 20_000 && 20_000 <= *cost {
+                    send.give(Some(employee.department));
                 }
             }
         },
@@ -59,23 +132,23 @@ fn groupby_monotonic_core() {
     input.give(Iter(BATCH_B.iter().cloned()));
     input.flush();
     hf.tick();
-    assert_eq!(&["mingwei", "justin"], &**output.borrow());
+    assert_eq!(&["sales", "accounting"], &**output.borrow());
 
     input.give(Iter(BATCH_C.iter().cloned()));
     input.flush();
     hf.tick();
-    assert_eq!(&["mingwei", "justin", "mae"], &**output.borrow());
+    assert_eq!(&["sales", "accounting", "engineering"], &**output.borrow());
 }
 
 /// Basic monotonic threshold: release a value once after it has been seen three times.
 /// Uses the surface (builder) API.
+/// SQL: SELECT department FROM employees WHERE 20_000 <= SUM(salary) GROUP BY department
 #[test]
 fn groupby_monotonic_surface() {
     use hydroflow::builder::prelude::*;
 
     let mut hf_builder = HydroflowBuilder::new();
-    let (input, source_recv) =
-        hf_builder.add_channel_input::<_, _, VecHandoff<&'static str>>("source");
+    let (input, source_recv) = hf_builder.add_channel_input::<_, _, VecHandoff<Employee>>("source");
 
     let output = <Rc<RefCell<Vec<&'static str>>>>::default();
     let output_ref = output.clone();
@@ -84,12 +157,19 @@ fn groupby_monotonic_surface() {
         "main",
         source_recv
             .flatten()
-            .map_scan(HashMap::new(), |groups, item| {
-                let count = groups.entry(item).or_default();
-                *count += 1;
-                (item, *count)
+            .map_scan(HashMap::<&'static str, u32>::new(), |groups, employee| {
+                let count = groups.entry(employee.department).or_default();
+                let old_count = *count;
+                *count += employee.salary;
+                (employee.department, old_count, *count)
             })
-            .filter_map(|(item, count)| if 3 == count { Some(item) } else { None })
+            .filter_map(|(department, old_count, count)| {
+                if old_count < 20_000 && 20_000 <= count {
+                    Some(department)
+                } else {
+                    None
+                }
+            })
             .pull_to_push()
             .for_each(move |item| output_ref.borrow_mut().push(item)),
     );
@@ -104,47 +184,55 @@ fn groupby_monotonic_surface() {
     input.give(Iter(BATCH_B.iter().cloned()));
     input.flush();
     hf.tick();
-    assert_eq!(&["mingwei", "justin"], &**output.borrow());
+    assert_eq!(&["sales", "accounting"], &**output.borrow());
 
     input.give(Iter(BATCH_C.iter().cloned()));
     input.flush();
     hf.tick();
-    assert_eq!(&["mingwei", "justin", "mae"], &**output.borrow());
+    assert_eq!(&["sales", "accounting", "engineering"], &**output.borrow());
 }
 
-/// Non-monotonic barrier. Find the median name.
+/// Non-monotonic barrier. Per epoch, for each department find the highest paid employee.
 /// Takes in BATCH_A in the first epoch, then BATCH_B *and* BATCH_C in the second epoch.
+/// SQL: SELECT department, name, salary FROM employees WHERE salary = MAX(salary) GROUP BY department
 #[test]
 fn groupby_nonmon_surface() {
     use hydroflow::builder::prelude::*;
 
     let mut hf_builder = HydroflowBuilder::new();
-    let (input, source_recv) =
-        hf_builder.add_channel_input::<_, _, VecHandoff<&'static str>>("source");
+    let (input, source_recv) = hf_builder.add_channel_input::<_, _, VecHandoff<Employee>>("source");
 
-    let (a_send, a_recv) = hf_builder.make_edge::<_, VecHandoff<&'static str>, _>("names a-m");
-    let (z_send, z_recv) = hf_builder.make_edge::<_, VecHandoff<&'static str>, _>("names n-z");
+    let (a_send, a_recv) = hf_builder.make_edge::<_, VecHandoff<Employee>, _>("names A-M");
+    let (z_send, z_recv) = hf_builder.make_edge::<_, VecHandoff<Employee>, _>("names N-Z");
     let (stratum_boundary_send, stratum_boundary_recv) =
-        hf_builder.make_edge::<_, VecHandoff<&'static str>, _>("names n-z");
+        hf_builder.make_edge::<_, VecHandoff<Employee>, _>("names N-Z");
 
     // Make output first, to mess with scheduler order.
-    let output = <Rc<Cell<Option<(usize, &'static str)>>>>::default();
+    let output = <Rc<RefCell<BinaryHeap<_>>>>::default();
     let output_ref = output.clone();
     hf_builder.add_subgraph_stratified(
         "find median",
         1,
         stratum_boundary_recv
-            .map(|mut buffer| {
-                let batch_size = buffer.len();
-                let median = *buffer
-                    .make_contiguous()
-                    .select_nth_unstable(batch_size / 2)
-                    .1;
-                (batch_size, median)
+            .flat_map(|buffer| {
+                let groups = buffer.into_iter().fold(
+                    HashMap::<&'static str, Employee>::new(),
+                    |mut groups, employee| {
+                        if groups
+                            .get(employee.department)
+                            .map(|other_employee| other_employee.salary < employee.salary)
+                            .unwrap_or(true)
+                        {
+                            groups.insert(employee.department, employee);
+                        }
+                        groups
+                    },
+                );
+                groups
             })
             .pull_to_push()
-            .map(Some)
-            .for_each(move |val| output_ref.set(val)),
+            .map(|(department, employee)| (department, employee.name, employee.salary))
+            .for_each(move |val| output_ref.borrow_mut().push(val)),
     );
 
     // Partition then re-merge names to make graph more interesting.
@@ -153,7 +241,7 @@ fn groupby_nonmon_surface() {
         "split",
         0,
         source_recv.flatten().pull_to_push().partition(
-            |&name| name < "n",
+            |employee| employee.name < "N",
             hf_builder.start_tee().map(Some).push_to(a_send),
             hf_builder.start_tee().map(Some).push_to(z_send),
         ),
@@ -177,12 +265,19 @@ fn groupby_nonmon_surface() {
     hf.tick_stratum();
     assert_eq!((0, 0), (hf.current_epoch(), hf.current_stratum()));
 
-    assert_eq!(None, output.get());
+    assert_eq!(0, output.borrow().len());
 
     hf.tick();
     assert_eq!((1, 1), (hf.current_epoch(), hf.current_stratum()));
 
-    assert_eq!(Some((BATCH_A.len(), "justin")), output.get());
+    assert_eq!(
+        &[
+            ("accounting", "Pierce", 7100),
+            ("hr", "Davis", 7000),
+            ("sales", "Alexa", 9000),
+        ],
+        &*std::mem::take(&mut *output.borrow_mut()).into_sorted_vec(),
+    );
 
     // Give BATCH_B but only run this stratum.
     input.give(Iter(BATCH_B.iter().cloned()));
@@ -199,6 +294,14 @@ fn groupby_nonmon_surface() {
     assert_eq!((3, 1), (hf.current_epoch(), hf.current_stratum()));
 
     // Second batch has 7+3 = 10 items.
-    assert_eq!(Some((BATCH_B.len() + BATCH_C.len(), "mae")), output.get());
+    assert_eq!(
+        &[
+            ("accounting", "Chaitali", 6900),
+            ("engineering", "Raul", 12000),
+            ("hr", "Miles", 6100),
+            ("sales", "Buffy", 8800),
+        ],
+        &*std::mem::take(&mut *output.borrow_mut()).into_sorted_vec(),
+    );
     assert_eq!(false, hf.next_stratum());
 }
