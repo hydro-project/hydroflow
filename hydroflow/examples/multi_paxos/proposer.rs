8d8b2eb903319f8359e73ee57deaf2cc56600cee
Rithvik <rtpanch@gmail.com>
2022-04-11 21:39:51 -0700

    Fix part of the proposer tcp outbound vertex

--- hydroflow/examples/multi_paxos/proposer.rs
+++ hydroflow/examples/multi_paxos/proposer.rs
@@ -70,62 +70,70 @@ pub(crate) async fn run_proposer(opts: Opts) {
             .flatten()
             .map_scan(
                 HashMap::<u16, ProposerSlotData>::new(),
-                move |slots, msg| match msg {
-                    Msg::ClientReq(msg) => {
-                        let max_slot = slots.keys().max().unwrap_or(&0);
-                        // let max_slot = slots.iter().max_by(|a, b| a.0.cmp(b.0)).map(|(k, _)| k);
-                        let next_slot = max_slot + 1;
+                move |slots, msg| {
+                    let resp = match msg {
+                        Msg::ClientReq(msg) => {
+                            let max_slot = slots.keys().max().unwrap_or(&0);
+                            // let max_slot = slots.iter().max_by(|a, b| a.0.cmp(b.0)).map(|(k, _)| k);
+                            let next_slot = max_slot + 1;
 
-                        let new_slot_entry = ProposerSlotData {
-                            val: msg.val,
-                            slot: next_slot,
-                            ballot: 0,
-                            p1b_count: 0,
-                            p2b_count: 0,
-                        };
+                            let new_slot_entry = ProposerSlotData {
+                                val: msg.val,
+                                slot: next_slot,
+                                ballot: 0,
+                                p1b_count: 0,
+                                p2b_count: 0,
+                            };
 
-                        // Put requested value into hash map
-                        slots.insert(next_slot, new_slot_entry);
-                        Some(ProposerMsg {
-                            addr: opts.addr,
-                            slot: new_slot_entry.slot,
-                            ballot: new_slot_entry.ballot,
-                            pid: proposer_id,
-                            val: msg.val,
-                            mtype: MsgType::P1A,
-                        })
-                    }
-                    Msg::AcceptorRes(msg) => {
-                        let slot_entry = slots.get_mut(&msg.slot).unwrap();
-                        let target = acceptor_count / 2 + 1;
-                        match msg.mtype {
-                            MsgType::P1B => {
-                                slot_entry.p1b_count += 1;
-                                slot_entry.val = msg.val.unwrap_or(slot_entry.val);
+                            // Put requested value into hash map
+                            slots.insert(next_slot, new_slot_entry);
+                            Some(ProposerMsg {
+                                addr: opts.addr,
+                                slot: new_slot_entry.slot,
+                                ballot: new_slot_entry.ballot,
+                                pid: proposer_id,
+                                val: msg.val,
+                                mtype: MsgType::P1A,
+                            })
+                        }
+                        Msg::AcceptorRes(msg) => {
+                            let slot_entry = slots.get_mut(&msg.slot).unwrap();
+                            let target = acceptor_count / 2 + 1;
+                            match msg.mtype {
+                                MsgType::P1B => {
+                                    slot_entry.p1b_count += 1;
+                                    slot_entry.val = msg.val.unwrap_or(slot_entry.val);
 
-                                if slot_entry.p1b_count == target {
-                                    Some(ProposerMsg {
-                                        addr: opts.addr,
-                                        slot: slot_entry.slot,
-                                        ballot: slot_entry.ballot,
-                                        pid: proposer_id,
-                                        val: slot_entry.val,
-                                        mtype: MsgType::P2A,
-                                    })
-                                } else {
+                                    if slot_entry.p1b_count == target {
+                                        Some(ProposerMsg {
+                                            addr: opts.addr,
+                                            slot: slot_entry.slot,
+                                            ballot: slot_entry.ballot,
+                                            pid: proposer_id,
+                                            val: slot_entry.val,
+                                            mtype: MsgType::P2A,
+                                        })
+                                    } else {
+                                        None
+                                    }
+                                }
+                                MsgType::P2B => {
+                                    slot_entry.p2b_count += 1;
+
+                                    if slot_entry.p2b_count == target {
+                                        println!("yeehaw: {:?}", slot_entry.slot);
+                                    }
                                     None
                                 }
+                                default => None,
                             }
-                            MsgType::P2B => {
-                                slot_entry.p2b_count += 1;
-
-                                if slot_entry.p2b_count == target {
-                                    println!("yeehaw: {:?}", slot_entry.slot);
-                                }
-                                None
-                            }
-                            default => None,
                         }
+                    };
+
+                    if resp.is_some() {
+                        Some((opts.addr.clone(), resp.unwrap().clone())) // FIXME
+                    } else {
+                        None
                     }
                 },
             )
