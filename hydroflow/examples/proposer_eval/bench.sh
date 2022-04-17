c10e8bdbbb2ef8fc4c8e61097bd62d3db2549dfb
Kaushik Shivakumar <kaushikshivakumar@kaushiks-mbp-2.lan>
2022-04-16 20:05:32 -0700

    add bench script

--- hydroflow/examples/proposer_eval/bench.sh
+++ hydroflow/examples/proposer_eval/bench.sh
@@ -1,7 +1,18 @@
+declare -a bgpids
+
+cleanup() {
+    for pid in ${bgpids[@]}; do
+        kill -9 $pid
+    done
+}
+trap cleanup EXIT SIGINT SIGTERM
 
 cargo run --example proposer_eval --  --role acceptor --port 1400 --addr localhost --id 14 &
+bgpids+=($!)
 cargo run --example proposer_eval --  --role acceptor --port 1401 --addr localhost --id 15 &
+bgpids+=($!)
 cargo run --example proposer_eval --  --role acceptor --port 1402 --addr localhost --id 16 &
+bgpids+=($!)
 
 sleep 2
 
