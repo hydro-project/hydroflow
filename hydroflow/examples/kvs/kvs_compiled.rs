8e32b2db15f59205f690c9b77cb737a546e64783
Justin Jaffray <justin.jaffray@gmail.com>
2022-04-26 10:44:11 -0700

    Make KVS benchmark more Anna-like (#146)
    I decided it made the most sense to remove the other implementations here,
    since there were a lot of structural changes going on, they made it annoying to
    update this. They live on in the repo history if they ever need to be revived.
    
    This commit moves the workload generation into the workers themselves, since
    otherwise I was unable to fully saturate the workers.

--- hydroflow/examples/kvs/kvs_compiled.rs
+++ hydroflow/examples/kvs/kvs_compiled.rs
@@ -1,9 +1,6 @@
-use std::time::Duration;
+use std::{cell::RefCell, collections::VecDeque, rc::Rc, time::Duration};
 
-use futures::{
-    channel::mpsc::{Receiver, Sender},
-    SinkExt, StreamExt,
-};
+use futures::channel::mpsc::{channel, Receiver, Sender};
 use hydroflow::{
     builder::{
         prelude::{BaseSurface, PullSurface, PushSurface},
@@ -18,7 +15,7 @@ use hydroflow::{
     scheduled::handoff::VecHandoff,
 };
 
-use crate::common::{spawn, Clock, ClockedDataRepr, ClockedUpdateRepr, Message};
+use crate::common::{Clock, ClockedDataRepr, ClockedUpdateRepr, Dist, Message, Sample};
 
 #[derive(Clone)]
 pub struct Kvs<K, V>
@@ -27,48 +24,55 @@ where
     V: Send + Clone,
 {
     senders: Vec<Sender<Message<K, V>>>,
+    pub monitor: Rc<RefCell<Receiver<MonitorInfo>>>,
     round_robin: usize,
 }
 
 impl<K, V> Kvs<K, V>
 where
-    K: 'static + Clone + Eq + std::hash::Hash + Send + std::fmt::Debug,
+    K: 'static + Clone + Eq + std::hash::Hash + Send + std::fmt::Debug + Default,
     V: 'static + Clone + Send + std::fmt::Debug + Ord + Default,
+    Dist: Sample<K>,
 {
-    pub fn new(workers: u64) -> Self {
-        let senders = spawn_threads::<K, V>(workers);
+    pub fn new(workers: u64, config: WorkerConfig) -> Self {
+        let (senders, monitor) = spawn_threads::<K, V>(workers, config);
 
         Kvs {
             senders,
+            monitor: Rc::new(RefCell::new(monitor)),
             round_robin: 0,
         }
     }
 
+    #[allow(unused)]
     pub async fn set(&mut self, k: K, v: V) {
-        let receiver = self.round_robin % self.senders.len();
-        self.round_robin += 1;
-        self.senders[receiver]
-            .send(Message::Set(k, v))
-            .await
-            .unwrap();
+        self.send_round_robin(Message::Set(k, v));
     }
 
+    #[allow(unused)]
     pub async fn get(&mut self, k: K) -> Option<(Clock, V)> {
         // TODO: We need to make sure we talk to one that is correct, but for
         // now since everyone owns everything just send a message to whoever.
-        let receiver_idx = self.round_robin % self.senders.len();
-        self.round_robin += 1;
         let (sender, receiver) = futures::channel::oneshot::channel();
-        self.senders[receiver_idx]
-            .send(Message::Get(k, sender))
-            .await
-            .unwrap();
-
+        self.send_round_robin(Message::Get(k, sender));
         receiver.await.ok()
+    }
+
+    fn send_round_robin(&mut self, mut message: Message<K, V>) {
+        let mut receiver_idx = self.round_robin % self.senders.len();
+        self.round_robin += 1;
+        while let Err(msg) = self.senders[receiver_idx].try_send(message) {
+            receiver_idx = self.round_robin % self.senders.len();
+            self.round_robin += 1;
+            message = msg.into_inner();
+        }
     }
 }
 
 // TODO(justin): this thing is hacky.
+// Alternates between sending out a single unit and sending out nothing. The
+// result of this is an iterator which, when put into a Hydroflow graph, will
+// tick exactly once per quantum.
 #[derive(Debug, Copy, Clone)]
 struct PerQuantumPulser {
     on: bool,
@@ -92,36 +96,149 @@ impl Iterator for PerQuantumPulser {
     }
 }
 
-fn spawn_threads<K, V>(workers: u64) -> Vec<Sender<Message<K, V>>>
+type Matrix<K, V> = Vec<(Receiver<Message<K, V>>, Vec<Sender<Message<K, V>>>)>;
+type MessageSender<K, V> = Sender<Message<K, V>>;
+
+// Constructs the NxN matrix of channels between the various participants in the
+// KVS so they can all send each other messages.
+fn make_communication_matrix<K, V>(n: u64) -> (Matrix<K, V>, Vec<MessageSender<K, V>>)
 where
-    K: 'static + Clone + Eq + std::hash::Hash + Send + std::fmt::Debug,
+    K: Send + Clone,
+    V: Ord + Send + Clone,
+{
+    let mut receivers = Vec::new();
+    let mut senders: Vec<_> = (0..n).map(|_| Vec::new()).collect();
+    let mut extra_senders = Vec::new();
+    for _ in 0..n {
+        let (sender, receiver) = channel(8192);
+        receivers.push(receiver);
+        for s in senders.iter_mut() {
+            s.push(sender.clone())
+        }
+        extra_senders.push(sender);
+    }
+
+    (
+        receivers.into_iter().zip(senders.into_iter()).collect(),
+        extra_senders,
+    )
+}
+
+// Every worker sends out information detailing its current status.
+pub struct MonitorInfo {
+    // Which worker this came from.
+    pub from: usize,
+    // How many messages this worker has so far processed.
+    pub data: usize,
+}
+
+pub(crate) fn spawn<F, K, V>(
+    n: u64,
+    config: WorkerConfig,
+    f: F,
+) -> (Vec<Sender<Message<K, V>>>, Receiver<MonitorInfo>)
+where
+    F: 'static
+        + Fn(
+            usize,
+            Receiver<Message<K, V>>,
+            Vec<Sender<Message<K, V>>>,
+            Sender<MonitorInfo>,
+            WorkerConfig,
+        )
+        + Send
+        + Clone,
+    K: 'static + Send + Clone,
+    V: 'static + Ord + Send + Clone,
+{
+    let core_ids = core_affinity::get_core_ids().unwrap();
+    let mut core_idx = 1;
+
+    let (monitor_send, monitor_recv) = channel(1024);
+
+    let (matrix, senders) = make_communication_matrix(n);
+    for (i, (receiver, senders)) in matrix.into_iter().enumerate() {
+        let f = f.clone();
+        let id = core_ids[core_idx];
+        core_idx = (core_idx + 1) % core_ids.len();
+
+        let monitor = monitor_send.clone();
+        let config = config.clone();
+        std::thread::spawn(move || {
+            core_affinity::set_for_current(id);
+            f(i, receiver, senders, monitor, config);
+        });
+    }
+
+    (senders, monitor_recv)
+}
+
+#[derive(Clone)]
+pub struct WorkerConfig {
+    pub epoch: Duration,
+    pub batch_size: usize,
+    pub dist: Dist,
+}
+
+fn spawn_threads<K, V>(
+    workers: u64,
+    config: WorkerConfig,
+) -> (Vec<Sender<Message<K, V>>>, Receiver<MonitorInfo>)
+where
+    K: 'static + Clone + Eq + std::hash::Hash + Send + std::fmt::Debug + Default,
     V: 'static + Clone + Send + std::fmt::Debug + Ord + Default,
+    Dist: Sample<K>,
 {
     spawn(
         workers,
-        move |id, receiver: Receiver<Message<K, V>>, mut senders: Vec<Sender<Message<K, V>>>| {
+        config,
+        move |id,
+              _receiver: Receiver<Message<K, V>>,
+              mut senders: Vec<Sender<Message<K, V>>>,
+              mut monitor: Sender<MonitorInfo>,
+              config: WorkerConfig| {
             let rt = tokio::runtime::Runtime::new().unwrap();
+            let mut dist = config.dist.clone();
             rt.block_on(async move {
                 let mut hf = HydroflowBuilder::default();
 
                 let (z_send, z_recv) = hf.add_channel_input::<_, Option<u64>, VecHandoff<u64>>("ticks");
 
                 // Construct the ticker.
-                let epoch_duration = Duration::from_millis(100);
                 tokio::spawn(async move {
                     let mut tick = 0;
                     loop {
-                        tokio::time::sleep(epoch_duration).await;
+                        tokio::time::sleep(config.epoch).await;
                         z_send.give(Some(tick));
                         z_send.flush();
                         tick += 1;
                     }
                 });
 
-                let q_recv = hf.add_input_from_stream::<_, Option<_>, VecHandoff<_>, _>("writes", receiver.map(Some));
+                let (system_send, system_recv) = hf.make_edge::<_, VecHandoff<Message<K, V>>, VecDeque<Message<K, V>>>("writes");
+
+                let mut rng = rand::thread_rng();
+
+                // Once per epoch, generate a batch of data.
+                hf.add_subgraph(
+                    "workload_generator",
+                    IterPullSurface::new(PerQuantumPulser::new()).map_with_context(|ctx, _| {
+                        // TODO(mingwei): figure out a nicer way to have a regularly scheduled operator.
+                        ctx.waker().wake();
+                        ctx.current_epoch()
+                    }).map(move |_|{
+                        (0..config.batch_size).map(|_| {
+                            Message::Set(dist.sample(&mut rng), <V as Default>::default())
+                        }).collect::<VecDeque<_>>()
+                    })
+                    .pull_to_push()
+                    .push_to(system_send)
+                );
 
                 // Make the ownership table. Everyone owns everything.
-                let ownership = (0..senders.len()).map(|i| ((), Single(i)));
+                let ownership = (0..0).map(|i| ((), Single(i)));
+                // TODO(justin): this should be configurable.
+                // let ownership = (0..senders.len()).map(|i| ((), Single(i)));
 
                 let (reads_send, reads_recv) =
                     hf.make_edge::<_, VecHandoff<(K, futures::channel::oneshot::Sender<(Clock, V)>)>, Option<(K, futures::channel::oneshot::Sender<(Clock, V)>)>>("reads");
@@ -129,13 +246,22 @@ where
                     hf.make_edge::<_, VecHandoff<(usize, u64, Vec<(K, V)>)>, Option<(usize, u64, Vec<(K, V)>)>>("received_batches");
                 let (y_send, y_recv) = hf.make_edge::<_, VecHandoff<(K, V)>, Option<(K, V)>>("write_requests");
 
+                let processed = hf.hydroflow.add_state(RefCell::new(0_usize));
+
                 hf.add_subgraph(
                     "write_handler",
                     y_recv
+                        .inspect_with_context(move |ctx, msg| {
+                            *ctx.state_ref(processed).borrow_mut() += msg.len();
+                            let _ = monitor.try_send(MonitorInfo {
+                                from: id,
+                                data: *ctx.state_ref(processed).borrow(),
+                            });
+                        })
                         .flatten()
                         .map(|(k, v)| Single((k, v)))
                         .batch_with::<_, MapUnionRepr<tag::HASH_MAP, K, MaxRepr<V>>, MapUnionRepr<tag::SINGLE, K, MaxRepr<V>>, _>(z_recv.flatten())
-                        .flat_map(|(epoch, batch)| {
+                        .flat_map(move |(epoch, batch)| {
                             batch.into_iter().map(move |x| (epoch, x))
                         })
                         // TODO(justin): have a real hasher here, right now have
@@ -168,7 +294,8 @@ where
                 // TODO(justin): this is super ugly, we probably need a macro or something for this.
                 hf.add_subgraph(
                     "demultiplexer",
-                    q_recv.flatten().pull_to_push().partition(
+                    system_recv.flatten().pull_to_push()
+                    .partition(
                         |x| matches!(x, Message::Set(_, _)),
                         hf.start_tee()
                             .map(|msg| {
--- hydroflow/examples/kvs/main.rs
+++ hydroflow/examples/kvs/main.rs
@@ -2,222 +2,55 @@
 
 extern crate hdrhistogram;
 
-use std::{
-    str::FromStr,
-    time::{Duration, Instant},
-};
+use std::time::{Duration, Instant};
 
-use clap::{ArgEnum, Parser};
-use common::Clock;
-use hdrhistogram::Histogram;
-use hydroflow::tokio;
-use rand::{prelude::Distribution, Rng};
-use zipf::ZipfDistribution;
+use clap::Parser;
+use common::Dist;
+
+use crate::kvs_compiled::WorkerConfig;
 
 mod common;
 mod kvs_compiled;
-mod kvs_raw;
-mod kvs_scheduled;
 
-// This would ideally be a trait, but trait methods can't be async.
-#[derive(Clone)]
-enum KvsImplementation<K, V>
-where
-    K: 'static + Clone + Eq + std::hash::Hash + Send + std::fmt::Debug,
-    V: 'static + Clone + Send + std::fmt::Debug + Ord + Default,
-{
-    Raw(kvs_raw::Kvs<K, V>),
-    Scheduled(kvs_scheduled::Kvs<K, V>),
-    Compiled(kvs_compiled::Kvs<K, V>),
-}
-
-impl<K, V> KvsImplementation<K, V>
-where
-    K: 'static + Clone + Eq + std::hash::Hash + Send + std::fmt::Debug,
-    V: 'static + Clone + Send + std::fmt::Debug + Ord + Default,
-{
-    async fn set(&mut self, k: K, v: V) {
-        match self {
-            Self::Raw(kvs) => kvs.set(k, v).await,
-            Self::Scheduled(kvs) => kvs.set(k, v).await,
-            Self::Compiled(kvs) => kvs.set(k, v).await,
-        }
-    }
-
-    async fn get(&mut self, k: K) -> Option<(Clock, V)> {
-        match self {
-            Self::Raw(kvs) => kvs.get(k).await,
-            Self::Scheduled(kvs) => kvs.get(k).await,
-            Self::Compiled(kvs) => kvs.get(k).await,
-        }
-    }
-}
-
-impl<K, V> FromStr for KvsImplementation<K, V>
-where
-    K: 'static + Clone + Eq + std::hash::Hash + Send + std::fmt::Debug,
-    V: 'static + Clone + Send + std::fmt::Debug + Ord + Default,
-{
-    type Err = String;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let (kind, workers) = s
-            .split_once(',')
-            .ok_or("need to specify number of workers")?;
-        let workers = workers.parse().map_err(|e| format!("{}", e))?;
-        match kind {
-            "raw" => Ok(KvsImplementation::Raw(kvs_raw::Kvs::new(workers))),
-            "scheduled" => Ok(KvsImplementation::Scheduled(kvs_scheduled::Kvs::new(
-                workers,
-            ))),
-            "compiled" => Ok(KvsImplementation::Compiled(kvs_compiled::Kvs::new(workers))),
-            _ => Err("not a valid implementation".into()),
-        }
-    }
-}
-
-#[derive(ArgEnum, Clone)]
-enum Dist {
-    Uniform(usize),
-    Zipf(ZipfDistribution),
-}
-
-impl Dist {
-    fn sample<R: rand::Rng>(&mut self, rng: &mut R) -> usize {
-        match self {
-            Self::Uniform(n) => rng.gen_range(0..*n),
-            Self::Zipf(d) => d.sample(rng),
-        }
-    }
-
-    fn uniform(n: usize) -> Self {
-        Self::Uniform(n)
-    }
-
-    fn zipf(n: usize, theta: f64) -> Self {
-        Self::Zipf(ZipfDistribution::new(n, theta).unwrap())
-    }
-}
-
-impl FromStr for Dist {
-    type Err = String;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut contents = s.split(',');
-        match contents
-            .next()
-            .ok_or("type of distribution is required (uniform, zipf)")?
-        {
-            "uniform" => {
-                let n: usize = contents
-                    .next()
-                    .ok_or("require a number of keys parameter")?
-                    .parse()
-                    .map_err(|e| format!("{}", e))?;
-
-                Ok(Dist::uniform(n))
-            }
-            "zipf" => {
-                let n: usize = contents
-                    .next()
-                    .ok_or("require a number of keys parameter")?
-                    .parse()
-                    .map_err(|e| format!("{}", e))?;
-
-                let theta: f64 = contents
-                    .next()
-                    .ok_or("require a theta parameter")?
-                    .parse()
-                    .map_err(|e| format!("{}", e))?;
-
-                Ok(Dist::zipf(n, theta))
-            }
-            _ => Err("invalid distribution".into()),
-        }
-    }
-}
-
-#[derive(Parser)]
+#[derive(Clone, Parser)]
 struct Args {
-    #[clap(long)]
-    read_percentage: f64,
-    #[clap(long)]
-    num_benchmark_workers: u64,
     #[clap(long)]
     dist: Dist,
     #[clap(long)]
     run_seconds: u64,
     #[clap(long)]
-    implementation: KvsImplementation<String, String>,
+    workers: u64,
+    #[clap(long)]
+    batch_size: usize,
+    #[clap(long)]
+    epoch_millis: u64,
 }
 
 fn main() {
     let args = Args::parse();
 
-    let kvs = args.implementation;
+    // let kvs = args.implementation;
+    let dist = args.dist.clone();
+    let epoch = Duration::from_millis(args.epoch_millis);
+
+    let config = WorkerConfig {
+        epoch,
+        dist,
+        batch_size: args.batch_size,
+    };
+
+    let kvs = kvs_compiled::Kvs::<u64, u64>::new(args.workers, config);
     let run_duration = Duration::from_millis(args.run_seconds * 1000);
 
-    let read_percentage = args.read_percentage;
-    let bench_workers = args.num_benchmark_workers;
-
     let start = Instant::now();
-    let handles: Vec<_> = (0..bench_workers)
-        .map(|_| {
-            let mut kvs = kvs.clone();
-            let mut distribution = args.dist.clone();
-            std::thread::spawn(move || {
-                let mut i = 0;
-                let mut rng = rand::thread_rng();
-                let rt = tokio::runtime::Builder::new_current_thread()
-                    .enable_all()
-                    .build()
-                    .unwrap();
-                let mut writes_hist = Histogram::<u64>::new(2).unwrap();
-                let mut reads_hist = Histogram::<u64>::new(2).unwrap();
-                rt.block_on(async move {
-                    while start.elapsed() < run_duration {
-                        let before = Instant::now();
-                        if rng.gen::<f64>() < read_percentage {
-                            kvs.get(format!("key{}", distribution.sample(&mut rng)))
-                                .await;
-                            reads_hist
-                                .record(before.elapsed().as_micros().try_into().unwrap())
-                                .unwrap();
-                        } else {
-                            kvs.set(
-                                format!("key{}", distribution.sample(&mut rng)),
-                                format!("bar{}", i),
-                            )
-                            .await;
-                            writes_hist
-                                .record(before.elapsed().as_micros().try_into().unwrap())
-                                .unwrap();
-                        }
-                        i += 1;
-                    }
 
-                    (writes_hist, reads_hist)
-                })
-            })
-        })
-        .collect();
-
-    let mut writes_hist = Histogram::<u64>::new(2).unwrap();
-    let mut reads_hist = Histogram::<u64>::new(2).unwrap();
-    for handle in handles {
-        let (writes, reads) = handle.join().unwrap();
-        writes_hist.add(writes).unwrap();
-        reads_hist.add(reads).unwrap();
+    let mut data: Vec<_> = (0..args.workers).map(|_| 0).collect();
+    while start.elapsed() < run_duration {
+        if let Ok(Some(v)) = (*kvs.monitor).borrow_mut().try_next() {
+            data[v.from] = v.data;
+        }
     }
 
-    println!(
-        "ops={}",
-        writes_hist.len() + reads_hist.len(),
-        // writes_hist.mean(),
-        // writes_hist.value_at_quantile(0.99),
-        // writes_hist.value_at_quantile(0.999),
-        // reads_hist.mean(),
-        // reads_hist.value_at_quantile(0.99),
-        // reads_hist.value_at_quantile(0.999),
-    );
+    let sum: usize = data.iter().sum();
+    println!("{},{}", args.workers, sum);
 }
