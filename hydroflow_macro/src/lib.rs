deba1d09a399a20f0359bcfe1ecaaddb9dbaecad
Mingwei Samuel <mingwei.samuel@gmail.com>
2022-06-10 19:10:08 -0700

    parse updated arrow syntax

--- hydroflow_macro/src/lib.rs
+++ hydroflow_macro/src/lib.rs
@@ -1,3 +1,5 @@
+#![feature(proc_macro_diagnostic)]
+
 use std::collections::HashMap;
 
 use proc_macro2::Literal;
@@ -6,7 +8,7 @@ use slotmap::{DefaultKey, SlotMap};
 use syn::{parse_macro_input, spanned::Spanned, Ident};
 
 mod parse;
-use parse::{HfCode, Operator, Pipeline};
+use parse::{HfCode, HfStatement, NamePipeline, NamedHfStatement, Operator, Pipeline};
 
 #[proc_macro]
 pub fn hydroflow_parser(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
@@ -51,47 +53,82 @@ impl Graph {
         Ok(())
     }
 
-    pub fn add_pipeline(&mut self, pipeline: Pipeline) -> PipelinePtrPair {
+    fn add_statement(&mut self, stmt: HfStatement) {
+        match stmt {
+            parse::HfStatement::Named(NamedHfStatement {
+                name,
+                equals: _,
+                pipeline,
+            }) => {
+                if let Some(pipe_ptr) = self.add_pipeline(pipeline) {
+                    self.names.insert(name, pipe_ptr);
+                }
+            }
+            parse::HfStatement::Pipeline(pipeline) => {
+                self.add_pipeline(pipeline);
+            }
+        }
+    }
+
+    pub fn add_pipeline(&mut self, pipeline: Pipeline) -> Option<PipelinePtrPair> {
         match pipeline {
             Pipeline::Chain(chain_pipeline) => {
-                let mut pipe_ptr = PipelinePtrPair::default();
+                let mut pipe_ptr: Option<PipelinePtrPair> = None;
 
                 if chain_pipeline.leading_arrow.is_some() {
                     // TODO(mingwei). this should do something
                 }
                 for elem in chain_pipeline.elems {
                     let sub_ptr = self.add_pipeline(elem);
+                    if let Some(sub_ptr) = sub_ptr {
+                        let pipe_ptr = pipe_ptr.get_or_insert_with(Default::default);
 
-                    if let (Some(out), Some(inn)) = (pipe_ptr.out, sub_ptr.inn) {
-                        self.operators[out].succs.push(inn);
-                        self.operators[inn].preds.push(out);
+                        if let (Some(out), Some(inn)) = (pipe_ptr.out, sub_ptr.inn) {
+                            self.operators[out].succs.push(inn);
+                            self.operators[inn].preds.push(out);
+                        }
+
+                        pipe_ptr.inn = pipe_ptr.inn.or(sub_ptr.inn);
+                        pipe_ptr.out = sub_ptr.out;
                     }
-
-                    pipe_ptr.inn = pipe_ptr.inn.or(sub_ptr.inn);
-                    pipe_ptr.out = sub_ptr.out;
                 }
 
                 pipe_ptr
             }
-            Pipeline::Multiple(multiple) => {
-                // TODO: match on name.
-                let (preds, succs) = Default::default();
-                let key = self.operators.insert(OpInfo {
-                    operator: None,
-                    preds,
-                    succs,
-                });
-                PipelinePtrPair {
-                    inn: Some(key),
-                    out: Some(key),
+            // Pipeline::Multiple(multiple) => {
+            //     // TODO: match on name.
+            //     let (preds, succs) = Default::default();
+            //     let key = self.operators.insert(OpInfo {
+            //         operator: None,
+            //         preds,
+            //         succs,
+            //     });
+            //     PipelinePtrPair {
+            //         inn: Some(key),
+            //         out: Some(key),
+            //     }
+            // }
+            // Pipeline::Ident(ident) => {
+            //     *self
+            //         .names
+            //         .get(&ident)
+            //         .unwrap_or_else(|| panic!("Failed to find name: {}", ident))
+            //     // TODO(mingwei): error reporting
+            // }
+            Pipeline::Name(NamePipeline {
+                prefix,
+                name,
+                suffix,
+            }) => {
+                // TODO(mingwei): PREFIX AND SUFFIX
+                let out = self.names.get(&name).copied();
+                if out.is_none() {
+                    name.span()
+                        .unwrap()
+                        .error(format!("Cannot find name `{}`.", name))
+                        .emit();
                 }
-            }
-            Pipeline::Ident(ident) => {
-                *self
-                    .names
-                    .get(&ident)
-                    .unwrap_or_else(|| panic!("Failed to find name: {}", ident))
-                // TODO(mingwei): error reporting
+                out
             }
             Pipeline::Operator(operator) => {
                 let (preds, succs) = Default::default();
@@ -100,10 +137,10 @@ impl Graph {
                     preds,
                     succs,
                 });
-                PipelinePtrPair {
+                Some(PipelinePtrPair {
                     inn: Some(key),
                     out: Some(key),
-                }
+                })
             }
         }
     }
@@ -128,19 +165,21 @@ fn hfcode_to_graph(input: HfCode) -> Graph {
     let mut graph = Graph::default();
 
     for stmt in input.statements {
-        let pipe_ptr = graph.add_pipeline(stmt.pipeline);
+        graph.add_statement(stmt);
 
-        match (stmt.name, stmt.equals) {
-            (Some(name), Some(_eq)) => {
-                graph.names.insert(name, pipe_ptr);
-            }
-            (None, None) => {
-                // Anonymous stmt.
-            }
-            _ => {
-                panic!("name and equal must be together!") // TODO(mingwei).
-            }
-        }
+        // let pipe_ptr = graph.add_pipeline(stmt.pipeline);
+
+        // match (stmt.name, stmt.equals) {
+        //     (Some(name), Some(_eq)) => {
+        //         graph.names.insert(name, pipe_ptr);
+        //     }
+        //     (None, None) => {
+        //         // Anonymous stmt.
+        //     }
+        //     _ => {
+        //         panic!("name and equal must be together!") // TODO(mingwei).
+        //     }
+        // }
     }
 
     graph
