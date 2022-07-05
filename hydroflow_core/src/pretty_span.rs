16a16054e9a3ff4bff81995e44e066d25267040d
Mingwei Samuel <mingwei.samuel@gmail.com>
2022-07-05 14:39:38 -0700

    Cleanup and rearrange hydroflow_core graph code

---
+++ hydroflow_core/src/pretty_span.rs
@@ -0,0 +1,15 @@
+/// Helper struct which displays the span as `path:row:col` for human reading/IDE linking.
+/// Example: `hydroflow\tests\surface_syntax.rs:42:18`.
+pub struct PrettySpan(pub proc_macro2::Span);
+impl std::fmt::Display for PrettySpan {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let span = self.0.unwrap();
+        write!(
+            f,
+            "{}:{}:{}",
+            span.source_file().path().display(),
+            span.start().line,
+            span.start().column
+        )
+    }
+}
