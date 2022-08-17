6c6f82e665982e5bf1589849772fef701aab4418
Mingwei Samuel <mingwei.samuel@gmail.com>
2022-08-17 14:06:30 -0700

    Move `scc_kosaraju()` into separate helper fn

--- hydroflow_lang/src/graph/graph_algorithms.rs
+++ hydroflow_lang/src/graph/graph_algorithms.rs
@@ -1,4 +1,5 @@
-use std::collections::HashSet;
+use std::collections::hash_map::Entry;
+use std::collections::{HashMap, HashSet};
 use std::hash::Hash;
 
 pub fn topo_sort<Id, NodeIds, PredsFn, PredsIter>(
@@ -40,6 +41,68 @@ where
     order
 }
 
+pub fn scc_kosaraju<Id, NodeIds, PredsFn, SuccsFn, PredsIter, SuccsIter>(
+    nodes: NodeIds,
+    mut preds_fn: PredsFn,
+    mut succs_fn: SuccsFn,
+) -> HashMap<Id, Id>
+where
+    Id: Copy + Eq + Hash,
+    NodeIds: IntoIterator<Item = Id>,
+    PredsFn: FnMut(Id) -> PredsIter,
+    SuccsFn: FnMut(Id) -> SuccsIter,
+    PredsIter: IntoIterator<Item = Id>,
+    SuccsIter: IntoIterator<Item = Id>,
+{
+    // https://en.wikipedia.org/wiki/Kosaraju%27s_algorithm
+    fn visit<Id, SuccsFn, SuccsIter>(
+        succs_fn: &mut SuccsFn,
+        u: Id,
+        seen: &mut HashSet<Id>,
+        stack: &mut Vec<Id>,
+    ) where
+        Id: Copy + Eq + Hash,
+        SuccsFn: FnMut(Id) -> SuccsIter,
+        SuccsIter: IntoIterator<Item = Id>,
+    {
+        if seen.insert(u) {
+            for v in (succs_fn)(u) {
+                visit(succs_fn, v, seen, stack);
+            }
+            stack.push(u);
+        }
+    }
+    let (mut seen, mut stack) = Default::default();
+    for sg in nodes {
+        visit(&mut succs_fn, sg, &mut seen, &mut stack);
+    }
+    let _ = seen;
+
+    fn assign<Id, PredsFn, PredsIter>(
+        preds_fn: &mut PredsFn,
+        v: Id,
+        root: Id,
+        components: &mut HashMap<Id, Id>,
+    ) where
+        Id: Copy + Eq + Hash,
+        PredsFn: FnMut(Id) -> PredsIter,
+        PredsIter: IntoIterator<Item = Id>,
+    {
+        if let Entry::Vacant(vacant_entry) = components.entry(v) {
+            vacant_entry.insert(root);
+            for u in (preds_fn)(v) {
+                assign(preds_fn, u, root, components);
+            }
+        }
+    }
+
+    let mut components = Default::default();
+    for sg in stack.into_iter().rev() {
+        assign(&mut preds_fn, sg, sg, &mut components);
+    }
+    components
+}
+
 #[cfg(test)]
 mod test {
     use std::collections::HashMap;
@@ -73,5 +136,54 @@ mod test {
         for (src, dst) in edges.iter() {
             assert!(position[src] < position[dst]);
         }
+    }
+
+    #[test]
+    pub fn test_scc_kosaraju() {
+        // https://commons.wikimedia.org/wiki/File:Scc-1.svg
+        let edges = [
+            ('a', 'b'),
+            ('b', 'c'),
+            ('b', 'f'),
+            ('b', 'e'),
+            ('c', 'd'),
+            ('c', 'g'),
+            ('d', 'c'),
+            ('d', 'h'),
+            ('e', 'a'),
+            ('e', 'f'),
+            ('f', 'g'),
+            ('g', 'f'),
+            ('h', 'd'),
+            ('h', 'g'),
+        ];
+
+        let scc = scc_kosaraju(
+            'a'..='g',
+            |v| {
+                edges
+                    .iter()
+                    .filter(move |&&(_, dst)| v == dst)
+                    .map(|&(src, _)| src)
+            },
+            |u| {
+                edges
+                    .iter()
+                    .filter(move |&&(src, _)| u == src)
+                    .map(|&(_, dst)| dst)
+            },
+        );
+
+        assert_ne!(scc[&'a'], scc[&'c']);
+        assert_ne!(scc[&'a'], scc[&'f']);
+        assert_ne!(scc[&'c'], scc[&'f']);
+
+        assert_eq!(scc[&'a'], scc[&'b']);
+        assert_eq!(scc[&'a'], scc[&'e']);
+
+        assert_eq!(scc[&'c'], scc[&'d']);
+        assert_eq!(scc[&'c'], scc[&'h']);
+
+        assert_eq!(scc[&'f'], scc[&'g']);
     }
 }
