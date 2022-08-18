8bc5b07fbb24351469a2137b418ce05944be1cf4
Shadaj Laddad <shadaj@users.noreply.github.com>
2022-08-18 14:54:39 -0700

    Initial graph creation logic

--- datalog_compiler/src/lib.rs
+++ datalog_compiler/src/lib.rs
@@ -1,5 +1,10 @@
-use quote::ToTokens;
-use syn::parse_quote;
+use hydroflow_lang::{
+    graph::flat_graph::FlatGraph,
+    parse::{ArrowConnector, NamedHfStatement, Operator, Pipeline, PipelineLink},
+};
+use proc_macro2::{Span, TokenStream};
+use quote::{quote, ToTokens};
+use syn::{parse_quote, token::Paren, Token};
 
 #[rust_sitter::grammar("datalog")]
 #[allow(dead_code)]
@@ -7,19 +12,19 @@ mod datalog_grammar {
     #[rust_sitter::language]
     #[derive(Debug)]
     pub struct Program {
-        rules: Vec<Declaration>,
+        pub rules: Vec<Declaration>,
     }
 
-    #[derive(Debug)]
+    #[derive(Debug, Clone)]
     pub enum Declaration {
         Input(#[rust_sitter::leaf(text = ".input")] (), Ident),
         Output(#[rust_sitter::leaf(text = ".output")] (), Ident),
         Rule(Rule),
     }
 
-    #[derive(Debug)]
+    #[derive(Debug, Clone)]
     pub struct Rule {
-        target: Target,
+        pub target: Target,
         #[rust_sitter::leaf(text = ":-")]
         _from: (),
         #[rust_sitter::repeat(non_empty = true)]
@@ -27,14 +32,14 @@ mod datalog_grammar {
             #[rust_sitter::leaf(text = ",")]
             ()
         )]
-        sources: Vec<Target>,
+        pub sources: Vec<Target>,
         #[rust_sitter::leaf(text = ".")]
         _dot: Option<()>,
     }
 
-    #[derive(Debug)]
+    #[derive(Debug, Clone)]
     pub struct Target {
-        name: Ident,
+        pub name: Ident,
         #[rust_sitter::leaf(text = "(")]
         _l_paren: (),
         #[rust_sitter::delimited(
@@ -46,7 +51,7 @@ mod datalog_grammar {
         _r_paren: (),
     }
 
-    #[derive(Debug)]
+    #[derive(Debug, Clone)]
     pub struct Ident {
         #[rust_sitter::leaf(pattern = r"[a-zA-Z_][a-zA-Z0-9_]*", transform = |s| s.to_string())]
         pub name: String,
@@ -59,15 +64,109 @@ mod datalog_grammar {
     }
 }
 
-fn gen_datalog_program(literal: proc_macro2::Literal) -> syn::Stmt {
+use datalog_grammar::*;
+
+fn gen_datalog_program(literal: proc_macro2::Literal, root: TokenStream) -> syn::Stmt {
     let str_node: syn::LitStr = parse_quote!(#literal);
     let actual_str = str_node.value();
     let program = datalog_grammar::parse(&actual_str).unwrap();
-    let program_tree = format!("{:?}", program);
+
+    let inputs = program
+        .rules
+        .iter()
+        .filter_map(|decl| match decl {
+            Declaration::Input(_, ident) => Some(ident.clone()),
+            _ => None,
+        })
+        .collect::<Vec<_>>();
+
+    let outputs = program
+        .rules
+        .iter()
+        .filter_map(|decl| match decl {
+            Declaration::Output(_, ident) => Some(ident.clone()),
+            _ => None,
+        })
+        .collect::<Vec<_>>();
+
+    let rules = program
+        .rules
+        .iter()
+        .filter_map(|decl| match decl {
+            Declaration::Rule(rule) => Some(rule.clone()),
+            _ => None,
+        })
+        .collect::<Vec<_>>();
+
+    let mut flat_graph = FlatGraph::default();
+
+    for target in inputs {
+        let target_ident = syn::Ident::new(&format!("{}_recv", &target.name), Span::call_site());
+        flat_graph.add_statement(hydroflow_lang::parse::HfStatement::Named(
+            NamedHfStatement {
+                name: syn::Ident::new(&target.name, Span::call_site()),
+                equals: Token![=](Span::call_site()),
+                pipeline: Pipeline::Operator(Operator {
+                    path: parse_quote!(recv_stream),
+                    paren_token: Paren::default(),
+                    args: vec![parse_quote!(#target_ident)]
+                        .iter()
+                        .cloned::<syn::Expr>()
+                        .collect(),
+                }),
+            },
+        ));
+    }
+
+    for target in outputs {
+        flat_graph.add_statement(hydroflow_lang::parse::HfStatement::Named(
+            NamedHfStatement {
+                name: syn::Ident::new(&target.name, Span::call_site()),
+                equals: Token![=](Span::call_site()),
+                pipeline: Pipeline::Operator(Operator {
+                    path: parse_quote!(for_each),
+                    paren_token: Paren::default(),
+                    args: vec![parse_quote!(|v| println!("{:?}", v))]
+                        .iter()
+                        .cloned::<syn::Expr>()
+                        .collect(),
+                }),
+            },
+        ));
+    }
+
+    for rule in rules {
+        let target = rule.target.name;
+        let target_ident = syn::Ident::new(&target.name, Span::call_site());
+        let sources: Vec<Ident> = rule
+            .sources
+            .iter()
+            .map(|source| source.name.clone())
+            .collect();
+
+        // TODO(shadaj): multiple sources
+        flat_graph.add_statement(hydroflow_lang::parse::HfStatement::Pipeline(
+            Pipeline::Link(PipelineLink {
+                lhs: Box::new(Pipeline::Name(syn::Ident::new(
+                    &sources[0].name,
+                    Span::call_site(),
+                ))),
+                connector: ArrowConnector {
+                    src: None,
+                    arrow: Token![->](Span::call_site()),
+                    dst: None,
+                },
+                rhs: Box::new(Pipeline::Name(target_ident)),
+            }),
+        ));
+    }
+
+    dbg!(flat_graph.surface_syntax_string());
+
+    let code_tokens = flat_graph.into_partitioned_graph().as_code(root);
 
     syn::parse_quote!({
-        let tree = #program_tree;
-        tree
+        #code_tokens
     })
 }
 
@@ -77,7 +176,18 @@ pub fn datalog(item: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let literal: proc_macro2::Literal = syn::parse_quote! {
         #item
     };
-    proc_macro::TokenStream::from(gen_datalog_program(literal).to_token_stream())
+
+    let hydroflow_crate = proc_macro_crate::crate_name("hydroflow")
+        .expect("hydroflow should be present in `Cargo.toml`");
+    let root = match hydroflow_crate {
+        proc_macro_crate::FoundCrate::Itself => quote! { hydroflow },
+        proc_macro_crate::FoundCrate::Name(name) => {
+            let ident = syn::Ident::new(&name, Span::call_site());
+            quote! { #ident }
+        }
+    };
+
+    proc_macro::TokenStream::from(gen_datalog_program(literal, root).to_token_stream())
 }
 
 #[cfg(test)]
@@ -117,15 +227,17 @@ mod tests {
 
     #[test]
     fn minimal_program() {
-        let out = &gen_datalog_program(parse_quote!(
-            r#"
-            .input edge
-            .output path
+        let out = &gen_datalog_program(
+            parse_quote!(
+                r#"
+            .input in
+            .output out
 
-            path(x, y) :- edge(x, y)
-            path(x, y) :- path(x, z), edge(z, y).
+            out(x, y) :- in(x, y).
         "#
-        ));
+            ),
+            quote::quote! { hydroflow },
+        );
 
         let wrapped: syn::Item = parse_quote! {
             fn main() {
--- hydroflow/tests/datalog_frontend.rs
+++ hydroflow/tests/datalog_frontend.rs
@@ -2,11 +2,17 @@ use datalog_compiler::datalog;
 
 #[test]
 pub fn test_simple() {
-  let flow = datalog!(r#"
-    .input edge
-    .output path
+    let (in_send, in_recv) = tokio::sync::mpsc::unbounded_channel::<(usize, usize)>();
 
-    path(x, y) :- edge(x, y)
-    path(x, y) :- path(x, z), edge(z, y).
-  "#);
+    let mut flow = datalog!(
+        r#"
+    .input in
+    .output out
+
+    out(x, y) :- in(x, y).
+  "#
+    );
+
+    in_send.send((1, 2)).unwrap();
+    flow.run_available();
 }
