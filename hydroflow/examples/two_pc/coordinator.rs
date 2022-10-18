04e9a699b103220b484544bda808558e8ae8552d
Joe Hellerstein <jmh@berkeley.edu>
2022-10-17 20:48:05 -0700

    hydroflow for 2PC

---
+++ hydroflow/examples/two_pc/coordinator.rs
@@ -0,0 +1,101 @@
+use crate::helpers::{
+    deserialize_msg, is_ackp2_msg, is_subord_abort, is_subord_commit, is_vote, parse_out,
+    serialize_msg,
+};
+use crate::protocol::{CoordMsg, MsgType, SubordResponse};
+use crate::{GraphType, Opts};
+use hydroflow::hydroflow_syntax;
+use hydroflow::scheduled::graph::Hydroflow;
+use std::collections::HashMap;
+use std::net::SocketAddr;
+use tokio::io::AsyncBufReadExt;
+use tokio::net::UdpSocket;
+use tokio_stream::wrappers::LinesStream;
+
+pub(crate) async fn run_coordinator(opts: Opts, subordinates: Vec<String>) {
+    // setup message send/recv ports
+    let server_socket = UdpSocket::bind(("127.0.0.1", opts.port)).await.unwrap();
+    let (outbound, inbound) = hydroflow::util::udp_lines(server_socket);
+
+    // We provide a command line for users to type a Transaction ID (integer) to commit.
+    let reader = tokio::io::BufReader::new(tokio::io::stdin());
+    let stdin_lines = LinesStream::new(reader.lines());
+
+    let mut df: Hydroflow = hydroflow_syntax! {
+        // fetch subordinates from file, convert ip:port to a SocketAddr, and tee
+        subords = recv_iter(subordinates)
+            -> map(|s| s.parse::<SocketAddr>().unwrap())
+            -> tee();
+
+        // set up channels
+        outbound_chan = tee();
+        outbound_chan[0] -> map(|(m,a)| (serialize_msg(m), a)) -> sink_async(outbound);
+        inbound_chan = recv_stream(inbound) -> map(deserialize_msg) -> tee();
+        votes_chan = inbound_chan[0] -> filter_map(is_vote) -> tee();
+        p2_ack_chan = inbound_chan[1] -> filter_map(is_ackp2_msg);
+
+        // we log all messages (in this prototype we just print)
+        inbound_chan[3] -> for_each(|m| println!("Received {:?}", m));
+        outbound_chan[1] -> for_each(|(m, a)| println!("Sending {:?} to {:?}", m, a));
+
+        // setup broadcast channel to all subords
+        broadcast_join = cross_join() -> outbound_chan;
+        broadcast = merge() -> [0]broadcast_join;
+        subords[1] -> [1]broadcast_join;
+        subords[2] -> for_each(|s| println!("Subordinate: {:?}", s));
+
+        // Phase 1 initiate:
+        // Given a transaction commit request from stdio, broadcast a Prepare to subordinates
+        recv_stream(stdin_lines)
+            -> filter_map(|l: Result<std::string::String, std::io::Error>| parse_out(l.unwrap()))
+            -> map(|xid| CoordMsg{xid: xid, mtype: MsgType::Prepare})
+            -> [0]broadcast;
+
+        // Phase 1 responses:
+        // as soon as we get an abort message for P1, we start Phase 2 with Abort.
+        // We'll respond to each abort message: this is redundant but correct (and monotone)
+        abort_vote = votes_chan[1] -> filter_map(is_subord_abort)
+            -> map(|m| CoordMsg{xid: m.xid, mtype: MsgType::Abort})
+            -> [1]broadcast;
+
+        // count commit votes
+        commit_votes = votes_chan[2] -> filter_map(is_subord_commit)
+            -> fold(HashMap::new(), |mut ht, m:SubordResponse| {
+                    let e = ht.entry(m.xid).or_insert(0);
+                    *e += 1;
+                    ht})
+            -> flat_map(|ht| ht)
+            -> map(|(xid, c)| (xid, c));
+
+        // count subordinates
+        subord_total = subords[0] -> fold(0, |a,_b| a+1); // -> for_each(|n| println!("There are {} subordinates.", n));
+
+        // If commit_votes for this xid is the same as all_votes, send a P2 Commit message
+        committed = join() -> map(|(_c, (xid, ()))| xid);
+        commit_votes -> map(|(xid, c)| (c, xid)) -> [0]committed;
+        subord_total -> map(|c| (c, ())) -> [1]committed;
+        committed -> map(|xid| CoordMsg{xid: xid, mtype: MsgType::Commit}) -> [2]broadcast;
+
+        // Handle p2 acknowledgments by sending an End message
+        p2_ack_chan -> map(|m:SubordResponse| CoordMsg{xid: m.xid, mtype: MsgType::End,})
+                    -> [3]broadcast;
+
+        // Handler for ended acknowledgments not necessary; we just print them
+    };
+
+    if let Some(graph) = opts.graph {
+        match graph {
+            GraphType::Mermaid => {
+                println!("{}", df.generate_mermaid())
+            }
+            GraphType::Dot => {
+                println!("{}", df.generate_dot())
+            }
+            GraphType::Json => {
+                println!("{}", df.generate_json())
+            }
+        }
+    }
+
+    df.run_async().await.unwrap();
+}
---
+++ hydroflow/examples/two_pc/subordinate.rs
@@ -0,0 +1,77 @@
+use crate::helpers::{
+    decide, deserialize_msg, is_coord_p2, is_end_msg, is_prepare_msg, serialize_msg,
+};
+use crate::protocol::{CoordMsg, MsgType, SubordResponse};
+use crate::{GraphType, Opts};
+use hydroflow::hydroflow_syntax;
+use hydroflow::scheduled::graph::Hydroflow;
+use std::net::SocketAddr;
+use tokio::net::UdpSocket;
+
+pub(crate) async fn run_subordinate(opts: Opts, coordinator: String) {
+    // setup message send/recv ports
+    let socket = UdpSocket::bind(("127.0.0.1", opts.port)).await.unwrap();
+    let my_addr = socket.local_addr().unwrap();
+    let (outbound, inbound) = hydroflow::util::udp_lines(socket);
+    println!("Coordinator: {}", coordinator);
+    let server_addr = coordinator.trim().parse::<SocketAddr>().unwrap();
+
+    let mut df: Hydroflow = hydroflow_syntax! {
+         // set up channels
+        outbound_chan = merge() -> tee();
+        outbound_chan[0] -> map(|(m,a)| (serialize_msg(m), a)) -> sink_async(outbound);
+        inbound_chan = recv_stream(inbound) -> map(deserialize_msg) -> tee();
+        prepare_chan = inbound_chan[0] -> filter_map(is_prepare_msg);
+        p2_chan = inbound_chan[1] -> filter_map(is_coord_p2);
+        end_chan = inbound_chan[2] -> filter_map(is_end_msg);
+
+        // we log all messages (in this prototype we just print)
+        inbound_chan[3] -> for_each(|m| println!("Received {:?}", m));
+        outbound_chan[1] -> for_each(|m| println!("Sending {:?}", m));
+
+
+        // handle p1 message: choose vote and respond
+        // in this prototype we choose randomly whether to abort via decide()
+        report_chan = prepare_chan -> map(|m: CoordMsg| (
+            SubordResponse{
+                xid: m.xid,
+                addr: my_addr.to_string(),
+                mtype: if decide(67) {MsgType::Commit} else {MsgType::Abort}
+            },
+            server_addr));
+        report_chan -> [0]outbound_chan;
+
+        // handle p2 message: acknowledge (and print)
+        p2_response = map(|(m, t)| (SubordResponse{
+            xid: m.xid,
+            addr: my_addr.to_string(),
+            mtype: t,
+        }, server_addr)) -> [1]outbound_chan;
+        p2_chan -> map(|m:CoordMsg| (m, MsgType::AckP2)) -> p2_response;
+
+        // handle end message: acknowledge (and print)
+        end_chan -> map(|m:CoordMsg| (SubordResponse{
+            xid: m.xid,
+            addr: my_addr.to_string(),
+            mtype: MsgType::Ended,
+        }, server_addr)) -> [2]outbound_chan;
+
+
+    };
+
+    if let Some(graph) = opts.graph {
+        match graph {
+            GraphType::Mermaid => {
+                println!("{}", df.generate_mermaid())
+            }
+            GraphType::Dot => {
+                println!("{}", df.generate_dot())
+            }
+            GraphType::Json => {
+                println!("{}", df.generate_json())
+            }
+        }
+    }
+
+    df.run_async().await.unwrap();
+}
