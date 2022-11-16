2230af8282ed9ddeb2a2e502a6e4737b9d0a2b0d
Alex Rasmussen <535829+alexras@users.noreply.github.com>
2022-11-16 08:36:30 -1000

    Add a driver for the chat example (#237)
    Clients can take messages in on stdin from any process, but in order to drive them autonomously we
    need a reliable and continuous source of input that's less spammy than commands like 'yes'. This
    diff adds a chat driver that spits out a random phrase every few seconds to serve as this driver.

---
+++ hydroflow/examples/chat/chat_driver.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+import argparse
+import random
+import time
+
+
+def chat_driver(
+        word_source: str,
+        delay_seconds: float,
+        jitter_millis: int,
+        phrase_length: int
+):
+    with open(word_source, 'r') as fp:
+        words = fp.readlines()
+
+    while True:
+        phrase = ' '.join([
+            x.strip()
+            for x in random.choices(words, k=phrase_length)
+        ])
+
+        print(phrase, flush=True)
+
+        sleep_time_seconds = delay_seconds + \
+            (random.randrange(-1 * jitter_millis, jitter_millis) / 1000.0)
+
+        time.sleep(sleep_time_seconds)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="A source of random phrases to pipe into chat "
+        "clients' stdin"
+    )
+
+    parser.add_argument(
+        '--word_source',
+        '-w',
+        help='where to get words from (default: %(default)s)',
+        default='/usr/share/dict/words'
+    )
+
+    parser.add_argument(
+        '--phrase_length',
+        '-l',
+        type=int,
+        help='phrase length in words',
+        default=5
+    )
+
+    parser.add_argument(
+        '--delay_seconds',
+        '-d',
+        type=float,
+        help='delay between phrases in seconds (default: %(default)s seconds)',
+        default=2.0
+    )
+
+    parser.add_argument(
+        '--jitter_millis',
+        '-j',
+        type=int,
+        help='maximum time jitter between phrases in milliseconds '
+        '(default: %(default)s milliseconds)',
+        default=50
+    )
+
+    args = parser.parse_args()
+
+    chat_driver(**vars(args))
