18c0bc81c155aa8ee35d4af77b21d068fa44079b
Mingwei Samuel <mingwei.samuel@gmail.com>
2022-11-23 10:51:22 -0800

    Limit `null()` to up to one input and/or output.
    In principle `null()` should be able to handle any combination of inputs
    and outputs, but in reality this makes it a huge edge case and causes
    lots of problems.... For now it is easier to limit null to [0,1] inputs
    and outputs (3 non-degenerate combos).
    
    * Fixes #243
    * Fixes #244

--- hydroflow/tests/compile-fail/surface_degenerate_null.rs
+++ hydroflow/tests/compile-fail/surface_degenerate_null.rs
@@ -1,6 +1,6 @@
 use hydroflow::hydroflow_syntax;
 
-/// I suppose technically this should/could compile, but it is a completely useless edge case.
+/// Technically this should/could compile, but it is a completely useless edge case.
 fn main() {
     let mut df = hydroflow_syntax! {
         null();
