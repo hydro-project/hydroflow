9d9a7ec4186b54f2fdb5ec07fe4f364618745258
Joe Hellerstein <jmh@berkeley.edu>
2022-12-06 14:01:08 -0800

    Echo Server example (#279)
    * echo server example
    
    * echoserver
    
    * formatting

---
+++ hydroflow/examples/echoserver/main.rs
@@ -0,0 +1,51 @@
+use clap::{ArgEnum, Parser};
+use client::run_client;
+use hydroflow::tokio;
+use server::run_server;
+
+mod client;
+mod helpers;
+mod protocol;
+mod server;
+
+#[derive(Clone, ArgEnum, Debug)]
+enum Role {
+    Client,
+    Server,
+}
+#[derive(Clone, ArgEnum, Debug)]
+enum GraphType {
+    Mermaid,
+    Dot,
+    Json,
+}
+
+#[derive(Parser, Debug)]
+struct Opts {
+    #[clap(arg_enum, long)]
+    role: Role,
+    #[clap(long)]
+    port: u16,
+    #[clap(long)]
+    addr: String,
+    #[clap(long)]
+    server_addr: Option<String>,
+    #[clap(long)]
+    server_port: Option<u16>,
+    #[clap(arg_enum, long)]
+    graph: Option<GraphType>,
+}
+
+#[tokio::main]
+async fn main() {
+    let opts = Opts::parse();
+
+    match opts.role {
+        Role::Client => {
+            run_client(opts).await;
+        }
+        Role::Server => {
+            run_server(opts).await;
+        }
+    }
+}
