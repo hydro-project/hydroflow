b09ecf9b0079a588475f3f1058da42fd6042e71b
Joe Hellerstein <jmh@berkeley.edu>
2022-12-07 00:03:06 -0800

    simplify boilerplate with new helpers, ops for net and stdin

--- hydroflow/examples/echoserver/main.rs
+++ hydroflow/examples/echoserver/main.rs
@@ -1,7 +1,9 @@
+#![feature(addr_parse_ascii)]
 use clap::{ArgEnum, Parser};
 use client::run_client;
 use hydroflow::tokio;
 use server::run_server;
+use std::net::{SocketAddr, ToSocketAddrs};
 
 mod client;
 mod helpers;
@@ -25,27 +27,40 @@ struct Opts {
     #[clap(arg_enum, long)]
     role: Role,
     #[clap(long)]
-    port: u16,
+    addr: Option<String>,
     #[clap(long)]
-    addr: String,
-    #[clap(long)]
-    server_addr: Option<String>,
-    #[clap(long)]
-    server_port: Option<u16>,
+    server_addr: String,
     #[clap(arg_enum, long)]
     graph: Option<GraphType>,
+}
+
+pub fn resolve_ipv4_connection_addr(server_addr: String) -> Option<SocketAddr> {
+    let mut addrs = server_addr.to_socket_addrs().unwrap();
+
+    addrs.find(|addr| addr.is_ipv4())
 }
 
 #[tokio::main]
 async fn main() {
     let opts = Opts::parse();
 
+    // get server_addr
+    println!("Server address: {}", opts.server_addr);
+    let server_addr = resolve_ipv4_connection_addr(opts.server_addr.clone()).unwrap_or_else(|| {
+        println!("Error parsing server address");
+        panic!();
+    });
     match opts.role {
         Role::Client => {
-            run_client(opts).await;
+            let client_addr = resolve_ipv4_connection_addr(opts.addr.clone().unwrap())
+                .unwrap_or_else(|| {
+                    println!("Error parsing server address");
+                    panic!();
+                });
+            run_client(opts, server_addr, client_addr).await;
         }
         Role::Server => {
-            run_server(opts).await;
+            run_server(opts, server_addr).await;
         }
     }
 }
---
+++ hydroflow_lang/src/graph/ops/sink_udp.rs
@@ -0,0 +1,78 @@
+use super::{
+    OperatorConstraints, OperatorWriteOutput, WriteContextArgs, WriteIteratorArgs, RANGE_0, RANGE_1,
+};
+
+use quote::quote_spanned;
+
+/// > Arguments: An [async `Sink`](https://docs.rs/futures/latest/futures/sink/trait.Sink.html).
+///
+/// Consumes items by sending them to an [async `Sink`](https://docs.rs/futures/latest/futures/sink/trait.Sink.html).
+///
+/// Note this operator must be used within a Tokio runtime.
+#[hydroflow_internalmacro::operator_docgen]
+pub const SINK_UDP: OperatorConstraints = OperatorConstraints {
+    name: "sink_udp",
+    hard_range_inn: RANGE_1,
+    soft_range_inn: RANGE_1,
+    hard_range_out: RANGE_0,
+    soft_range_out: RANGE_0,
+    ports_inn: None,
+    ports_out: None,
+    num_args: 1,
+    input_delaytype_fn: &|_| None,
+    write_fn: &(|wc @ &WriteContextArgs { root, op_span, .. },
+                 &WriteIteratorArgs {
+                     ident, arguments, ..
+                 },
+                 _| {
+        let port = &arguments[0];
+
+        let send_ident = wc.make_ident("item_send");
+        let recv_ident = wc.make_ident("item_recv");
+        let sink_ident = wc.make_ident("item_sink");
+
+        let write_prologue = quote_spanned! {op_span=>
+            let #sink_ident =
+            {
+                use std::net::ToSocketAddrs;
+                let mut addrs = format!("127.0.0.1:{}", #port)
+                    .to_socket_addrs()
+                    .unwrap();
+                let addr = addrs.find(|addr| addr.is_ipv4()).expect("Unable to resolve connection address");
+                let socket = UdpSocket::bind(addr).await.unwrap();
+                let (outbound, _inbound) = hydroflow::util::udp_lines(socket);
+                outbound
+            };
+            // TODO: Replace the code below with an invocation of sink_async()
+            let (#send_ident, #recv_ident) = #root::tokio::sync::mpsc::unbounded_channel();
+            df
+                .spawn_task(async move {
+                    use #root::futures::sink::SinkExt;
+
+                    let mut recv = #recv_ident;
+                    let mut sink = #sink_ident;
+                    while let Some(item) = recv.recv().await {
+                        sink.feed(item).await.expect("Error processing async sink item.");
+                        // Receive as many items synchronously as possible before flushing.
+                        while let Ok(item) = recv.try_recv() {
+                            sink.feed(item).await.expect("Error processing async sink item.");
+                        }
+                        sink.flush().await.expect("Failed to flush async sink.");
+                    }
+                })
+                .expect("sink_async() must be used within a tokio runtime");
+        };
+
+        let write_iterator = quote_spanned! {op_span=>
+            let #ident = #root::pusherator::for_each::ForEach::new(|item| {
+                #send_ident.send(item).expect("Failed to send async write item for processing.");
+            });
+        };
+
+        Ok(OperatorWriteOutput {
+            write_prologue,
+            write_iterator,
+            ..Default::default()
+        })
+    }),
+};
