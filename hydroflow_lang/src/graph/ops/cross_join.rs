00564171f9907ef74fdd518ff7927b7eee545f9b
Mingwei Samuel <mingwei.samuel@gmail.com>
2023-01-05 12:46:11 -0800

    Implement `'tick`/`'static` lifetimes for `cross_join` #272
    Managed to find a nice DRY way

--- hydroflow_lang/src/graph/ops/cross_join.rs
+++ hydroflow_lang/src/graph/ops/cross_join.rs
@@ -1,13 +1,12 @@
-use super::{
-    OperatorConstraints, OperatorWriteOutput, WriteContextArgs, WriteIteratorArgs, RANGE_1,
-};
+use super::{OperatorConstraints, WriteContextArgs, WriteIteratorArgs, RANGE_1};
 
 use quote::quote_spanned;
 use syn::parse_quote;
 
 /// > 2 input streams of type S and T, 1 output stream of type (S, T)
 ///
-/// Forms the cross-join (Cartesian Product) of the items in the input streams, returning all tupled pairs.
+/// Forms the cross-join (Cartesian product) of the items in the input streams, returning all
+/// tupled pairs.
 ///
 /// ```hydroflow
 /// // should print all 4 pairs of emotion and animal
@@ -16,6 +15,25 @@ use syn::parse_quote;
 /// source_iter(vec!["dog", "cat"]) -> [1]my_join;
 /// my_join -> for_each(|(v1, v2)| println!("({}, {})", v1, v2));
 /// ```
+///
+/// `cross_join` can also be provided with one or two generic lifetime persistence arguments
+/// in the same was as [`join`](#join), see [`join`'s documentation](#join) for more info.
+///
+/// ```rustbook
+/// let (input_send, input_recv) = hydroflow::util::unbounded_channel::<&str>();
+/// let mut flow = hydroflow::hydroflow_syntax! {
+///     my_join = cross_join::<'tick>();
+///     source_iter(["hello", "bye"]) -> [0]my_join;
+///     source_stream(input_recv) -> [1]my_join;
+///     my_join -> for_each(|(s, t)| println!("({}, {})", s, t));
+/// };
+/// input_send.send("oakland").unwrap();
+/// flow.run_tick();
+/// input_send.send("san francisco").unwrap();
+/// flow.run_tick();
+/// ```
+/// Prints only `"(hello, oakland)"` and `"(bye, oakland)"`. The `source_iter` is only included in
+/// the first tick, then forgotten.
 #[hydroflow_internalmacro::operator_docgen]
 pub const CROSS_JOIN: OperatorConstraints = OperatorConstraints {
     name: "cross_join",
@@ -27,27 +45,21 @@ pub const CROSS_JOIN: OperatorConstraints = OperatorConstraints {
     ports_out: None,
     num_args: 0,
     input_delaytype_fn: &|_| None,
-    write_fn: &(|wc @ &WriteContextArgs { root, op_span, .. },
-                 &WriteIteratorArgs { ident, inputs, .. },
-                 _| {
-        let joindata_ident = wc.make_ident("joindata");
-        let write_prologue = quote_spanned! {op_span=>
-            let mut #joindata_ident = Default::default();
-        };
+    write_fn: &(|wc @ &WriteContextArgs { op_span, .. },
+                 wi @ &WriteIteratorArgs { ident, inputs, .. },
+                 diagnostics| {
+        let mut output = (super::join::JOIN.write_fn)(wc, wi, diagnostics)?;
 
         let lhs = &inputs[0];
         let rhs = &inputs[1];
-        let write_iterator = quote_spanned! {op_span=>
+        let write_iterator = output.write_iterator;
+        output.write_iterator = quote_spanned!(op_span=>
             let #lhs = #lhs.map(|a| ((), a));
             let #rhs = #rhs.map(|b| ((), b));
-            let #ident = #root::compiled::pull::SymmetricHashJoin::new(#lhs, #rhs, &mut #joindata_ident);
+            #write_iterator
             let #ident = #ident.map(|((), (a, b))| (a, b));
-        };
+        );
 
-        Ok(OperatorWriteOutput {
-            write_prologue,
-            write_iterator,
-            ..Default::default()
-        })
+        Ok(output)
     }),
 };
