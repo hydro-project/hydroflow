53538dccecfc8195d9092164d5453c4f90023049
Joe Hellerstein <jmh@berkeley.edu>
2023-01-12 19:45:48 -0800

    text crdt example: RGA (#344)
    * rga example(s)
    
    * clean up README for rga
    
    * Update README.md formatting
    
    fix code quoting
    
    * remove commented out queue code
    
    * clippy getting stricter

---
+++ hydroflow/examples/rga/datalog.rs
@@ -0,0 +1,87 @@
+use crate::protocol::{Timestamp, Token};
+use hydroflow::{hydroflow_syntax, scheduled::graph::Hydroflow};
+
+use tokio::sync::mpsc::UnboundedSender;
+use tokio_stream::wrappers::UnboundedReceiverStream;
+
+pub(crate) fn rga_datalog(
+    input_recv: UnboundedReceiverStream<(Token, Timestamp)>,
+    rga_send: UnboundedSender<(Token, Timestamp)>,
+    list_send: UnboundedSender<(Timestamp, Timestamp)>,
+) -> Hydroflow {
+    hydroflow_syntax! {
+        edges = source_stream(input_recv) -> tee();
+        insertAfter = edges -> map(|(c, p): (Token, Timestamp) | (c.ts, p)) -> tee();
+
+        // isListElem(Oid) :- insertAfter(Oid, Parent)
+        isListElem = insertAfter -> map(|(oid, _parent)| oid);
+
+        // hasChild(Parent) :- insertAfter(Child, Parent)
+        hasChild = insertAfter -> map(|(_child, parent)| parent);
+
+        // laterChild(Parent, Child2) :- insertAfter( Child1, Parent), insertAfter( Child2, Parent), Child1 > Child2
+        laterChild = join() -> filter_map(|(parent, (child1, child2))| if child1 > child2 {Some((parent, child2))} else {None} );
+        insertAfter -> map(|(child, parent)| (parent, child)) -> [0]laterChild;
+        insertAfter -> map(|(child, parent)| (parent, child)) -> [1]laterChild;
+
+        // firstChild(Parent, Child) :- insertAfter(Child, Parent), !laterChild (Parent, Child)
+        firstChild = difference();
+        insertAfter -> map(|(child, parent)| (parent, child)) -> [pos]firstChild;
+        laterChild -> [neg]firstChild;
+
+        // sibling (Child1, Child2) :- insertAfter(Child1,Parent), insertAfter(Child2, Parent)
+        sibling = join() -> map(|(_p, (c1, c2))| (c1, c2)) -> tee();
+        insertAfter -> map(|(c, p)| (p, c)) -> [0]sibling;
+        insertAfter -> map(|(c, p)| (p, c)) -> [1]sibling;
+
+        // laterSibling (Sib1, Sib2) :- sibling (Sibl,Sib2), Sibl > Sib2
+        laterSibling = sibling -> filter(|(s1, s2)| s1 > s2) -> tee();
+
+        // laterSibling2(SibI, Sib3) :- sibling(Sibl,Sib2), sibling (Sibl,Sib3), Sibl> Sib2, Sib2 > Sib3
+        laterSibling2 = join() -> filter_map(|(s1, (s2, s3))| if s1 > s2 && s2 > s3 {Some((s1, s3))} else {None});
+        sibling -> [0]laterSibling2;
+        sibling -> [1]laterSibling2;
+
+        // nextSibling (Sib1,Sib2) :- laterSibling (Sibl, Sib2), - laterSibling2(Sibl, Sib2)
+        nextSibling = difference();
+        laterSibling -> [pos]nextSibling;
+        laterSibling2 -> [neg]nextSibling;
+
+        // hasNextSibling(Sib1) :- laterSibling (Sibl, Sib2)
+        hasNextSibling = laterSibling -> map(|(s1, _s2)| s1);
+
+        // nextSiblingAnc(Start, Next) :- nextSibling (Start, Next)
+        nextSiblingAnc = merge() -> tee();
+        nextSibling -> nextSiblingAnc;
+
+        // nextSiblingAnc (Start, Next) :- !hasNextSibling (Start), insertAfter(Start, Parent), nextSiblingAnc( Parent, Next)
+        nsa_diff = difference();
+        hasNextSibling -> [neg]nsa_diff;
+        insertAfter -> map(|(start, _parent)| start) -> [pos]nsa_diff;
+        nsa_join1 = join() -> map(|(start, (_, parent))| (parent, start));
+        nsa_diff -> map(|start| (start, ())) -> [0]nsa_join1;
+        insertAfter -> [1]nsa_join1;
+        nsa_join2 = join() -> map(|(_parent, (start, next))| (start, next)) -> nextSiblingAnc;
+        nsa_join1 -> [0]nsa_join2;
+        nsa = nextSiblingAnc -> [1]nsa_join2;
+
+        // hasSiblingAnc (Start) :- nextSiblingAnc( Start, Next)
+        hasSiblingAnc = nextSiblingAnc -> map(|(start, _next)| start) -> null();
+
+        // nextElem(Prev,Next) :- firstChild(Prev, Next)
+        nextElem = merge();
+        firstChild -> nextElem;
+
+        // nextElem(Prev,Next) :- isListElem(Prev), - hasChild (Prev), nextSiblingAnc(Prev,Next)
+        ne_diff = difference();
+        isListElem -> [pos]ne_diff;
+        hasChild -> [neg]ne_diff;
+        ne_join = join() -> map(|(prev, (_, next))| (prev, next));
+        ne_diff -> map(|prev| (prev, ())) -> [0]ne_join;
+        nextSiblingAnc -> [1]ne_join;
+        ne_join -> nextElem;
+
+        edges -> for_each(|(c, p): (Token, Timestamp)| rga_send.send((c, p)).unwrap());
+        nextElem -> for_each(|(first, second)| list_send.send((first, second)).unwrap());
+    }
+}
---
+++ hydroflow/examples/rga/main.rs
@@ -0,0 +1,169 @@
+use clap::{Parser, ValueEnum};
+use hydroflow::util::collect_ready;
+use std::collections::{HashMap, HashSet};
+use tokio::sync::mpsc::UnboundedSender;
+use tokio_stream::wrappers::UnboundedReceiverStream;
+
+use adjacency::rga_adjacency;
+use datalog::rga_datalog;
+use datalog_agg::rga_datalog_agg;
+use minimal::rga_minimal;
+use protocol::{Timestamp, Token};
+
+mod adjacency;
+mod datalog;
+mod datalog_agg;
+mod minimal;
+mod protocol;
+
+#[derive(Parser, Debug, Clone, ValueEnum)]
+enum Implementation {
+    Datalog,
+    Adjacency,
+    DatalogAgg,
+    Minimal,
+}
+
+#[derive(Clone, ValueEnum, Debug)]
+enum GraphType {
+    Mermaid,
+    Dot,
+    Json,
+}
+
+#[derive(Parser, Debug)]
+struct Opts {
+    #[clap(value_enum, long = "impl", short)]
+    implementation: Option<Implementation>,
+    #[clap(value_enum, long, short)]
+    graph: Option<GraphType>,
+    #[clap(value_enum, long, short)]
+    output: String,
+}
+
+#[tokio::main]
+pub async fn main() {
+    // An edge in the input data = a pair of `Token` vertex IDs.
+    let (input_send, input_recv) = hydroflow::util::unbounded_channel::<(Token, Timestamp)>();
+    let (rga_send, mut rga_recv) = hydroflow::util::unbounded_channel::<(Token, Timestamp)>();
+    let (list_send, mut list_recv) = hydroflow::util::unbounded_channel::<(Timestamp, Timestamp)>();
+    let opts = Opts::parse();
+
+    let mut df = match opts.implementation {
+        Some(Implementation::Datalog) => rga_datalog(input_recv, rga_send, list_send),
+        Some(Implementation::Adjacency) => rga_adjacency(input_recv, rga_send, list_send),
+        Some(Implementation::Minimal) => rga_minimal(input_recv, rga_send, list_send),
+        Some(Implementation::DatalogAgg) => rga_datalog_agg(input_recv, rga_send, list_send),
+        None => rga_adjacency(input_recv, rga_send, list_send),
+    };
+
+    keystroke((1, 0, 'a'), (0, 0), &input_send).await;
+    keystroke((2, 0, 'b'), (1, 0), &input_send).await;
+    keystroke((3, 0, 'a'), (2, 0), &input_send).await;
+    keystroke((4, 0, 't'), (3, 0), &input_send).await;
+    keystroke((5, 0, 'e'), (4, 0), &input_send).await;
+
+    keystroke((6, 1, 'o'), (2, 0), &input_send).await;
+    keystroke((7, 1, 'r'), (6, 1), &input_send).await;
+    keystroke((8, 0, 'C'), (0, 0), &input_send).await;
+    keystroke((9, 0, 'o'), (8, 0), &input_send).await;
+    keystroke((10, 0, 'l'), (9, 0), &input_send).await;
+    keystroke((11, 0, 'l'), (10, 0), &input_send).await;
+
+    df.run_available();
+
+    let mut output = String::new();
+    write_to_dot(&mut rga_recv, &mut list_recv, &mut output);
+    std::fs::write(opts.output, output).expect("write to output file failed");
+
+    if let Some(graph) = opts.graph {
+        let serde_graph = df
+            .serde_graph()
+            .expect("No graph found, maybe failed to parse.");
+        match graph {
+            GraphType::Mermaid => {
+                println!("{}", serde_graph.to_mermaid());
+            }
+            GraphType::Dot => {
+                println!("{}", serde_graph.to_dot())
+            }
+            GraphType::Json => {
+                unimplemented!();
+            }
+        }
+    }
+}
+
+async fn keystroke(
+    (node_ts, node_id, c): (usize, usize, char),
+    (parent_ts, parent_id): (usize, usize),
+    input_send: &UnboundedSender<(Token, Timestamp)>,
+) {
+    input_send
+        .send((
+            Token {
+                ts: Timestamp { node_ts, node_id },
+                value: c,
+            },
+            Timestamp {
+                node_ts: parent_ts,
+                node_id: parent_id,
+            },
+        ))
+        .unwrap();
+}
+
+fn write_to_dot(
+    rga_recv: &mut UnboundedReceiverStream<(Token, Timestamp)>,
+    list_recv: &mut UnboundedReceiverStream<(Timestamp, Timestamp)>,
+    w: &mut impl std::fmt::Write,
+) {
+    let tree_edges: HashSet<_> = collect_ready(rga_recv);
+    let list_edges: HashMap<_, _> = collect_ready(list_recv);
+    let node_names = tree_edges
+        .iter()
+        .map(|(c, _)| (c.ts, c.value))
+        .collect::<HashMap<_, _>>();
+
+    // print RGA tree in dot format
+    writeln!(w, "digraph G {{\nrankdir = TB").unwrap();
+    writeln!(
+        w,
+        "ts0_0 [label=<root<SUB><FONT COLOR=\"red\" POINT-SIZE=\"8\">ts0_0</FONT></SUB>>]"
+    )
+    .unwrap();
+    for (c, p) in tree_edges {
+        writeln!(w, "{} -> {} [color=gray, constraint=true]", p, c.ts).unwrap();
+        writeln!(w, "{}", format_dot_node(c)).unwrap();
+    }
+    for (first, second) in list_edges.iter() {
+        writeln!(
+            w,
+            "{} -> {} [style=dashed, color=blue, constraint=false]",
+            first, second
+        )
+        .unwrap();
+    }
+
+    let mut x = Timestamp {
+        node_ts: 0,
+        node_id: 0,
+    };
+    let mut result = String::new();
+    while let Some(y) = list_edges.get(&x) {
+        result.push(*(node_names.get(y).unwrap()));
+        x = *y;
+    }
+    if result.is_empty() {
+        result = "Unknown".to_string()
+    };
+    writeln!(w, "label=<<FONT COLOR=\"blue\">{}</FONT>>", result).unwrap();
+    writeln!(w, "}}").unwrap();
+}
+
+fn format_dot_node(n: Token) -> String {
+    format!(
+        "{} [label=<{}<SUB><FONT COLOR=\"red\" POINT-SIZE=\"8\">{}</FONT></SUB>>]",
+        n.ts, n.value, n.ts
+    )
+}
