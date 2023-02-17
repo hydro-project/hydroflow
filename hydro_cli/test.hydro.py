52aa6e0e5d5417bc185cf8f1f961c5494b5b5129
Shadaj Laddad <shadaj@users.noreply.github.com>
2023-02-17 15:43:44 -0800

    Introduce initial Hydro CLI architecture (#390)
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * Parallel service init and stdout capture
    
    * Simplify Python interface
    
    * Polish
    
    * Update design doc for new deployment API
    
    * Capture stderr as well
    
    * Add API for getting exit code
    
    * Initial logic for configuring ports
    
    * Break out into several modules
    
    * Set up infrastructure for provisioning with Terraform

---
+++ hydro_cli/test.hydro.py
@@ -0,0 +1,29 @@
+import hydro
+
+async def main():
+    deployment = hydro.Deployment()
+    machine = deployment.Localhost()
+
+    program = deployment.HydroflowCrate(
+        src=".",
+        example="simple",
+        on=machine
+    )
+
+    program2 = deployment.HydroflowCrate(
+        src=".",
+        example="simple",
+        on=machine
+    )
+
+    program.ports.foo.send_to(program2.ports.bar)
+
+    await deployment.deploy()
+    
+    async for log in program.stdout():
+        print(log)
+
+    async for log in program2.stdout():
+        print(log)
+
+    print(program.exit_code())
