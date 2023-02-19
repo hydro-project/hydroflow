853062bca9355f3a9c3034d65d018fe47d315b89
Mingwei Samuel <mingwei.samuel@gmail.com>
2023-02-18 21:20:47 -0800

    placeholder commit for adding more tests

---
+++ hydroflow/tests/compile-fail/surface_forwardref_chain_double.rs
@@ -0,0 +1,22 @@
+use hydroflow::hydroflow_syntax;
+
+fn main() {
+    let mut df = hydroflow_syntax! {
+        a = b -> k;
+
+        b = c;
+        c = d;
+        d = e;
+        e = f;
+        f = g;
+        g = h;
+        h = i;
+        i = j;
+        j = a;
+
+        k = l;
+        l = m;
+        m = k;
+    };
+    df.run_available();
+}
