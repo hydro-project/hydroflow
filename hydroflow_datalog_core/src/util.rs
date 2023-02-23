def269c955a21df19f594516601cf09fc86e9c4d
Shadaj Laddad <shadaj@users.noreply.github.com>
2023-02-22 20:36:11 -0800

    Implement simple aggregations for Dedalus (#403)
    * Implement simple aggregations for Dedalus
    
    * Address feedback
    
    * Cleanup
    
    * Extract aggregation logic into separate function
    
    * Fix confusion between keys and aggs and add more tests
    
    * Even more tests
    
    * Add repeat_tuple utility

--- hydroflow_datalog_core/src/util.rs
+++ hydroflow_datalog_core/src/util.rs
@@ -1,3 +1,12 @@
 use std::ops::RangeFrom;
 
+use quote::ToTokens;
+use syn::parse_quote;
+
 pub type Counter = RangeFrom<isize>;
+
+pub fn repeat_tuple<I: ToTokens, T: syn::parse::Parse>(rep: fn() -> I, n: usize) -> T {
+    let values = (1..=n).map(|_| rep()).collect::<Vec<I>>();
+
+    parse_quote!((#(#values, )*))
+}
