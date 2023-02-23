160673b1778d6f02d3470392d8eb752b120b4948
Mingwei Samuel <mingwei.samuel@gmail.com>
2023-02-23 15:25:19 -0800

    Add more generics compile-fail tests

---
+++ hydroflow/tests/compile-fail/surface_demux_port_duplicate.rs
@@ -0,0 +1,18 @@
+use hydroflow::{hydroflow_syntax, var_args};
+
+fn main() {
+    let mut df = hydroflow_syntax! {
+        my_demux = source_iter(0..10) -> demux(|item, var_args!(a, b, c)| {
+            match item % 3 {
+                0 => a.give(item),
+                1 => b.give(item),
+                2 => c.give(item),
+            }
+        });
+        my_demux[a] -> for_each(std::mem::drop);
+        my_demux[b] -> for_each(std::mem::drop);
+        my_demux[c] -> for_each(std::mem::drop);
+        my_demux[c] -> for_each(std::mem::drop);
+    };
+    df.run_available();
+}
