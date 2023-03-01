5f56ec2fb3d887dceaff036d983c7c530734cc6a
Tyler Hou <t@tylerhou.com>
2023-03-01 12:48:36 -0800

    Remove errant dep info file (#423)
    missed in #414

--- hydroflow_internalmacro.d
+++
@@ -1,5 +0,0 @@
-hydroflow_internalmacro.d: hydroflow_internalmacro/src/lib.rs
-
-hydroflow_internalmacro/src/lib.rs:
-
-# env-dep:CARGO_MANIFEST_DIR=/Users/tylerhou/code/hydroflow/hydroflow_internalmacro
