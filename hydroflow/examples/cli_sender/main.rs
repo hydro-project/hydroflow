cca0aea9b625da030058497ad2e3181913055245
Shadaj Laddad <shadaj@users.noreply.github.com>
2023-03-07 00:56:39 -0800

    Extract common logic for establishing CLI-configured connections

--- hydroflow/examples/cli_sender/main.rs
+++ hydroflow/examples/cli_sender/main.rs
@@ -1,50 +1,16 @@
-use std::collections::HashMap;
-
-use hydroflow::{
-    hydroflow_syntax,
-    util::connection::{BindConfig, ConnectionPipe},
-};
+use hydroflow::{hydroflow_syntax, util::serialize_to_bytes};
 
 #[tokio::main]
 async fn main() {
-    let mut input = String::new();
-    std::io::stdin().read_line(&mut input).unwrap();
-    let trimmed = input.trim();
+    let mut ports = hydroflow::util::cli::init().await;
+    let foo_send = ports.remove("foo").unwrap().1.unwrap();
 
-    #[allow(unused)]
-    let mut bind_config = serde_json::from_str::<HashMap<String, BindConfig>>(trimmed).unwrap();
-
-    // bind to sockets
-    #[allow(unused)]
-    let mut bind_results: HashMap<String, ConnectionPipe> = HashMap::new();
-
-    let bind_serialized = serde_json::to_string(&bind_results).unwrap();
-    println!("ready: {bind_serialized}");
-
-    // listen for incoming connections
-
-    // receive outgoing connection config
-    let mut start_buf = String::new();
-    std::io::stdin().read_line(&mut start_buf).unwrap();
-    let mut connection_pipes = if start_buf.starts_with("start: ") {
-        serde_json::from_str::<HashMap<String, ConnectionPipe>>(
-            start_buf.trim_start_matches("start: ").trim(),
-        )
-        .unwrap()
-    } else {
-        panic!("expected start");
-    };
-
-    // connect to sockets
-    let (foo_send, _) = connection_pipes.remove("foo").unwrap().connect().await;
-
-    // start program
     let mut df = hydroflow_syntax! {
-        foo = dest_sink(foo_send);
+        foo = map(|v: String| serialize_to_bytes(v)) -> dest_sink(foo_send);
 
         repeat_iter([
-            "Hello",
-            "World",
+            "Hello".to_string(),
+            "World".to_string(),
         ]) -> foo;
     };
 
