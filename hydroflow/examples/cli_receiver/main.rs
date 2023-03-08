296b8bc65f98b89d2354ad0661e0f2e56a68db3a
Shadaj Laddad <shadaj@users.noreply.github.com>
2023-03-07 20:33:46 -0800

    Add `demux` operator to Hydro CLI to map node IDs to connections

--- hydroflow/examples/cli_receiver/main.rs
+++ hydroflow/examples/cli_receiver/main.rs
@@ -1,14 +1,22 @@
-use hydroflow::util::deserialize_from_bytes;
+use hydroflow::util::{
+    cli::{Connected, ConnectedBidi},
+    deserialize_from_bytes,
+};
 use hydroflow_datalog::datalog;
 
 #[tokio::main]
 async fn main() {
     let mut ports = hydroflow::util::cli::init().await;
-    let bar_recv = ports.remove("bar").unwrap().0.unwrap();
+    let bar_recv = ports
+        .remove("bar")
+        .unwrap()
+        .connect::<ConnectedBidi>()
+        .await
+        .take_source();
 
     let mut df = datalog!(
         r#"
-        .async repeated `for_each(|_: String| ())` `source_stream(bar_recv) -> map(|x| deserialize_from_bytes::<(String,)>(x.unwrap()))`
+        .async repeated `null::<(String,)>()` `source_stream(bar_recv) -> map(|x| deserialize_from_bytes::<(String,)>(x.unwrap()))`
         .output stdout `for_each(|tup| println!("echo {:?}", tup))`
 
         stdout(x) :- repeated(x)
