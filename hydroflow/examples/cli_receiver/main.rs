b107c476a5a817516a5a756e4c6ca6084a78e251
Shadaj Laddad <shadaj@users.noreply.github.com>
2023-03-07 20:03:34 -0800

    Require users to specify Hydroflow pipelines at the edges of a Dedalus program (#442)
    Rather than hardcoding, which bakes in a lot of assumptions, this allows flexibility for sending data locally or over the network. Also updates the CLI example to use Dedalus.

--- hydroflow/examples/cli_receiver/main.rs
+++ hydroflow/examples/cli_receiver/main.rs
@@ -1,16 +1,19 @@
-use hydroflow::{hydroflow_syntax, util::deserialize_from_bytes};
+use hydroflow::util::deserialize_from_bytes;
+use hydroflow_datalog::datalog;
 
 #[tokio::main]
 async fn main() {
     let mut ports = hydroflow::util::cli::init().await;
     let bar_recv = ports.remove("bar").unwrap().0.unwrap();
 
-    let mut df = hydroflow_syntax! {
-        bar = source_stream(bar_recv)
-            -> map(|x| deserialize_from_bytes(x.unwrap()));
+    let mut df = datalog!(
+        r#"
+        .input bar `source_stream(bar_recv) -> map(|x| deserialize_from_bytes::<(String,)>(x.unwrap()))`
+        .output stdout `for_each(|tup| println!("echo {:?}", tup))`
 
-        bar[0] -> for_each(|s: String| println!("echo {}", s));
-    };
+        stdout(x) :- bar(x)
+    "#
+    );
 
     df.run_async().await;
 }
--- hydroflow/examples/cli_sender/main.rs
+++ hydroflow/examples/cli_sender/main.rs
@@ -1,20 +1,19 @@
-use hydroflow::{hydroflow_syntax, util::serialize_to_bytes};
+use hydroflow::util::serialize_to_bytes;
+use hydroflow_datalog::datalog;
 
 #[tokio::main]
 async fn main() {
     let mut ports = hydroflow::util::cli::init().await;
     let foo_send = ports.remove("foo").unwrap().1.unwrap();
 
-    let mut df = hydroflow_syntax! {
-        foo = map(serialize_to_bytes::<String>) -> dest_sink(foo_send);
+    let mut df = datalog!(
+        r#"
+        .input repeated `repeat_iter(["Hello".to_string(), "world".to_string()]) -> map(|x| (x,))`
+        .output foo `map(|(v,)| serialize_to_bytes(v)) -> dest_sink(foo_send)`
 
-        repeat_iter([
-            "Hello".to_string(),
-            "World".to_string(),
-        ]) -> foo;
-    };
-
-    println!("hello from the sender!");
+        foo(x) :- repeated(x)
+    "#
+    );
 
     df.run_async().await;
 }
