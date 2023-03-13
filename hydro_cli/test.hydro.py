d24f8a68802c890e06c9fb9df0437235b3903cdb
Shadaj Laddad <shadaj@users.noreply.github.com>
2023-03-13 16:58:11 -0700

    Support networking topologies that mix local and cloud through SSH tunneling (#461)

--- hydro_cli/test.hydro.py
+++ hydro_cli/test.hydro.py
@@ -2,24 +2,27 @@ import hydro
 import json
 
 async def main(args):
-    gcp = args[0] == "gcp"
+    machine_1_gcp = args[0] == "gcp"
+    machine_2_gcp = args[1] == "gcp"
 
     deployment = hydro.Deployment()
+    localhost_machine = deployment.Localhost()
+
     machine1 = deployment.GCPComputeEngineHost(
         project="autocompartmentalization",
         machine_type="e2-micro",
         image="debian-cloud/debian-11",
         region="us-west1-a"
-    ) if gcp else deployment.Localhost()
+    ) if machine_1_gcp else localhost_machine
 
     machine2 = deployment.GCPComputeEngineHost(
         project="autocompartmentalization",
         machine_type="e2-micro",
         image="debian-cloud/debian-11",
         region="us-west1-a"
-    ) if gcp else machine1
+    ) if machine_2_gcp else localhost_machine
 
-    program = deployment.HydroflowCrate(
+    sender = deployment.HydroflowCrate(
         src="../hydroflow",
         example="cli_sender",
         features=["cli_integration"],
@@ -27,15 +30,15 @@ async def main(args):
         on=machine1
     )
 
-    program2 = deployment.HydroflowCrate(
+    receiver = deployment.HydroflowCrate(
         src="../hydroflow",
         example="cli_receiver",
         features=["cli_integration"],
         on=machine2
     )
 
-    program.ports.broadcast.send_to(hydro.demux({
-        0: program2.ports.broadcast
+    sender.ports.broadcast.send_to(hydro.demux({
+        0: receiver.ports.broadcast
     }))
 
     await deployment.deploy()
@@ -43,19 +46,19 @@ async def main(args):
     print("deployed!")
 
     # create this as separate variable to indicate to Hydro that we want to capture all stdout, even after the loop
-    program2out = await program2.stdout()
+    receiver_out = await receiver.stdout()
 
     await deployment.start()
     print("started!")
 
     counter = 0
-    async for log in program2out:
+    async for log in receiver_out:
         print(f"{counter}: {log}")
         counter += 1
         if counter == 10:
             break
 
-    print(await program.exit_code())
+    print(await sender.exit_code())
 
 if __name__ == "__main__":
     import sys
