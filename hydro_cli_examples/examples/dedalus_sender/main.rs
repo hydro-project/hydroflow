e1d8a96bce0c5da3319afd0cd0ca4f528e489bad
Shadaj Laddad <shadaj@users.noreply.github.com>
2023-03-16 10:23:27 -0700

    Properly handle interrupts and fix non-flushing demux

--- hydro_cli_examples/examples/dedalus_sender/main.rs
+++ hydro_cli_examples/examples/dedalus_sender/main.rs
@@ -1,6 +1,9 @@
-use hydroflow::util::{
-    cli::{Connected, ConnectedBidi, ConnectedDemux},
-    serialize_to_bytes,
+use hydroflow::{
+    tokio_stream::wrappers::IntervalStream,
+    util::{
+        cli::{Connected, ConnectedBidi, ConnectedDemux},
+        serialize_to_bytes,
+    },
 };
 use hydroflow_datalog::datalog;
 
@@ -16,13 +19,16 @@ async fn main() {
     let peers: Vec<u32> = serde_json::from_str(&std::env::args().nth(1).unwrap()).unwrap();
     let broadcast_sink = broadcast_port.take_sink();
 
+    let periodic = IntervalStream::new(tokio::time::interval(std::time::Duration::from_secs(1)));
+
     let mut df = datalog!(
         r#"
         .input repeated `repeat_iter([("Hello".to_string(),), ("world".to_string(),)])`
+        .input periodic `source_stream(periodic) -> map(|_| () )`
         .input peers `repeat_iter(peers.clone()) -> map(|p| (p,))`
         .async broadcast `map(|(node_id, v)| (node_id, serialize_to_bytes(v))) -> dest_sink(broadcast_sink)` `null::<(String,)>()`
 
-        broadcast@n(x) :~ repeated(x), peers(n)
+        broadcast@n(x) :~ repeated(x), periodic(), peers(n)
     "#
     );
 
