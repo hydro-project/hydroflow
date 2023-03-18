488c001bb7a042d1eda4df24d93ca3fc3741d359
Mingwei Samuel <mingwei.samuel@gmail.com>
2023-03-17 17:02:16 -0700

    Add `source_interval` op based on `tokio::time::Interval`, close #361 (#493)
    * Add `source_interval` op based on `tokio::time::Interval`, close #361
    
    * Add compile-fail test for bad arg, number instead of `Duration`.

---
+++ hydroflow/tests/compile-fail/surface_source_interval_badarg.rs
@@ -0,0 +1,10 @@
+use hydroflow::{hydroflow_syntax, var_args};
+
+#[tokio::main]
+async fn main() {
+    let mut df = hydroflow_syntax! {
+        // Should be a `Duration`.
+        source_interval(5) -> for_each(std::mem::drop);
+    };
+    df.run_async().await;
+}
