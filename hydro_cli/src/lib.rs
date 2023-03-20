337133e938c70231d30c5e2e495c49bb9d2f94bb
Shadaj Laddad <shadaj@users.noreply.github.com>
2023-03-20 15:42:01 -0700

    Add API to get CLI connection config as JSON

--- hydro_cli/src/lib.rs
+++ hydro_cli/src/lib.rs
@@ -5,11 +5,12 @@ use async_channel::Receiver;
 use bytes::Bytes;
 use futures::{Future, SinkExt, StreamExt};
 use hydroflow_cli_integration::{
-    ConnectedBidi, ConnectedSink, ConnectedSource, DynSink, DynStream,
+    ConnectedBidi, ConnectedSink, ConnectedSource, DynSink, DynStream, ServerOrBound,
 };
 use pyo3::exceptions::PyException;
 use pyo3::types::{PyBytes, PyDict};
 use pyo3::{create_exception, prelude::*, wrap_pymodule};
+use pythonize::pythonize;
 use tokio::sync::RwLock;
 
 use crate::core::hydroflow_crate::ports::HydroflowSource;
@@ -512,7 +513,15 @@ struct ServerPort {
 
 #[pymethods]
 impl ServerPort {
-    fn source<'p>(&mut self, py: Python<'p>) -> &'p pyo3::PyAny {
+    fn json(&self, py: Python<'_>) -> Py<pyo3::PyAny> {
+        if let ServerOrBound::Server(server) = self.underlying.as_ref().unwrap() {
+            pythonize(py, &server).unwrap()
+        } else {
+            panic!()
+        }
+    }
+
+    fn take_source<'p>(&mut self, py: Python<'p>) -> &'p pyo3::PyAny {
         let underlying = self.underlying.take().unwrap();
         interruptible_future_to_py(py, async move {
             convert_next_to_generator(PythonStream {
@@ -524,7 +533,7 @@ impl ServerPort {
         .unwrap()
     }
 
-    fn sink<'p>(&mut self, py: Python<'p>) -> &'p pyo3::PyAny {
+    fn take_sink<'p>(&mut self, py: Python<'p>) -> &'p pyo3::PyAny {
         let underlying = self.underlying.take().unwrap();
         interruptible_future_to_py(py, async move {
             let connected = underlying.connect::<ConnectedBidi>().await.take_sink();
