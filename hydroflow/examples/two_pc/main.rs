c5933a549703b1d7f88d4f5801523864c263069e
Mingwei Samuel <mingwei.samuel@gmail.com>
2023-03-21 21:22:26 -0700

    Add `source_json` operator, use in `two_pc` (#497)

--- hydroflow/examples/two_pc/main.rs
+++ hydroflow/examples/two_pc/main.rs
@@ -5,9 +5,6 @@ use hydroflow::util::{bind_udp_bytes, ipv4_resolve};
 use serde::Deserialize;
 use subordinate::run_subordinate;
 
-use std::error::Error;
-use std::fs::File;
-use std::io::BufReader;
 use std::net::SocketAddr;
 use std::path::Path;
 
@@ -49,37 +46,20 @@ struct Addresses {
     subordinates: Vec<String>,
 }
 
-fn read_addresses_from_file<P: AsRef<Path>>(path: P) -> Result<Addresses, Box<dyn Error>> {
-    // Open the file in read-only mode with buffer.
-    let file = File::open(path)?;
-    let reader = BufReader::new(file);
-
-    // Read the JSON contents of the file as an instance of `Subordinates`.
-    let u = serde_json::from_reader(reader)?;
-
-    // Return the `Subordinates`.
-    Ok(u)
-}
-
 #[tokio::main]
 async fn main() {
     let opts = Opts::parse();
     let path = Path::new(&opts.path);
-    let subordinates = read_addresses_from_file(path).unwrap().subordinates;
-    let coordinator = read_addresses_from_file(path).unwrap().coordinator;
     let addr = opts.addr;
 
     match opts.role {
         Role::Coordinator => {
             let (outbound, inbound, _) = bind_udp_bytes(addr).await;
-            run_coordinator(outbound, inbound, subordinates, opts.graph.clone()).await;
+            run_coordinator(outbound, inbound, path, opts.graph.clone()).await;
         }
         Role::Subordinate => {
             let (outbound, inbound, _) = bind_udp_bytes(addr).await;
-            println!("Coordinator: {}", coordinator);
-            let server_addr = ipv4_resolve(coordinator.trim()).unwrap();
-
-            run_subordinate(outbound, inbound, server_addr, opts.graph.clone()).await;
+            run_subordinate(outbound, inbound, path, opts.graph.clone()).await;
         }
     }
 }
