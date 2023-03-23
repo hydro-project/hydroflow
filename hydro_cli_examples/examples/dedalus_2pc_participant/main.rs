0b99f0627fcd43c1ad4ff31dbf2b2aa5e47a2e6d
David Chu <davidchuyaya@gmail.com>
2023-03-23 16:45:07 -0700

    Dedalus 2pc implementation

---
+++ hydro_cli_examples/examples/dedalus_2pc_participant/main.rs
@@ -0,0 +1,69 @@
+use hydroflow::util::{
+    cli::{ConnectedBidi, ConnectedDemux, ConnectedSink, ConnectedSource},
+    deserialize_from_bytes, serialize_to_bytes,
+};
+use hydroflow_datalog::datalog;
+
+#[tokio::main]
+async fn main() {
+    let mut ports = hydroflow::util::cli::init().await;
+    let vote_to_participant_source = ports
+        .remove("vote_to_participant")
+        .unwrap()
+        .connect::<ConnectedBidi>()
+        .await
+        .into_source();
+
+    let vote_from_participant_port = ports
+        .remove("vote_from_participant")
+        .unwrap()
+        .connect::<ConnectedDemux<ConnectedBidi>>()
+        .await;
+
+    let peers = vote_from_participant_port.keys.clone();
+    let vote_from_participant_sink = vote_from_participant_port.into_sink();
+
+    let instruct_to_participant_source = ports
+        .remove("instruct_to_participant")
+        .unwrap()
+        .connect::<ConnectedBidi>()
+        .await
+        .into_source();
+
+    let ack_from_participant_sink = ports
+        .remove("ack_from_participant")
+        .unwrap()
+        .connect::<ConnectedDemux<ConnectedBidi>>()
+        .await
+        .into_sink();
+
+    let my_id: Vec<u32> = serde_json::from_str(&std::env::args().nth(1).unwrap()).unwrap();
+    println!("my_id: {:?}", my_id);
+    println!("coordinator: {:?}", peers);
+
+    let mut df = datalog!(
+        r#"
+        .input myID `repeat_iter(my_id.clone()) -> map(|p| (p,))`
+        .input coordinator `repeat_iter(peers.clone()) -> map(|p| (p,))`
+        .input verdict `repeat_iter([(true,),])`
+        // .output voteOut `for_each(|(i,myID):(u32,u32,)| println!("participant {:?}: message {:?}", myID, i))`
+        
+        .async voteToParticipant `null::<(u32,String,)>()` `source_stream(vote_to_participant_source) -> map(|x| deserialize_from_bytes::<(u32,String,)>(x.unwrap()))`
+        .async voteFromParticipant `map(|(node_id, v)| (node_id, serialize_to_bytes(v))) -> dest_sink(vote_from_participant_sink)` `null::<(u32,String,)>()`
+        .async instructToParticipant `null::<(u32,String,bool,)>()` `source_stream(instruct_to_participant_source) -> map(|x| deserialize_from_bytes::<(u32,String,bool,)>(x.unwrap()))`
+        .async ackFromParticipant `map(|(node_id, v)| (node_id, serialize_to_bytes(v))) -> dest_sink(ack_from_participant_sink)` `null::<(u32,String,u32,)>()`
+    
+        # .output verdictRequest    
+        # .output log
+        
+        # verdictRequest(i, msg) :- voteToParticipant(i, msg)
+        voteFromParticipant@addr(i, msg, res, l_from) :~ voteToParticipant(i, msg), coordinator(addr), myID(l_from), verdict(res)
+        ackFromParticipant@addr(i, msg, l_from) :~ instructToParticipant(i, msg, b), coordinator(addr), myID(l_from)
+        // voteOut(i, l) :- voteToParticipant(i, msg), myID(l)
+        
+        # log(i, msg, type) :- instructToParticipant(i, msg, type) # the log channel will sort everything out
+        "#
+    );
+
+    df.run_async().await;
+}
