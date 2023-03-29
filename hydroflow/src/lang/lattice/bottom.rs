63825036e0f9a4a9d1e7323f5ceb30e4d0a9a1f2
Mingwei Samuel <mingwei.samuel@gmail.com>
2023-03-29 12:14:17 -0700

    implement `BottomRepr`, add `Merge::merge_owned` helper (#520)

--- hydroflow/src/lang/lattice/bottom.rs
+++ hydroflow/src/lang/lattice/bottom.rs
@@ -1,4 +1,6 @@
-use super::{Debottom, LatticeRepr};
+use std::cmp::Ordering;
+
+use super::{Compare, Convert, Debottom, Lattice, LatticeRepr, Merge};
 
 pub struct BottomRepr<Lr: LatticeRepr> {
     _phantom: std::marker::PhantomData<Lr>,
@@ -7,6 +9,59 @@ pub struct BottomRepr<Lr: LatticeRepr> {
 impl<Lr: LatticeRepr> LatticeRepr for BottomRepr<Lr> {
     type Lattice = Lr::Lattice;
     type Repr = Option<Lr::Repr>;
+}
+
+impl<SelfLr, DeltaLr, L> Merge<BottomRepr<DeltaLr>> for BottomRepr<SelfLr>
+where
+    SelfLr: LatticeRepr<Lattice = L> + Merge<DeltaLr>,
+    DeltaLr: LatticeRepr<Lattice = L> + Convert<SelfLr>,
+    L: Lattice,
+{
+    fn merge(this: &mut Self::Repr, delta: <BottomRepr<DeltaLr> as LatticeRepr>::Repr) -> bool {
+        match (this, delta) {
+            (None, None) => false,
+            (Some(_), None) => false,
+            (this @ None, Some(delta_inner)) => {
+                *this = Some(<DeltaLr as Convert<SelfLr>>::convert(delta_inner));
+                true
+            }
+            (Some(this_inner), Some(delta_inner)) => {
+                <SelfLr as Merge<DeltaLr>>::merge(this_inner, delta_inner)
+            }
+        }
+    }
+}
+
+impl<SelfLr, TargetLr, L> Convert<BottomRepr<TargetLr>> for BottomRepr<SelfLr>
+where
+    SelfLr: LatticeRepr<Lattice = L> + Convert<TargetLr>,
+    TargetLr: LatticeRepr<Lattice = L>,
+    L: Lattice,
+{
+    fn convert(this: Self::Repr) -> <BottomRepr<TargetLr> as LatticeRepr>::Repr {
+        this.map(<SelfLr as Convert<TargetLr>>::convert)
+    }
+}
+
+impl<SelfLr, OtherLr, L> Compare<BottomRepr<OtherLr>> for BottomRepr<SelfLr>
+where
+    SelfLr: LatticeRepr<Lattice = L> + Compare<OtherLr>,
+    OtherLr: LatticeRepr<Lattice = L>,
+    L: Lattice,
+{
+    fn compare(
+        this: &Self::Repr,
+        other: &<BottomRepr<OtherLr> as LatticeRepr>::Repr,
+    ) -> Option<Ordering> {
+        match (this, other) {
+            (None, None) => Some(Ordering::Equal),
+            (None, Some(_)) => Some(Ordering::Less),
+            (Some(_), None) => Some(Ordering::Greater),
+            (Some(this_inner), Some(other_inner)) => {
+                <SelfLr as Compare<OtherLr>>::compare(this_inner, other_inner)
+            }
+        }
+    }
 }
 
 impl<Lr: LatticeRepr> Debottom for BottomRepr<Lr> {
