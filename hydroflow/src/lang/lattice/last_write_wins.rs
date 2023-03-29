78545ca253871ddaf7795c1cc21b4dd1487cc5f2
zzlk <2418897+zzlk@users.noreply.github.com>
2023-03-29 12:00:22 -0700

    address comments

--- hydroflow/src/lang/lattice/last_write_wins.rs
+++ hydroflow/src/lang/lattice/last_write_wins.rs
@@ -1,3 +1,4 @@
+use std::fmt::Debug;
 use std::{cmp::Ordering, marker::PhantomData};
 
 use super::Convert;
@@ -16,7 +17,9 @@ impl<M: Ord + Clone, T: Clone> LatticeRepr for LastWriteWinsRepr<M, T> {
     type Repr = (M, T);
 }
 
-impl<M: Ord + Clone, T: Clone> Merge<LastWriteWinsRepr<M, T>> for LastWriteWinsRepr<M, T> {
+impl<M: Ord + Clone, T: Clone + PartialEq + Debug> Merge<LastWriteWinsRepr<M, T>>
+    for LastWriteWinsRepr<M, T>
+{
     fn merge(
         this: &mut <LastWriteWinsRepr<M, T> as LatticeRepr>::Repr,
         delta: <LastWriteWinsRepr<M, T> as LatticeRepr>::Repr,
@@ -32,10 +35,9 @@ impl<M: Ord + Clone, T: Clone> Merge<LastWriteWinsRepr<M, T>> for LastWriteWinsR
             }
             Ordering::Equal => {
                 // If markers are equal then payloads must be equal
-                // But don't want to require T: PartialEq + Debug
-                // assert_eq!(*payload_this, payload_delta)
                 // Maybe an additional node-id tag could be added
                 // to ensure a total order for when the marker is tied?
+                debug_assert_eq!(*payload_this, payload_delta);
             }
             Ordering::Greater => {
                 changed = true;
@@ -48,16 +50,24 @@ impl<M: Ord + Clone, T: Clone> Merge<LastWriteWinsRepr<M, T>> for LastWriteWinsR
     }
 }
 
-impl<M: Ord + Clone, T: Clone> Compare<LastWriteWinsRepr<M, T>> for LastWriteWinsRepr<M, T> {
+impl<M: Ord + Clone, T: Clone + PartialEq + Debug> Compare<LastWriteWinsRepr<M, T>>
+    for LastWriteWinsRepr<M, T>
+{
     fn compare(
         this: &<LastWriteWinsRepr<M, T> as LatticeRepr>::Repr,
         delta: &<LastWriteWinsRepr<M, T> as LatticeRepr>::Repr,
     ) -> Option<Ordering> {
-        let (marker_this, _) = this;
-        let (marker_delta, _) = delta;
+        let (marker_this, payload_this) = this;
+        let (marker_delta, payload_delta) = delta;
 
-        // Could also do an extra assert here for when markers are equal that payload is also equal.
-        Some(marker_this.cmp(marker_delta))
+        let ordering = marker_this.cmp(marker_delta);
+
+        if ordering == Ordering::Equal {
+            // If markers are equal then payloads must be equal
+            debug_assert_eq!(*payload_this, *payload_delta);
+        }
+
+        Some(ordering)
     }
 }
 
@@ -72,28 +82,66 @@ impl<M: Ord + Clone, T: Clone> Convert<LastWriteWinsRepr<M, T>> for LastWriteWin
 #[cfg(test)]
 mod tests {
     use super::LastWriteWinsRepr;
-    use crate::lang::lattice::Merge;
+    use crate::lang::lattice::{Compare, Convert, Merge};
+    use std::cmp::Ordering;
 
     type Lww = LastWriteWinsRepr<usize, usize>;
 
     #[test]
-    fn lattice_moves_forward() {
+    fn merge_lattice_moves_forward() {
         let mut x = (0, 0);
         assert!(<Lww as Merge<Lww>>::merge(&mut x, (1, 2)));
         assert_eq!(x, (1, 2));
     }
 
     #[test]
-    fn lattice_doesnt_move_backward() {
+    fn merge_lattice_doesnt_move_backward() {
         let mut x = (1, 2);
         assert!(!<Lww as Merge<Lww>>::merge(&mut x, (0, 0)));
         assert_eq!(x, (1, 2));
     }
 
     #[test]
-    fn equal_marker_does_nothing() {
+    #[should_panic]
+    fn merge_equal_marker_different_payload_panics() {
         let mut x = (0, 0);
         assert!(!<Lww as Merge<Lww>>::merge(&mut x, (0, 1)));
         assert_eq!(x, (0, 0));
+    }
+
+    #[test]
+    fn merge_equal_marker_same_payload_does_not_panic() {
+        let mut x = (3, 3);
+        assert!(!<Lww as Merge<Lww>>::merge(&mut x, (3, 3)));
+        assert_eq!(x, (3, 3));
+    }
+
+    #[test]
+    fn compare() {
+        assert_eq!(
+            <Lww as Compare<Lww>>::compare(&(0, 0), &(0, 0)),
+            Some(Ordering::Equal)
+        );
+
+        assert_eq!(
+            <Lww as Compare<Lww>>::compare(&(0, 0), &(1, 0)),
+            Some(Ordering::Less)
+        );
+
+        assert_eq!(
+            <Lww as Compare<Lww>>::compare(&(1, 0), &(0, 0)),
+            Some(Ordering::Greater)
+        );
+    }
+
+    #[test]
+    #[should_panic]
+    fn compare_equal_marker_different_payload_panics() {
+        <Lww as Compare<Lww>>::compare(&(0, 0), &(0, 1));
+    }
+
+    #[test]
+    fn convert() {
+        assert_eq!(<Lww as Convert<Lww>>::convert((0, 0)), (0, 0));
     }
 }
