ad1609d0c9a700ada5678a8df05694ff9606c54c
Shadaj Laddad <shadaj@users.noreply.github.com>
2023-04-02 22:37:26 -0700

    Allow specifying the user to sign in as on a GCP machine (#534)

--- hydro_cli/src/core/terraform.rs
+++ hydro_cli/src/core/terraform.rs
@@ -78,6 +78,7 @@ impl Drop for TerraformPool {
 #[derive(Serialize, Deserialize)]
 pub struct TerraformBatch {
     pub terraform: TerraformConfig,
+    #[serde(skip_serializing_if = "HashMap::is_empty")]
     pub data: HashMap<String, HashMap<String, serde_json::Value>>,
     pub resource: HashMap<String, HashMap<String, serde_json::Value>>,
     pub output: HashMap<String, TerraformOutput>,
