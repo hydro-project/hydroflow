f2d1595c443424bfa257b906fda447c0e3d13593
zzlk <2418897+zzlk@users.noreply.github.com>
2023-04-05 16:40:17 -0700

    add comments for bufferpool

--- hydroflow/examples/kvs_bench/buffer_pool/mod.rs
+++ hydroflow/examples/kvs_bench/buffer_pool/mod.rs
@@ -9,17 +9,28 @@ use std::{
 
 pub type BufferType = [u8; 1024];
 
+/// It is slow to allocate and, in particular, free blocks of memory.
+/// If we know ahead of time what size buffers we will need then we can pre-allocate a bunch of them and
+/// reuse those allocations.
+/// To comply with rust's type system, these buffers have shared owernship and interior mutability, so Rc<RefCell>
+/// Eventually these buffers can be made more dynamic by using scatter-gather I/O and a buffer-of-buffers pattern.
+/// We are not there yet and so this is a simple approximation of what it will eventually look like.
 #[derive(Debug)]
 pub struct BufferPool {
     buffers: Vec<Rc<RefCell<BufferType>>>,
 }
 
+/// This buffer will be returned to it's owning buffer pool when it is dropped.
+/// In order to return it, it has to know where it is being returned to, this is the [collector] member.
+/// The collector pointer is weak because otherwise there would be a cycle between BufferPool and AutoReturnBufferInner
+/// and an AutoReturnBufferInner does not logically have any kind of ownership over the BufferPool shared pool.
 #[derive(Clone, Debug)]
 pub struct AutoReturnBufferInner {
     pub collector: Weak<RefCell<BufferPool>>,
     pub inner: Rc<RefCell<BufferType>>,
 }
 
+/// This extra layer of wrapping is necessary so that AutoReturnBuffer can have a reasonable default value.
 #[derive(Clone, Debug, Default)]
 pub struct AutoReturnBuffer {
     pub inner: Option<AutoReturnBufferInner>,
@@ -58,13 +69,12 @@ impl Drop for AutoReturnBuffer {
         if let Some(inner) = &self.inner {
             if Rc::strong_count(&self.inner.as_ref().unwrap().inner) == 1 {
                 // This is the last one, give the buffer back to the collection.
-                inner
-                    .collector
-                    .upgrade()
-                    .unwrap()
-                    .borrow_mut()
-                    .buffers
-                    .push(inner.inner.clone());
+                if let Some(pool) = inner.collector.upgrade() {
+                    pool.borrow_mut().buffers.push(inner.inner.clone());
+                }
+
+                // If the upgrade fails then the buffer pool has been freed and so there
+                // is nothing to return this buffer to, just drop it instead.
             }
         }
     }
