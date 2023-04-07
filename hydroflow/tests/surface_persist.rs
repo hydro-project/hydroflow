fe006d5eabc4eca8c5898e661dccb711b9fd9fd7
Shadaj Laddad <shadaj@users.noreply.github.com>
2023-04-06 21:05:38 -0700

    Add `test_persist_replay_join` test (#548 #550)

--- hydroflow/tests/surface_persist.rs
+++ hydroflow/tests/surface_persist.rs
@@ -87,3 +87,37 @@ pub fn test_persist_join() {
     input_send.send(("hello", "san francisco")).unwrap();
     flow.run_tick();
 }
+
+#[multiplatform_test]
+pub fn test_persist_replay_join() {
+    let (persist_input_send, persist_input) = hydroflow::util::unbounded_channel::<u32>();
+    let (other_input_send, other_input) = hydroflow::util::unbounded_channel::<u32>();
+    let (result_send, mut result_recv) = hydroflow::util::unbounded_channel::<(u32, u32)>();
+
+    let mut hf = hydroflow_syntax! {
+        source_stream(persist_input)
+            -> persist()
+            -> fold::<'tick>(0, |a, b| (a + b))
+            -> next_stratum()
+            -> [0]product_node;
+
+        source_stream(other_input) -> [1] product_node;
+
+        product_node = cross_join::<'tick, 'tick>() -> for_each(|x| result_send.send(x).unwrap());
+    };
+    assert_graphvis_snapshots!(hf);
+
+    persist_input_send.send(1).unwrap();
+    other_input_send.send(2).unwrap();
+    hf.run_tick();
+    assert_eq!(&[(1, 2)], &*collect_ready::<Vec<_>, _>(&mut result_recv));
+
+    persist_input_send.send(2).unwrap();
+    other_input_send.send(2).unwrap();
+    hf.run_tick();
+    assert_eq!(&[(3, 2)], &*collect_ready::<Vec<_>, _>(&mut result_recv));
+
+    other_input_send.send(3).unwrap();
+    hf.run_tick();
+    assert_eq!(&[(3, 3)], &*collect_ready::<Vec<_>, _>(&mut result_recv));
+}
