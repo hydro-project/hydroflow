2cdccce0c0e139d594fd201032b0731d10767d77
Shadaj Laddad <shadaj@users.noreply.github.com>
2023-04-15 18:03:15 -0700

    Add classic counter CRDT benchmark to compare against

--- hydro_cli/src/core/deployment.rs
+++ hydro_cli/src/core/deployment.rs
@@ -1,4 +1,5 @@
 use super::ResourcePool;
+use super::ResourceResult;
 use super::Service;
 
 use super::Host;
@@ -7,26 +8,46 @@ use anyhow::Result;
 use tokio::sync::RwLock;
 
 use std::sync::Arc;
+use std::sync::Weak;
 
 #[derive(Default)]
 pub struct Deployment {
     pub hosts: Vec<Arc<RwLock<dyn Host>>>,
-    pub services: Vec<Arc<RwLock<dyn Service>>>,
+    pub services: Vec<Weak<RwLock<dyn Service>>>,
     pub resource_pool: ResourcePool,
+    last_resource_result: Option<Arc<ResourceResult>>,
 }
 
 impl Deployment {
     pub async fn deploy(&mut self) -> Result<()> {
         let mut resource_batch = super::ResourceBatch::new();
+        let active_services = self
+            .services
+            .iter()
+            .filter(|service| service.upgrade().is_some())
+            .cloned()
+            .collect::<Vec<_>>();
+        self.services = active_services;
+
         for service in self.services.iter_mut() {
-            service.write().await.collect_resources(&mut resource_batch);
+            service
+                .upgrade()
+                .unwrap()
+                .write()
+                .await
+                .collect_resources(&mut resource_batch);
         }
 
         for host in self.hosts.iter_mut() {
             host.write().await.collect_resources(&mut resource_batch);
         }
 
-        let result = Arc::new(resource_batch.provision(&mut self.resource_pool).await?);
+        let result = Arc::new(
+            resource_batch
+                .provision(&mut self.resource_pool, self.last_resource_result.clone())
+                .await?,
+        );
+        self.last_resource_result = Some(result.clone());
 
         let hosts_provisioned =
             self.hosts
@@ -40,8 +61,14 @@ impl Deployment {
         let services_future =
             self.services
                 .iter_mut()
-                .map(|service: &mut Arc<RwLock<dyn Service>>| async {
-                    service.write().await.deploy(&result).await;
+                .map(|service: &mut Weak<RwLock<dyn Service>>| async {
+                    service
+                        .upgrade()
+                        .unwrap()
+                        .write()
+                        .await
+                        .deploy(&result)
+                        .await;
                 });
 
         futures::future::join_all(services_future).await;
@@ -50,8 +77,8 @@ impl Deployment {
         let all_services_ready =
             self.services
                 .iter()
-                .map(|service: &Arc<RwLock<dyn Service>>| async {
-                    service.write().await.ready().await?;
+                .map(|service: &Weak<RwLock<dyn Service>>| async {
+                    service.upgrade().unwrap().write().await.ready().await?;
                     Ok(()) as Result<()>
                 });
 
@@ -62,11 +89,19 @@ impl Deployment {
     }
 
     pub async fn start(&mut self) {
+        let active_services = self
+            .services
+            .iter()
+            .filter(|service| service.upgrade().is_some())
+            .cloned()
+            .collect::<Vec<_>>();
+        self.services = active_services;
+
         let all_services_start =
             self.services
                 .iter()
-                .map(|service: &Arc<RwLock<dyn Service>>| async {
-                    service.write().await.start().await;
+                .map(|service: &Weak<RwLock<dyn Service>>| async {
+                    service.upgrade().unwrap().write().await.start().await;
                 });
 
         futures::future::join_all(all_services_start).await;
@@ -83,7 +118,8 @@ impl Deployment {
 
     pub fn add_service<T: Service + 'static>(&mut self, service: T) -> Arc<RwLock<T>> {
         let arc = Arc::new(RwLock::new(service));
-        self.services.push(arc.clone());
+        let dyn_arc: Arc<RwLock<dyn Service>> = arc.clone();
+        self.services.push(Arc::downgrade(&dyn_arc));
         arc
     }
 }
