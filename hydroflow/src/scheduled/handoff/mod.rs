c0006c4c73e0f3f5c65274e3ad76537ea9fe2643
Mingwei Samuel <mingwei.samuel@gmail.com>
2023-05-02 17:34:43 -0700

    Move lattice2 into new separate `lattices` crate (#634)

--- hydroflow/src/scheduled/handoff/mod.rs
+++ hydroflow/src/scheduled/handoff/mod.rs
@@ -47,3 +47,21 @@ pub trait Handoff: Default + HandoffMeta {
         <Self as TryCanReceive<T>>::try_give(self, item)
     }
 }
+
+/// Wrapper around `IntoIterator` to avoid trait impl conflicts.
+#[repr(transparent)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Iter<I>(pub I)
+where
+    I: IntoIterator;
+impl<I> IntoIterator for Iter<I>
+where
+    I: IntoIterator,
+{
+    type Item = I::Item;
+    type IntoIter = I::IntoIter;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.into_iter()
+    }
+}
