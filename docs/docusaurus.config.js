ed510188839a2a9314af151582690515fbe60952
Shadaj Laddad <shadaj@users.noreply.github.com>
2023-05-23 10:09:28 -0700

    Fix paper preview rendering in dark mode (#682)

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -178,7 +178,7 @@ const config = {
               // },
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/hydro-project/hydroflow',
               },
             ],
           },
