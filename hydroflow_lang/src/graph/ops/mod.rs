8d88e8e01a985db8ebd8dbc6768163452cedc3ab
Mingwei Samuel <mingwei.samuel@gmail.com>
2023-05-25 10:48:42 -0700

    feat(hydroflow_lang): Add `multiset_delta` operator (#696)

--- hydroflow_lang/src/graph/ops/mod.rs
+++ hydroflow_lang/src/graph/ops/mod.rs
@@ -251,6 +251,7 @@ declare_ops![
     lattice_merge::LATTICE_MERGE,
     map::MAP,
     union::UNION,
+    multiset_delta::MULTISET_DELTA,
     next_stratum::NEXT_STRATUM,
     next_tick::NEXT_TICK,
     null::NULL,
