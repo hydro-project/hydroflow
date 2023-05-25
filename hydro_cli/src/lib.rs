2b4e353334ca815ba30aeb913bc4e2d119e0f722
Shadaj Laddad <shadaj@users.noreply.github.com>
2023-05-25 12:54:02 -0700

    feat(hydro_deploy): finish up WebSocket chat example and avoid deadlocks in network setup

--- hydro_cli/src/lib.rs
+++ hydro_cli/src/lib.rs
@@ -6,7 +6,7 @@ use async_channel::Receiver;
 use bytes::Bytes;
 use futures::{Future, SinkExt, StreamExt};
 use hydroflow_cli_integration::{
-    ConnectedBidi, ConnectedSink, ConnectedSource, DynSink, DynStream, ServerOrBound,
+    ConnectedBidi, ConnectedSink, ConnectedSource, DynSink, DynStream, ServerOrBound, ServerPort,
 };
 use pyo3::exceptions::{PyException, PyStopAsyncIteration};
 use pyo3::prelude::*;
@@ -476,8 +476,11 @@ impl CustomClientPort {
         let underlying = self.underlying.clone();
         interruptible_future_to_py(py, async move {
             let underlying = underlying.read().await;
-            Ok(ServerPort {
-                underlying: Some(underlying.server_port().await),
+            let port = underlying.server_port().await;
+            let realized = (&port).into();
+            Ok(PythonServerPort {
+                underlying: port,
+                underlying_realized: Some(ServerOrBound::Server(realized)),
             })
         })
     }
@@ -684,23 +687,20 @@ fn null(py: Python<'_>) -> PyResult<Py<PyAny>> {
 }
 
 #[pyclass]
-struct ServerPort {
-    underlying: Option<ServerOrBound>,
+struct PythonServerPort {
+    underlying: ServerPort,
+    underlying_realized: Option<ServerOrBound>,
 }
 
 #[pymethods]
-impl ServerPort {
+impl PythonServerPort {
     fn json(&self, py: Python<'_>) -> Py<PyAny> {
-        if let ServerOrBound::Server(server) = self.underlying.as_ref().unwrap() {
-            pythonize(py, &server).unwrap()
-        } else {
-            panic!()
-        }
+        pythonize(py, &self.underlying).unwrap()
     }
 
     #[allow(clippy::wrong_self_convention)]
     fn into_source<'p>(&mut self, py: Python<'p>) -> PyResult<&'p PyAny> {
-        let underlying = self.underlying.take().unwrap();
+        let underlying = self.underlying_realized.take().unwrap();
         interruptible_future_to_py(py, async move {
             Ok(PythonStream {
                 underlying: Arc::new(RwLock::new(
@@ -712,7 +712,7 @@ impl ServerPort {
 
     #[allow(clippy::wrong_self_convention)]
     fn into_sink<'p>(&mut self, py: Python<'p>) -> PyResult<&'p PyAny> {
-        let underlying = self.underlying.take().unwrap();
+        let underlying = self.underlying_realized.take().unwrap();
         interruptible_future_to_py(py, async move {
             let connected = underlying.connect::<ConnectedBidi>().await.into_sink();
 
@@ -817,7 +817,7 @@ async def coroutine_to_safely_cancellable(c, cancel_token):
     module.add_class::<HydroflowCrate>()?;
     module.add_class::<HydroflowCratePort>()?;
 
-    module.add_class::<ServerPort>()?;
+    module.add_class::<PythonServerPort>()?;
     module.add_class::<PythonSink>()?;
     module.add_class::<PythonStream>()?;
 
