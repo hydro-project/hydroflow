da146f0ea931feefef8920cdb114918ebfb9af78
Shadaj Laddad <shadaj@users.noreply.github.com>
2023-05-25 13:22:43 -0700

    feat(hydro_deploy): finish up WebSocket chat example and avoid deadlocks in network setup

--- hydro_cli/src/lib.rs
+++ hydro_cli/src/lib.rs
@@ -477,7 +477,7 @@ impl CustomClientPort {
         interruptible_future_to_py(py, async move {
             let underlying = underlying.read().await;
             Ok(ServerPort {
-                underlying: Some(underlying.server_port().await),
+                underlying: underlying.server_port().await,
             })
         })
     }
@@ -685,26 +685,27 @@ fn null(py: Python<'_>) -> PyResult<Py<PyAny>> {
 
 #[pyclass]
 struct ServerPort {
-    underlying: Option<ServerOrBound>,
+    underlying: hydroflow_cli_integration::ServerPort,
 }
 
 #[pymethods]
 impl ServerPort {
     fn json(&self, py: Python<'_>) -> Py<PyAny> {
-        if let ServerOrBound::Server(server) = self.underlying.as_ref().unwrap() {
-            pythonize(py, &server).unwrap()
-        } else {
-            panic!()
-        }
+        pythonize(py, &self.underlying).unwrap()
     }
 
     #[allow(clippy::wrong_self_convention)]
     fn into_source<'p>(&mut self, py: Python<'p>) -> PyResult<&'p PyAny> {
-        let underlying = self.underlying.take().unwrap();
+        let runtime_read = TOKIO_RUNTIME.read().unwrap();
+        let guard = runtime_read.as_ref().unwrap().enter();
+        let realized = ServerOrBound::Server((&self.underlying).into());
+        drop(guard);
+        drop(runtime_read);
+
         interruptible_future_to_py(py, async move {
             Ok(PythonStream {
                 underlying: Arc::new(RwLock::new(
-                    underlying.connect::<ConnectedBidi>().await.into_source(),
+                    realized.connect::<ConnectedBidi>().await.into_source(),
                 )),
             })
         })
@@ -712,12 +713,17 @@ impl ServerPort {
 
     #[allow(clippy::wrong_self_convention)]
     fn into_sink<'p>(&mut self, py: Python<'p>) -> PyResult<&'p PyAny> {
-        let underlying = self.underlying.take().unwrap();
-        interruptible_future_to_py(py, async move {
-            let connected = underlying.connect::<ConnectedBidi>().await.into_sink();
+        let runtime_read = TOKIO_RUNTIME.read().unwrap();
+        let guard = runtime_read.as_ref().unwrap().enter();
+        let realized = ServerOrBound::Server((&self.underlying).into());
+        drop(guard);
+        drop(runtime_read);
 
+        interruptible_future_to_py(py, async move {
             Ok(PythonSink {
-                underlying: Arc::new(RwLock::new(connected)),
+                underlying: Arc::new(RwLock::new(
+                    realized.connect::<ConnectedBidi>().await.into_sink(),
+                )),
             })
         })
     }
---
+++ hydro_cli_examples/examples/ws_chat_server/util.rs
@@ -0,0 +1,72 @@
+use std::collections::HashMap;
+use std::sync::Arc;
+
+use futures::channel::mpsc::UnboundedSender;
+use futures::stream::SplitSink;
+use futures::{self, SinkExt, StreamExt};
+use hydroflow::tokio_stream::wrappers::UnboundedReceiverStream;
+use tokio::net::TcpListener;
+use tokio_tungstenite::tungstenite::Message;
+
+pub(crate) async fn ws_server(
+    ws_port: TcpListener,
+) -> (
+    UnboundedReceiverStream<usize>,
+    UnboundedReceiverStream<usize>,
+    UnboundedReceiverStream<(usize, String)>,
+    UnboundedSender<(usize, String)>,
+) {
+    let (clients_send, clients_connect) = hydroflow::util::unbounded_channel();
+    let (client_disconnect_send, clients_disconnect) = hydroflow::util::unbounded_channel();
+
+    let (received_messages_send, from_client) =
+        hydroflow::util::unbounded_channel::<(usize, String)>();
+    let recipients = Arc::new(tokio::sync::Mutex::new(HashMap::<
+        usize,
+        SplitSink<_, Message>,
+    >::new()));
+
+    let send_recipients = recipients.clone();
+    let (to_client, mut to_client_recv) = futures::channel::mpsc::unbounded::<(usize, String)>();
+    tokio::spawn(async move {
+        while let Some((recipient, msg)) = to_client_recv.next().await {
+            let mut recipients_locked = send_recipients.lock().await;
+            if let Some(r) = recipients_locked.get_mut(&recipient) {
+                if r.send(Message::Text(msg)).await.is_err() {
+                    let _ = client_disconnect_send.send(recipient);
+                    recipients_locked.remove(&recipient);
+                }
+            } else {
+                // dbg!("could not find recipient");
+            }
+        }
+    });
+
+    tokio::spawn(async move {
+        let mut counter: usize = 0;
+        loop {
+            if let Ok((stream, _)) = ws_port.accept().await {
+                if let Ok(ws_stream) = tokio_tungstenite::accept_async(stream).await {
+                    let (send, mut recv) = ws_stream.split();
+                    let my_counter = counter;
+                    counter += 1;
+
+                    recipients.lock().await.insert(my_counter, send);
+
+                    let my_received_messages_send = received_messages_send.clone();
+                    tokio::spawn(async move {
+                        while let Some(msg) = recv.next().await {
+                            if let Ok(Message::Text(msg)) = msg {
+                                my_received_messages_send.send((my_counter, msg)).unwrap();
+                            }
+                        }
+                    });
+
+                    clients_send.send(my_counter).unwrap();
+                }
+            }
+        }
+    });
+
+    (clients_connect, clients_disconnect, from_client, to_client)
+}
