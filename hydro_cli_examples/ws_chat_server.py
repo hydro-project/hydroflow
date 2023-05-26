6ff102c0ccffc23a4e9262f5dcd07e4913a59ec1
Shadaj Laddad <shadaj@users.noreply.github.com>
2023-05-26 14:27:39 -0700

    feat(hydro_deploy): finish up WebSocket chat example and avoid deadlocks in network setup

--- hydro_cli_examples/ws_chat_server.py
+++ hydro_cli_examples/ws_chat_server.py
@@ -1,9 +1,11 @@
 import asyncio
+from typing import Dict, List
 import hydro
 from pathlib import Path
 
 async def main(args):
     machine_gcp = args[0] == "gcp"
+    num_replicas = int(args[1]) if len(args) > 1 else 1
 
     deployment = hydro.Deployment()
     localhost_machine = deployment.Localhost()
@@ -12,45 +14,48 @@ async def main(args):
         project="autocompartmentalization",
     )
 
-    machine = deployment.GCPComputeEngineHost(
-        project="autocompartmentalization",
-        machine_type="e2-micro",
-        image="debian-cloud/debian-11",
-        region="us-west1-a",
-        network=gcp_vpc
-    ) if machine_gcp else localhost_machine
+    machines: List[hydro.GCPComputeEngineHost] = []
+    chat_servers: Dict[int, hydro.HydroflowCrate] = {}
+    ports: List[int] = []
+    for i in range(num_replicas):
+        machine = deployment.GCPComputeEngineHost(
+            project="autocompartmentalization",
+            machine_type="e2-micro",
+            image="debian-cloud/debian-11",
+            region="us-west1-a",
+            network=gcp_vpc
+        ) if machine_gcp else localhost_machine
 
-    machine2 = deployment.GCPComputeEngineHost(
-        project="autocompartmentalization",
-        machine_type="e2-micro",
-        image="debian-cloud/debian-11",
-        region="us-west1-a",
-        network=gcp_vpc
-    ) if machine_gcp else localhost_machine
+        machines.append(machine)
 
-    echo_server = deployment.HydroflowCrate(
-        src=str(Path(__file__).parent.absolute()),
-        example="ws_chat_server",
-        on=machine,
-        external_ports=[8080]
-    )
+        port = 8080 if machine_gcp else (8080 + i)
+        ports.append(port)
+        chat_servers[i] = deployment.HydroflowCrate(
+            src=str(Path(__file__).parent.absolute()),
+            example="ws_chat_server",
+            profile="release" if machine_gcp else "dev",
+            on=machine,
+            external_ports=[port],
+            args=[str(num_replicas), str(i), str(port)]
+        )
 
-    logger_server = deployment.HydroflowCrate(
-        src=str(Path(__file__).parent.absolute()),
-        example="stdout_receiver",
-        on=machine2,
-        display_id="logger"
-    )
-
-    echo_server.ports.to_logger.send_to(logger_server.ports.echo)
+    for server in chat_servers.values():
+        server.ports.to_peer.send_to(hydro.demux({
+            i: to_server.ports.from_peer.merge()
+            for (i, to_server) in chat_servers.items()
+        }))
 
     await deployment.deploy()
 
     await deployment.start()
 
     if machine_gcp:
-        print(machine.external_ip)
-    
+        for machine in machines:
+            print(f"Will listen on {machine.external_ip}:8080")
+    else:
+        for port in ports:
+            print(f"Will listen on localhost:{port}")
+
     while True:
         await asyncio.sleep(1)
 
