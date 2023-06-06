c99242378caf06810fa7de94e504e36af8aeaaf4
Mingwei Samuel <mingwei.samuel@gmail.com>
2023-06-06 09:31:10 -0700

    test(hydroflow): add `test_stratum/tick_loop` tests (#748)

---
+++ hydroflow/tests/surface_scheduling.rs
@@ -0,0 +1,40 @@
+use hydroflow::hydroflow_syntax;
+use multiplatform_test::multiplatform_test;
+
+#[multiplatform_test]
+pub fn test_stratum_loop() {
+    let (out_send, mut out_recv) = hydroflow::util::unbounded_channel::<usize>();
+
+    let mut df = hydroflow_syntax! {
+        source_iter([0]) -> union_tee;
+        union_tee = union() -> tee();
+        union_tee -> map(|n| n + 1) -> filter(|&n| n < 10) -> next_stratum() -> union_tee;
+        union_tee -> for_each(|v| out_send.send(v).unwrap());
+    };
+    df.run_available();
+
+    assert_eq!(
+        &[0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        &*hydroflow::util::collect_ready::<Vec<_>, _>(&mut out_recv)
+    );
+    assert_eq!((11, 0), (df.current_tick(), df.current_stratum()));
+}
+
+#[multiplatform_test]
+pub fn test_tick_loop() {
+    let (out_send, mut out_recv) = hydroflow::util::unbounded_channel::<usize>();
+
+    let mut df = hydroflow_syntax! {
+        source_iter([0]) -> union_tee;
+        union_tee = union() -> tee();
+        union_tee -> map(|n| n + 1) -> filter(|&n| n < 10) -> next_tick() -> union_tee;
+        union_tee -> for_each(|v| out_send.send(v).unwrap());
+    };
+    df.run_available();
+
+    assert_eq!(
+        &[0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        &*hydroflow::util::collect_ready::<Vec<_>, _>(&mut out_recv)
+    );
+    assert_eq!((10, 0), (df.current_tick(), df.current_stratum()));
+}
