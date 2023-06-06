c1b028089ea9d76ab71cd9cb4eaaaf16aa4b65a6
Mingwei Samuel <mingwei.samuel@gmail.com>
2023-06-06 12:53:56 -0700

    feat(multiplatform_test)!: `hydroflow`, `logging`/`tracing` features (#755)
    * Adds `tokio` for `#[tokio::test]`.
    * Adds `async_std` for `#[async_std::test]`.
    * Adds `hydroflow` for `#[hydroflow::test]`.
    * Adds `env_logging` for `env_logger` registering.
    * Adds `env_tracing` for `EnvFilter` `FmtSubscriber` `tracing`.
    
    BREAKING CHANGE: `default` is renamed to `test` (for `#[test]`)

--- multiplatform_test/src/lib.rs
+++ multiplatform_test/src/lib.rs
@@ -1,16 +1,84 @@
+#![doc = include_str!("../README.md")]
+
 use std::iter::Iterator;
 
 use proc_macro::TokenStream;
-use quote::quote;
+use quote::{quote, quote_spanned};
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy)]
 enum Platform {
     Default,
+    Tokio,
+    AsyncStd,
+    Hydroflow,
     Wasm,
+    EnvLogging,
+    EnvTracing,
+}
+impl Platform {
+    // All platforms.
+    const ALL: [Self; 7] = [
+        Self::Default,
+        Self::Tokio,
+        Self::AsyncStd,
+        Self::Hydroflow,
+        Self::Wasm,
+        Self::EnvLogging,
+        Self::EnvTracing,
+    ];
+    // Default when no platforms are specified.
+    const DEFAULT: [Self; 2] = [Self::Default, Self::Wasm];
+
+    /// Name of platform ident in attribute.
+    const fn name(self) -> &'static str {
+        match self {
+            Self::Default => "test",
+            Self::Tokio => "tokio",
+            Self::AsyncStd => "async_std",
+            Self::Hydroflow => "hydroflow",
+            Self::Wasm => "wasm",
+            Self::EnvLogging => "env_logging",
+            Self::EnvTracing => "env_tracing",
+        }
+    }
+
+    /// Generate the attribute for this platform (if any).
+    fn make_attribute(self) -> proc_macro2::TokenStream {
+        // Fully specify crate names so that the consumer does not need to add another
+        // use statement. They still need to depend on the crate in their `Cargo.toml`,
+        // though.
+        // TODO(mingwei): use `proc_macro_crate::crate_name(...)` to handle renames.
+        match self {
+            Platform::Default => quote! { #[test] },
+            Platform::Tokio => quote! { #[tokio::test ] },
+            Platform::AsyncStd => quote! { #[async_std::test] },
+            Platform::Hydroflow => quote! { #[hydroflow::test] },
+            Platform::Wasm => {
+                quote! { #[wasm_bindgen_test::wasm_bindgen_test] }
+            }
+            Platform::EnvLogging | Platform::EnvTracing => Default::default(),
+        }
+    }
+
+    /// Generate the initialization code statements for this platform (if any).
+    fn make_init_code(self) -> proc_macro2::TokenStream {
+        match self {
+            Platform::EnvLogging => quote! {
+                let _ = env_logger::builder().is_test(true).try_init();
+            },
+            Platform::EnvTracing => quote! {
+                let subscriber = tracing_subscriber::FmtSubscriber::builder()
+                    .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+                    .with_test_writer()
+                    .finish();
+                let _ = tracing::subscriber::set_global_default(subscriber);
+            },
+            _ => Default::default(),
+        }
+    }
 }
 
-const DEFAULT_PLATFORMS: [Platform; 2] = [Platform::Default, Platform::Wasm];
-
+/// See the [crate] docs for usage information.
 #[proc_macro_attribute]
 pub fn multiplatform_test(attr: TokenStream, body: TokenStream) -> TokenStream {
     let ts = multiplatform_test_impl(
@@ -28,46 +96,62 @@ fn multiplatform_test_impl(
     let mut platforms = Vec::<Platform>::new();
 
     while let Some(token) = attr.next() {
-        let platform = match &token {
-            proc_macro2::TokenTree::Ident(i) => match i.to_string().as_str() {
-                "default" => Platform::Default,
-                "wasm" => Platform::Wasm,
-                other => panic!(
-                    "unknown platform {}; want platform in [default, wasm]",
-                    other
-                ),
-            },
-            _ => panic!("malformed #[multiplatform_test] attribute; expected identifier"),
+        let proc_macro2::TokenTree::Ident(i) = &token else {
+            return quote_spanned! {token.span()=>
+                compile_error!("malformed #[multiplatform_test] attribute; expected identifier.");
+            };
+        };
+        let name = i.to_string();
+        let Some(&platform) = Platform::ALL
+            .iter()
+            .find(|platform| name == platform.name())
+        else {
+            let msg = proc_macro2::Literal::string(&format!(
+                "unknown platform {}; expected one of [{}]",
+                name,
+                Platform::ALL.map(Platform::name).join(", "),
+            ));
+            return quote_spanned! {token.span()=> compile_error!(#msg); };
         };
         platforms.push(platform);
 
         match &attr.next() {
             Some(proc_macro2::TokenTree::Punct(op)) if op.as_char() == ',' => {}
-            Some(_) => panic!("malformed `#[multiplatform_test]` attribute; expected `,`"),
+            Some(other) => {
+                return quote_spanned! {other.span()=>
+                    compile_error!("malformed `#[multiplatform_test]` attribute; expected `,`.");
+                };
+            }
             None => break,
         }
     }
-
-    let mut output = Vec::<proc_macro2::TokenStream>::new();
-
     if platforms.is_empty() {
-        platforms.extend(DEFAULT_PLATFORMS.iter());
+        platforms.extend(Platform::DEFAULT.iter());
     }
+
+    let mut output = proc_macro2::TokenStream::new();
+    let mut init_code = proc_macro2::TokenStream::new();
 
     for p in platforms {
-        let tokens = match p {
-            Platform::Default => quote! { #[test] },
-            // Fully specify wasm_bindgen_test so that the consumer does not need to add another
-            // use statement. They still need to depend on the wasm_bindgen_test_macro crate,
-            // though.
-            Platform::Wasm => quote! { #[wasm_bindgen_test_macro::wasm_bindgen_test] },
-        };
-        output.push(tokens);
+        output.extend(p.make_attribute());
+        init_code.extend(p.make_init_code());
     }
 
-    output.push(body);
+    if init_code.is_empty() {
+        output.extend(body);
+    } else {
+        let mut body_head = body.into_iter().collect::<Vec<_>>();
+        let Some(proc_macro2::TokenTree::Group(body_code)) = body_head.pop() else { panic!(); };
 
-    output.into_iter().collect()
+        output.extend(body_head);
+        output.extend(quote! {
+            {
+                { #init_code };
+                #body_code
+            }
+        });
+    }
+    output
 }
 
 #[cfg(test)]
@@ -76,12 +160,12 @@ mod tests {
 
     #[test]
     fn test_default_platforms() {
-        let test_fn = quote! { fn test() { } };
+        let test_fn: proc_macro2::TokenStream = quote! { fn test() { } };
         let attrs = proc_macro2::TokenStream::new();
-        let got = multiplatform_test_impl(attrs, test_fn);
+        let got: proc_macro2::TokenStream = multiplatform_test_impl(attrs, test_fn);
         let want = quote! {
             #[test]
-            #[wasm_bindgen_test_macro::wasm_bindgen_test]
+            #[wasm_bindgen_test::wasm_bindgen_test]
             fn test() { }
         };
 
@@ -91,7 +175,7 @@ mod tests {
     #[test]
     fn test_host_platform() {
         let test_fn = quote! { fn test() { } };
-        let attrs = quote! { default };
+        let attrs = quote! { test };
         let got = multiplatform_test_impl(attrs, test_fn);
         let want = quote! {
             #[test]
@@ -107,7 +191,7 @@ mod tests {
         let attrs = quote! { wasm };
         let got = multiplatform_test_impl(attrs, test_fn);
         let want = quote! {
-            #[wasm_bindgen_test_macro::wasm_bindgen_test]
+            #[wasm_bindgen_test::wasm_bindgen_test]
             fn test() { }
         };
 
@@ -117,11 +201,11 @@ mod tests {
     #[test]
     fn test_host_wasm_platform() {
         let test_fn = quote! { fn test() { } };
-        let attrs = quote! { default, wasm };
+        let attrs = quote! { test, wasm };
         let got = multiplatform_test_impl(attrs, test_fn);
         let want = quote! {
             #[test]
-            #[wasm_bindgen_test_macro::wasm_bindgen_test]
+            #[wasm_bindgen_test::wasm_bindgen_test]
             fn test() { }
         };
 
@@ -129,26 +213,26 @@ mod tests {
     }
 
     #[test]
-    #[should_panic(expected = "unknown platform")]
     fn test_unknown_platform() {
         let test_fn = quote! { fn test() { } };
         let attrs = quote! { hello };
-        multiplatform_test_impl(attrs, test_fn);
+        let got = multiplatform_test_impl(attrs, test_fn);
+        assert!(got.to_string().starts_with("compile_error !"));
     }
 
     #[test]
-    #[should_panic(expected = "expected `,`")]
     fn test_invalid_attr_nocomma_platform() {
         let test_fn = quote! { fn test() { } };
         let attrs = quote! { wasm() };
-        multiplatform_test_impl(attrs, test_fn);
+        let got = multiplatform_test_impl(attrs, test_fn);
+        assert!(got.to_string().starts_with("compile_error !"));
     }
 
     #[test]
-    #[should_panic(expected = "expected identifier")]
     fn test_invalid_attr_noident_platform() {
         let test_fn = quote! { fn test() { } };
         let attrs = quote! { () };
-        multiplatform_test_impl(attrs, test_fn);
+        let got = multiplatform_test_impl(attrs, test_fn);
+        assert!(got.to_string().starts_with("compile_error !"));
     }
 }
