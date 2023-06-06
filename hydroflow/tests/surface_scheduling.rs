a23381854a45f9c5791bd399dd633fee291d400a
Mingwei Samuel <mingwei.samuel@gmail.com>
2023-06-06 13:13:13 -0700

    feat(hydroflow): add basic tracing support, use in (some) tests. (#749)

--- hydroflow/tests/surface_scheduling.rs
+++ hydroflow/tests/surface_scheduling.rs
@@ -1,7 +1,7 @@
 use hydroflow::hydroflow_syntax;
 use multiplatform_test::multiplatform_test;
 
-#[multiplatform_test]
+#[multiplatform_test(test, wasm, env_tracing)]
 pub fn test_stratum_loop() {
     let (out_send, mut out_recv) = hydroflow::util::unbounded_channel::<usize>();
 
@@ -20,7 +20,7 @@ pub fn test_stratum_loop() {
     assert_eq!((11, 0), (df.current_tick(), df.current_stratum()));
 }
 
-#[multiplatform_test]
+#[multiplatform_test(test, wasm, env_tracing)]
 pub fn test_tick_loop() {
     let (out_send, mut out_recv) = hydroflow::util::unbounded_channel::<usize>();
 
