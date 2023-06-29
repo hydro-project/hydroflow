30621f1e203e6ec29073386c30fec3ecb5d65486
zzlk <2418897+zzlk@users.noreply.github.com>
2023-06-29 14:09:21 -0700

    fix(join)!: make join default to multiset join (#774)

--- hydroflow/examples/example_6_unreachability.rs
+++ hydroflow/examples/example_6_unreachability.rs
@@ -7,26 +7,26 @@ pub fn main() {
     let mut flow = hydroflow_syntax! {
         origin = source_iter(vec![0]);
         stream_of_edges = source_stream(pairs_recv) -> tee();
-        reached_vertices = union()->tee();
-        origin -> [0]reached_vertices;
+        reached_vertices = union() -> unique() -> tee();
+        origin -> reached_vertices;
 
         // the join for reachable vertices
         my_join = join() -> flat_map(|(src, ((), dst))| [src, dst]);
-        reached_vertices[0] -> map(|v| (v, ())) -> [0]my_join;
+        reached_vertices -> map(|v| (v, ())) -> [0]my_join;
         stream_of_edges[1] -> [1]my_join;
 
         // the loop
-        my_join -> [1]reached_vertices;
+        my_join -> reached_vertices;
 
         // the difference all_vertices - reached_vertices
         all_vertices = stream_of_edges[0]
           -> flat_map(|(src, dst)| [src, dst]) -> tee();
         unreached_vertices = difference();
-        all_vertices[0] -> [pos]unreached_vertices;
-        reached_vertices[1] -> [neg]unreached_vertices;
+        all_vertices -> [pos]unreached_vertices;
+        reached_vertices -> [neg]unreached_vertices;
 
         // the output
-        all_vertices[1] -> unique() -> for_each(|v| println!("Received vertex: {}", v));
+        all_vertices -> for_each(|v| println!("Received vertex: {}", v));
         unreached_vertices -> for_each(|v| println!("unreached_vertices vertex: {}", v));
     };
 
