0695a56a7609e85156b9d52b152495908137637e
zzlk <2418897+zzlk@users.noreply.github.com>
2023-07-11 11:19:11 -0700

    fix: rename next_tick -> defer, batch -> defer_signal

--- hydroflow/tests/compile-fail/surface_badgeneric_type.rs
+++ hydroflow/tests/compile-fail/surface_badgeneric_type.rs
@@ -2,8 +2,8 @@ use hydroflow::hydroflow_syntax;
 
 fn main() {
     let mut df = hydroflow_syntax! {
-        // no generic arguments for `next_tick`.
-        source_iter(0..10) -> next_tick::<usize>() -> for_each(std::mem::drop);
+        // no generic arguments for `defer`.
+        source_iter(0..10) -> defer_tick::<usize>() -> for_each(std::mem::drop);
     };
     df.run_available();
 }
