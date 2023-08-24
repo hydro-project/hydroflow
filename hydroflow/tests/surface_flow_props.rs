200ddf01ea3f4b5a9f172297bf5fca8d56951362
Mingwei Samuel <mingwei.samuel@gmail.com>
2023-08-24 09:10:42 -0700

    feat(hydroflow_lang): add `cast` operator (#882)

--- hydroflow/tests/surface_flow_props.rs
+++ hydroflow/tests/surface_flow_props.rs
@@ -8,9 +8,17 @@ use lattices::set_union::SetUnion;
 #[test]
 pub fn test_basic() -> Result<(), Box<dyn Error>> {
     let mut hf = hydroflow_syntax! {
-        source_iter_delta((0..10).map(SetUnionSingletonSet::new_from))
+        my_tee = source_iter_delta((0..10).map(SetUnionSingletonSet::new_from))
             -> map(|SetUnion(SingletonSet(x))| SetUnion(SingletonSet(x + 5)))
-            -> for_each(|s| println!("{:?}", s));
+            -> tee();
+
+        my_tee
+            -> cast(None)
+            -> map(|SetUnion(SingletonSet(x))| 10 * x)
+            -> for_each(|x| println!("seq {:?}", x));
+
+        my_tee
+            -> for_each(|s| println!("delta {:?}", s));
     };
     hf.run_available();
 
