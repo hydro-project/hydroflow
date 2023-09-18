9353370f16ba1b9ba968e7b7f058ffb8af04e5ef
Saikrishna Achalla <saikrishnaachalla@Saikrishnas-MacBook-Pro.local>
2023-09-18 13:14:28 -0700

    feat(topolotree): new implementation and Hydro Deploy setup
    --
    
    
    Co-authored-by: zzlk <2418897+zzlk@users.noreply.github.com>
    Co-authored-by: Saikrishna Achalla <saikrishnaachalla@Saikrishnas-MacBook-Pro.local>

--- topolotree/src/pn_delta.rs
+++ topolotree/src/pn_delta.rs
@@ -4,23 +4,20 @@ use std::ops::Deref;
 use std::rc::Rc;
 
 use hydroflow::serde::{Deserialize, Serialize};
-use hydroflow::util::cli::{ConnectedDemux, ConnectedDirect, ConnectedSink, ConnectedSource};
+use hydroflow::util::cli::{ConnectedDemux, ConnectedDirect, ConnectedSink, ConnectedSource, ConnectedTagged};
 use hydroflow::util::{deserialize_from_bytes, serialize_to_bytes};
 use hydroflow::{hydroflow_syntax, tokio};
 
-#[derive(Serialize, Deserialize, Clone, Debug)]
-struct IncrementRequest {
-    tweet_id: u64,
-    likes: i32,
-}
+mod protocol;
+use protocol::*;
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 enum GossipOrIncrement {
-    Gossip(Vec<(u64, (usize, u32, u32))>),
-    Increment(u64, i32),
+    Gossip(Vec<(u64, (usize, u64, u64))>),
+    Increment(u64, i64),
 }
 
-type NextStateType = (u64, bool, Rc<RefCell<(Vec<u32>, Vec<u32>)>>);
+type NextStateType = (u64, bool, Rc<RefCell<(Vec<u64>, Vec<u64>)>>);
 
 #[hydroflow::main]
 async fn main() {
@@ -51,7 +48,7 @@ async fn main() {
 
     let from_peer = ports
         .port("from_peer")
-        .connect::<ConnectedDirect>()
+        .connect::<ConnectedTagged<ConnectedDirect>>()
         .await
         .into_source();
 
@@ -67,7 +64,7 @@ async fn main() {
 
     let df = hydroflow_syntax! {
         next_state = union()
-            -> fold::<'static>((HashMap::<u64, Rc<RefCell<(Vec<u32>, Vec<u32>)>>>::new(), HashMap::new(), 0), |(cur_state, modified_tweets, last_tick): &mut (HashMap<_, _>, HashMap<_, _>, _), goi| {
+            -> fold::<'static>((HashMap::<u64, Rc<RefCell<(Vec<u64>, Vec<u64>)>>>::new(), HashMap::new(), 0), |(cur_state, modified_tweets, last_tick): &mut (HashMap<_, _>, HashMap<_, _>, _), goi| {
                 if context.current_tick() != *last_tick {
                     modified_tweets.clear();
                 }
@@ -99,9 +96,9 @@ async fn main() {
                         let mut cur_value = cur_value.as_ref().borrow_mut();
 
                         if delta > 0 {
-                            cur_value.0[my_id] += delta as u32;
+                            cur_value.0[my_id] += delta as u64;
                         } else {
-                            cur_value.1[my_id] += (-delta) as u32;
+                            cur_value.1[my_id] += (-delta) as u64;
                         }
 
                         *modified_tweets.entry(counter_id).or_insert(false) |= true;
@@ -116,12 +113,12 @@ async fn main() {
             -> tee();
 
         source_stream(from_peer)
-            -> map(|x| deserialize_from_bytes::<GossipOrIncrement>(&x.unwrap()).unwrap())
+            -> map(|x| deserialize_from_bytes::<GossipOrIncrement>(&x.unwrap().1).unwrap())
             -> next_state;
 
         source_stream(increment_requests)
-            -> map(|x| deserialize_from_bytes::<IncrementRequest>(&x.unwrap()).unwrap())
-            -> map(|t| GossipOrIncrement::Increment(t.tweet_id, t.likes))
+            -> map(|x| deserialize_from_bytes::<OperationPayload>(&x.unwrap()).unwrap())
+            -> map(|t| GossipOrIncrement::Increment(t.key, t.change))
             -> next_state;
 
         all_peers = source_iter(0..num_replicas)
@@ -144,10 +141,13 @@ async fn main() {
                 a.into_iter().map(|(k, _, rc_array)| {
                     let rc_borrowed = rc_array.as_ref().borrow();
                     let (pos, neg) = rc_borrowed.deref();
-                    (k, pos.iter().sum::<u32>() as i32 - neg.iter().sum::<u32>() as i32)
+                    QueryResponse {
+                        key: k,
+                        value: pos.iter().sum::<u64>() as i64 - neg.iter().sum::<u64>() as i64
+                    }
                 }).collect::<Vec<_>>()
             })
-            -> map(serialize_to_bytes::<(u64, i32)>)
+            -> map(serialize_to_bytes::<QueryResponse>)
             -> dest_sink(query_responses);
     };
 
