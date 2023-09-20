849d8dda8eae244bfd4619a192d8339e870ac41e
Mingwei Samuel <mingwei.samuel@gmail.com>
2023-09-19 23:43:28 -0700

    refactor(hydroflow_lang): `demux_enum` requires enum type name, add better error handling

--- hydroflow/src/util/demux_enum.rs
+++ hydroflow/src/util/demux_enum.rs
@@ -1,12 +1,31 @@
 pub use hydroflow_macro::DemuxEnum;
 use pusherator::demux::PusheratorList;
 use pusherator::for_each::ForEach;
-use pusherator::Pusherator;
-use variadics::{var_args, var_expr, var_type};
+use pusherator::{Pusherator, PusheratorBuild};
+use variadics::{var_args, var_expr, var_type, Variadic};
 
-pub trait DemuxEnum<Nexts>
+// TODO(mingwei): doc this trait should be derived only.
+pub trait DemuxEnum<Nexts>: DemuxEnumItems
 where
-    Nexts: PusheratorList,
+    Nexts: PusheratorListForItems<Self::Items>,
 {
     fn demux_enum(self, outputs: &mut Nexts);
 }
+
+pub trait DemuxEnumItems {
+    type Items: Variadic;
+}
+
+pub trait PusheratorListForItems<Items>: PusheratorList
+where
+    Items: Variadic,
+{
+}
+impl<HeadPush, RestPush, Head, Rest> PusheratorListForItems<(Head, Rest)> for (HeadPush, RestPush)
+where
+    HeadPush: Pusherator<Item = Head>,
+    RestPush: PusheratorListForItems<Rest>,
+    Rest: Variadic,
+{
+}
+impl PusheratorListForItems<()> for () {}
