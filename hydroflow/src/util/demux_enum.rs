bf3bd509193c539bb2fc3ccf80b66c2bcf251d1b
Mingwei Samuel <mingwei.samuel@gmail.com>
2023-09-22 13:05:51 -0700

    feat(hydroflow_lang): Update documentation and improve error messages for `demux_enum` operator

--- hydroflow/src/util/demux_enum.rs
+++ hydroflow/src/util/demux_enum.rs
@@ -1,21 +1,33 @@
+//! Traits for the `demux_enum` derive and operator.
+
 pub use hydroflow_macro::DemuxEnum;
 use pusherator::demux::PusheratorList;
-use pusherator::for_each::ForEach;
-use pusherator::{Pusherator, PusheratorBuild};
-use variadics::{var_args, var_expr, var_type, Variadic};
+use pusherator::Pusherator;
+use variadics::Variadic;
 
-// TODO(mingwei): doc this trait should be derived only.
+/// Trait for use with the `demux_enum` operator.
+///
+/// This trait is meant to be derived: `#[derive(DemuEnum)]`.
 pub trait DemuxEnum<Nexts>: DemuxEnumItems
 where
     Nexts: PusheratorListForItems<Self::Items>,
 {
+    /// Pushes self into the corresponding output pusherator.
     fn demux_enum(self, outputs: &mut Nexts);
 }
 
+/// Fixed output item list for [`DemuxEnum`].
+///
+/// This trait is meant to be derived: `#[derive(DemuEnum)]`.
 pub trait DemuxEnumItems {
+    /// A `var_type!(...)` list of items corresponding to each variant's output type.
     type Items: Variadic;
 }
 
+/// Helper trait to bound a [`PusheratorList`] variadic to some coresponding item list variadic.
+///
+/// A pusherator list `var_type!(PushA, PushB, PushC)` implements `PusheratorListForItems<var_type!(ItemA, ItemB, ItemC)>`,
+/// where `PushA: Pusherator<Item = ItemA>`, etc.
 pub trait PusheratorListForItems<Items>: PusheratorList
 where
     Items: Variadic,
---
+++ hydroflow/tests/compile-fail/surface_demuxenum_wrongitems.rs
@@ -0,0 +1,20 @@
+use hydroflow::util::demux_enum::DemuxEnum;
+use hydroflow::{hydroflow_syntax, var_args};
+
+fn main() {
+    #[derive(DemuxEnum)]
+    enum Shape {
+        Square(f64),
+        Rectangle { w: f64, h: f64 },
+        Circle { r: f64 },
+    }
+
+    let mut df = hydroflow_syntax! {
+        my_demux = source_iter(0..5) -> demux_enum::<Shape>();
+        my_demux[Rectangle] -> for_each(std::mem::drop);
+        my_demux[Circle] -> for_each(std::mem::drop);
+        my_demux[Square] -> for_each(std::mem::drop);
+        my_demux[Square] -> for_each(std::mem::drop);
+    };
+    df.run_available();
+}
