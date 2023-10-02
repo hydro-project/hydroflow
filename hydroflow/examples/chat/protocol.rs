3c8dbd709fa5a1c1a458d2aa5e2be9ecd8fc4b49
Mingwei Samuel <mingwei.samuel@gmail.com>
2023-10-02 12:09:18 -0700

    refactor(hydroflow): update `chat` example to use `demux_enum` (#918)

--- hydroflow/examples/chat/protocol.rs
+++ hydroflow/examples/chat/protocol.rs
@@ -1,7 +1,10 @@
+use std::net::SocketAddr;
+
 use chrono::prelude::*;
+use hydroflow_macro::DemuxEnum;
 use serde::{Deserialize, Serialize};
 
-#[derive(PartialEq, Eq, Clone, Serialize, Deserialize, Debug)]
+#[derive(PartialEq, Eq, Clone, Serialize, Deserialize, Debug, DemuxEnum)]
 pub enum Message {
     ConnectRequest,
     ConnectResponse,
@@ -10,4 +13,38 @@ pub enum Message {
         message: String,
         ts: DateTime<Utc>,
     },
+}
+
+#[derive(Clone, Debug, DemuxEnum)]
+pub enum MessageWithAddr {
+    ConnectRequest {
+        addr: SocketAddr,
+    },
+    ConnectResponse {
+        addr: SocketAddr,
+    },
+    ChatMsg {
+        addr: SocketAddr,
+        nickname: String,
+        message: String,
+        ts: DateTime<Utc>,
+    },
+}
+impl MessageWithAddr {
+    pub fn from_message(message: Message, addr: SocketAddr) -> Self {
+        match message {
+            Message::ConnectRequest => Self::ConnectRequest { addr },
+            Message::ConnectResponse => Self::ConnectResponse { addr },
+            Message::ChatMsg {
+                nickname,
+                message,
+                ts,
+            } => Self::ChatMsg {
+                addr,
+                nickname,
+                message,
+                ts,
+            },
+        }
+    }
 }
