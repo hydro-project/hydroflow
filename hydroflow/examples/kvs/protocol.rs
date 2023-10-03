b0c43c75954741edffe7e4e12909697e75ab1d26
Mingwei Samuel <mingwei.samuel@gmail.com>
2023-10-03 13:19:09 -0700

    refactor(hydroflow): refactor `kvs` example, cleanup, use `demux_enum` (#918)

--- hydroflow/examples/kvs/protocol.rs
+++ hydroflow/examples/kvs/protocol.rs
@@ -1,8 +1,37 @@
+use std::net::SocketAddr;
+
+use hydroflow_macro::DemuxEnum;
 use serde::{Deserialize, Serialize};
 
-#[derive(PartialEq, Eq, Clone, Serialize, Deserialize, Debug)]
-pub enum KVSMessage {
+#[derive(Clone, Debug, Serialize, Deserialize, DemuxEnum)]
+pub enum KvsMessage {
     Put { key: String, value: String },
     Get { key: String },
-    Response { key: String, value: String },
+}
+
+#[derive(Clone, Debug, DemuxEnum)]
+pub enum KvsMessageWithAddr {
+    Put {
+        key: String,
+        value: String,
+        addr: SocketAddr,
+    },
+    Get {
+        key: String,
+        addr: SocketAddr,
+    },
+}
+impl KvsMessageWithAddr {
+    pub fn from_message(message: KvsMessage, addr: SocketAddr) -> Self {
+        match message {
+            KvsMessage::Put { key, value } => Self::Put { key, value, addr },
+            KvsMessage::Get { key } => Self::Get { key, addr },
+        }
+    }
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct KvsResponse {
+    pub key: String,
+    pub value: String,
 }
