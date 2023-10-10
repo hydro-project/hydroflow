3ac9b1634223b50deeebb5535205427b5a9aa201
Mingwei Samuel <mingwei.samuel@gmail.com>
2023-10-09 20:35:49 -0700

    fix(hydroflow): fix example `kvs`/`kvs_replicated` tests, improve comments (#932)

--- hydroflow/examples/kvs/server.rs
+++ hydroflow/examples/kvs/server.rs
@@ -9,30 +9,27 @@ pub(crate) async fn run_server(outbound: UdpSink, inbound: UdpStream, opts: Opts
     println!("Server live!");
 
     let mut hf: Hydroflow = hydroflow_syntax! {
-        // network channels
-        network_send = union() -> dest_sink_serde(outbound);
+        // Setup network channels.
+        network_send = dest_sink_serde(outbound);
         network_recv = source_stream_serde(inbound)
             -> _upcast(Some(Delta))
             -> map(Result::unwrap)
             -> inspect(|(msg, addr)| println!("Message received {:?} from {:?}", msg, addr))
             -> map(|(msg, addr)| KvsMessageWithAddr::from_message(msg, addr))
             -> demux_enum::<KvsMessageWithAddr>();
-        puts = network_recv[Put] -> tee();
+        puts = network_recv[Put];
         gets = network_recv[Get];
 
-        // ack puts
-        puts -> map(|(key, value, client_addr)| (KvsResponse { key, value }, client_addr)) -> [0]network_send;
-
-        // join PUTs and GETs by key
+        // Join PUTs and GETs by key, persisting the PUTs.
         puts -> map(|(key, value, _addr)| (key, value)) -> [0]lookup;
         gets -> [1]lookup;
         lookup = join::<'static, 'tick>();
 
-        // network_send lookup responses back to the client address from the GET
-        lookup[1]
+        // Send GET responses back to the client address.
+        lookup
             -> inspect(|tup| println!("Found a match: {:?}", tup))
             -> map(|(key, (value, client_addr))| (KvsResponse { key, value }, client_addr))
-            -> [1]network_send;
+            -> network_send;
     };
 
     if let Some(graph) = opts.graph {
