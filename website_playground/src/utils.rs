49d1ef03f9d86aeb6c73222f387ce420165af352
Mingwei Samuel <mingwei.samuel@gmail.com>
2023-10-10 23:20:56 -0700

    fix(website_playground): uncomment needed `console_error_panic_hook` feature gate (#938)

--- website_playground/src/utils.rs
+++ website_playground/src/utils.rs
@@ -5,6 +5,6 @@ pub fn set_panic_hook() {
     //
     // For more details see
     // https://github.com/rustwasm/console_error_panic_hook#readme
-    //#[cfg(feature = "console_error_panic_hook")]
+    #[cfg(feature = "console_error_panic_hook")]
     console_error_panic_hook::set_once();
 }
