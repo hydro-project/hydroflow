172a58b4a2b0e33460c1b25c82d01304e51c68af
Shadaj Laddad <shadaj@users.noreply.github.com>
2023-10-19 14:49:09 -0700

    feat(hydroflow): prototype a functional surface syntax using staging

--- hydroflow/examples/rga/minimal.rs
+++ hydroflow/examples/rga/minimal.rs
@@ -9,7 +9,7 @@ pub(crate) fn rga_minimal(
     input_recv: UnboundedReceiverStream<(Token, Timestamp)>,
     rga_send: UnboundedSender<(Token, Timestamp)>,
     _list_send: UnboundedSender<(Timestamp, Timestamp)>,
-) -> Hydroflow {
+) -> Hydroflow<'static> {
     hydroflow_syntax! {
         insertAfter = source_stream(input_recv);
 
