961c997fafc74faea40260393e222b5561ccbe03
Shadaj Laddad <shadaj@users.noreply.github.com>
2023-12-18 17:13:34 -0800

    feat(hydroflow): support building graphs for symmetric clusters in Hydroflow+

--- hydroflow_plus_cli_integration/src/runtime.rs
+++ hydroflow_plus_cli_integration/src/runtime.rs
@@ -2,17 +2,25 @@ use std::cell::RefCell;
 use std::rc::Rc;
 
 use hydroflow_plus::lang::parse::Pipeline;
-use hydroflow_plus::node::{HfDeploy, HfNode, HfNodeBuilder, HfSendTo};
-use hydroflow_plus::util::cli::HydroCLI;
+use hydroflow_plus::node::{
+    HfCluster, HfClusterBuilder, HfDemuxTo, HfDeploy, HfNode, HfNodeBuilder, HfSendTo,
+};
+use hydroflow_plus::util::cli::{
+    ConnectedDemux, ConnectedDirect, ConnectedSink, ConnectedSource, HydroCLI,
+};
 use hydroflow_plus::HfBuilder;
-use stageleft::internal::{quote, Span};
-use stageleft::{Quoted, RuntimeData};
+use stageleft::{q, Quoted, RuntimeData};
 use syn::parse_quote;
+
+use super::HydroflowPlusMeta;
 
 pub struct CLIRuntime {}
 
 impl<'a> HfDeploy<'a> for CLIRuntime {
     type Node = CLIRuntimeNode<'a>;
+    type Cluster = CLIRuntimeCluster<'a>;
+    type Meta = String;
+    type RuntimeID = usize;
 }
 
 #[derive(Clone)]
@@ -20,11 +28,12 @@ pub struct CLIRuntimeNode<'a> {
     id: usize,
     builder: &'a HfBuilder<'a, CLIRuntime>,
     next_port: Rc<RefCell<usize>>,
-    cli: RuntimeData<&'a HydroCLI>,
+    cli: RuntimeData<&'a HydroCLI<HydroflowPlusMeta>>,
 }
 
 impl<'a> HfNode<'a> for CLIRuntimeNode<'a> {
     type Port = String;
+    type Meta = String;
 
     fn id(&self) -> usize {
         self.id
@@ -40,78 +49,140 @@ impl<'a> HfNode<'a> for CLIRuntimeNode<'a> {
         format!("port_{}", next_send_port)
     }
 
-    fn gen_source_statement(&self, port: &String) -> Pipeline {
-        let hydroflow_crate = proc_macro_crate::crate_name("hydroflow_plus")
-            .expect("hydroflow_plus should be present in `Cargo.toml`");
-        let root = match hydroflow_crate {
-            proc_macro_crate::FoundCrate::Itself => quote! { hydroflow_plus },
-            proc_macro_crate::FoundCrate::Name(name) => {
-                let ident = syn::Ident::new(&name, Span::call_site());
-                quote! { #ident }
-            }
-        };
+    fn build(&mut self, _meta: &Option<Self::Meta>) {}
+}
 
-        let self_cli_splice = self.cli.splice();
-        parse_quote! {
-            source_stream({
-                use #root::util::cli::ConnectedSource;
-                #self_cli_splice
-                    .port(#port)
-                    .connect_local_blocking::<#root::util::cli::ConnectedDirect>()
-                    .into_source()
-            })
-        }
+#[derive(Clone)]
+pub struct CLIRuntimeCluster<'a> {
+    id: usize,
+    builder: &'a HfBuilder<'a, CLIRuntime>,
+    next_port: Rc<RefCell<usize>>,
+    cli: RuntimeData<&'a HydroCLI<HydroflowPlusMeta>>,
+}
+
+impl<'a> HfNode<'a> for CLIRuntimeCluster<'a> {
+    type Port = String;
+    type Meta = String;
+
+    fn id(&self) -> usize {
+        self.id
     }
 
-    fn gen_sink_statement(&self, port: &String) -> Pipeline {
-        let hydroflow_crate = proc_macro_crate::crate_name("hydroflow_plus")
-            .expect("hydroflow_plus should be present in `Cargo.toml`");
-        let root = match hydroflow_crate {
-            proc_macro_crate::FoundCrate::Itself => quote! { hydroflow_plus },
-            proc_macro_crate::FoundCrate::Name(name) => {
-                let ident = syn::Ident::new(&name, Span::call_site());
-                quote! { #ident }
-            }
-        };
+    fn graph_builder(&self) -> (&'a RefCell<usize>, &'a hydroflow_plus::builder::Builders) {
+        self.builder.builder_components()
+    }
 
-        let self_cli_splice = self.cli.splice();
-        parse_quote! {
-            dest_sink({
-                use #root::util::cli::ConnectedSink;
-                #self_cli_splice
-                    .port(#port)
-                    .connect_local_blocking::<#root::util::cli::ConnectedDirect>()
-                    .into_sink()
-            })
-        }
+    fn next_port(&self) -> String {
+        let next_send_port = *self.next_port.borrow();
+        *self.next_port.borrow_mut() += 1;
+        format!("port_{}", next_send_port)
+    }
+
+    fn build(&mut self, _meta: &Option<Self::Meta>) {}
+}
+
+impl<'a> HfCluster<'a> for CLIRuntimeCluster<'a> {
+    fn ids(&self) -> impl Quoted<'a, &'a Vec<u32>> {
+        let cli = self.cli;
+        let self_id = self.id;
+        q!(cli.meta.as_ref().unwrap().clusters.get(&self_id).unwrap())
     }
 }
 
 impl<'a> HfSendTo<'a, CLIRuntimeNode<'a>> for CLIRuntimeNode<'a> {
     fn send_to(&self, _other: &CLIRuntimeNode, _source_port: &String, _recipient_port: &String) {}
-}
 
-pub struct CLIRuntimeNodeBuilder<'a> {
-    cli: RuntimeData<&'a HydroCLI>,
-}
+    fn gen_sink_statement(&self, port: &String) -> Pipeline {
+        let self_cli = self.cli;
+        let port = port.as_str();
+        let sink_quote = q!({
+            self_cli
+                .port(port)
+                .connect_local_blocking::<ConnectedDirect>()
+                .into_sink()
+        })
+        .splice();
 
-impl CLIRuntimeNodeBuilder<'_> {
-    pub fn new(cli: RuntimeData<&HydroCLI>) -> CLIRuntimeNodeBuilder {
-        CLIRuntimeNodeBuilder { cli }
+        parse_quote!(dest_sink(#sink_quote))
+    }
+
+    fn gen_source_statement(other: &CLIRuntimeNode<'a>, port: &String) -> Pipeline {
+        let self_cli = other.cli;
+        let port = port.as_str();
+        let source_quote = q!({
+            self_cli
+                .port(port)
+                .connect_local_blocking::<ConnectedDirect>()
+                .into_source()
+        })
+        .splice();
+
+        parse_quote!(source_stream(#source_quote))
     }
 }
 
-impl<'cli> HfNodeBuilder<'cli, CLIRuntime> for CLIRuntimeNodeBuilder<'cli> {
-    fn build(
-        &mut self,
-        id: usize,
-        builder: &'cli HfBuilder<'cli, CLIRuntime>,
-    ) -> CLIRuntimeNode<'cli> {
+impl<'a> HfDemuxTo<'a, CLIRuntimeCluster<'a>> for CLIRuntimeNode<'a> {
+    fn demux_to(
+        &self,
+        _other: &CLIRuntimeCluster,
+        _source_port: &String,
+        _recipient_port: &String,
+    ) {
+    }
+
+    fn gen_sink_statement(&self, port: &String) -> Pipeline {
+        let self_cli = self.cli;
+        let port = port.as_str();
+
+        let sink_quote = q!({
+            self_cli
+                .port(port)
+                .connect_local_blocking::<ConnectedDemux<ConnectedDirect>>()
+                .into_sink()
+        })
+        .splice();
+
+        parse_quote!(dest_sink(#sink_quote))
+    }
+
+    fn gen_source_statement(other: &CLIRuntimeCluster<'a>, port: &String) -> Pipeline {
+        let self_cli = other.cli;
+        let port = port.as_str();
+
+        let source_quote = q!({
+            self_cli
+                .port(port)
+                .connect_local_blocking::<ConnectedDirect>()
+                .into_source()
+        })
+        .splice();
+
+        parse_quote!(source_stream(#source_quote))
+    }
+}
+
+impl<'cli> HfNodeBuilder<'cli, CLIRuntime> for RuntimeData<&'cli HydroCLI<HydroflowPlusMeta>> {
+    fn build(&self, id: usize, builder: &'cli HfBuilder<'cli, CLIRuntime>) -> CLIRuntimeNode<'cli> {
         CLIRuntimeNode {
             id,
             builder,
             next_port: Rc::new(RefCell::new(0)),
-            cli: self.cli,
+            cli: *self,
+        }
+    }
+}
+
+impl<'cli> HfClusterBuilder<'cli, CLIRuntime> for RuntimeData<&'cli HydroCLI<HydroflowPlusMeta>> {
+    fn build(
+        &self,
+        id: usize,
+        builder: &'cli HfBuilder<'cli, CLIRuntime>,
+    ) -> CLIRuntimeCluster<'cli> {
+        CLIRuntimeCluster {
+            id,
+            builder,
+            next_port: Rc::new(RefCell::new(0)),
+            cli: *self,
         }
     }
 }
