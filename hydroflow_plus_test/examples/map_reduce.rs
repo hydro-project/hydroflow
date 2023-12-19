9e7f646e2ff2c9b41a0c3bb992c93cd0387b6b05
Shadaj Laddad <shadaj@users.noreply.github.com>
2023-12-18 19:05:34 -0800

    feat(hydroflow): support building graphs for symmetric clusters in Hydroflow+

--- hydroflow_plus_test/examples/map_reduce.rs
+++ hydroflow_plus_test/examples/map_reduce.rs
@@ -1,8 +1,9 @@
+use std::cell::RefCell;
 use std::sync::Arc;
 
 use hydro_cli::core::gcp::GCPNetwork;
 use hydro_cli::core::{Deployment, Host};
-use hydroflow_plus_cli_integration::CLIDeployNodeBuilder;
+use hydroflow_plus_cli_integration::{CLIDeployClusterBuilder, CLIDeployNodeBuilder};
 use tokio::sync::RwLock;
 
 type HostCreator = Box<dyn Fn(&mut Deployment) -> Arc<RwLock<dyn Host>>>;
@@ -10,7 +11,7 @@ type HostCreator = Box<dyn Fn(&mut Deployment) -> Arc<RwLock<dyn Host>>>;
 // run with no args for localhost, with `gcp <GCP PROJECT>` for GCP
 #[tokio::main]
 async fn main() {
-    let mut deployment = Deployment::new();
+    let deployment = RefCell::new(Deployment::new());
     let host_arg = std::env::args().nth(1).unwrap_or_default();
 
     let (create_host, profile): (HostCreator, Option<String>) = if host_arg == *"gcp" {
@@ -31,7 +32,7 @@ async fn main() {
             None,
         )
     } else {
-        let localhost = deployment.Localhost();
+        let localhost = deployment.borrow_mut().Localhost();
         (
             Box::new(move |_| -> Arc<RwLock<dyn Host>> { localhost.clone() }),
             Some("dev".to_string()),
@@ -39,24 +40,46 @@ async fn main() {
     };
 
     let builder = hydroflow_plus::HfBuilder::new();
-    hydroflow_plus_test::first_ten::first_ten_distributed(
+    hydroflow_plus_test::cluster::map_reduce(
         &builder,
         &CLIDeployNodeBuilder::new(|id| {
+            let mut deployment = deployment.borrow_mut();
             let host = create_host(&mut deployment);
             deployment.HydroflowCrate(
                 ".",
-                host,
-                Some("first_ten_distributed".into()),
+                host.clone(),
+                Some("map_reduce".into()),
                 None,
                 profile.clone(),
                 None,
                 Some(vec![id.to_string()]),
-                None,
+                Some("leader".to_string()),
                 vec![],
             )
         }),
+        &CLIDeployClusterBuilder::new(|id| {
+            let mut deployment = deployment.borrow_mut();
+            (0..2)
+                .map(|idx| {
+                    let host = create_host(&mut deployment);
+                    deployment.HydroflowCrate(
+                        ".",
+                        host.clone(),
+                        Some("map_reduce".into()),
+                        None,
+                        profile.clone(),
+                        None,
+                        Some(vec![id.to_string()]),
+                        Some(format!("cluster/{}", idx)),
+                        vec![],
+                    )
+                })
+                .collect()
+        }),
     );
     builder.wire();
+
+    let mut deployment = deployment.into_inner();
 
     deployment.deploy().await.unwrap();
 
