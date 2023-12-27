0143fe716a0c603bb8c5646b7ef62f938c9b1b9a
Shadaj Laddad <shadaj@users.noreply.github.com>
2023-12-26 17:16:09 -0800

    feat(hydroflow_plus): provide simpler API for launching and minimize dependencies
    The new `hydroflow::cli::util::launch` API takes a function from the `HydroCLI` config to a `Hydroflow`, which allows us to wait until the `Hydroflow` struct is instantiated (and all network connections are established) before returning the "ack start" message. This is also why this PR is stacked, because it triggers the race condition much more consistently.
    
    This also disables depending on default features of Hydroflow from Hydroflow+, which should speed up builds because we no longer load the Hydroflow / Datalog macros.

--- hydroflow_plus_test/src/bin/map_reduce.rs
+++ hydroflow_plus_test/src/bin/map_reduce.rs
@@ -4,8 +4,8 @@ extern crate alloc;
 // cannot use hydroflow::main because connect_local_blocking causes a deadlock
 #[tokio::main]
 async fn main() {
-    let ports = hydroflow::util::cli::init().await;
-
-    hydroflow::util::cli::launch_flow(hydroflow_plus_test::cluster::map_reduce_runtime!(&ports))
-        .await;
+    hydroflow_plus::util::cli::launch(|ports| {
+        hydroflow_plus_test::cluster::map_reduce_runtime!(ports)
+    })
+    .await;
 }
--- hydroflow_plus_test/src/bin/simple_cluster.rs
+++ hydroflow_plus_test/src/bin/simple_cluster.rs
@@ -1,10 +1,8 @@
 // cannot use hydroflow::main because connect_local_blocking causes a deadlock
 #[tokio::main]
 async fn main() {
-    let ports = hydroflow::util::cli::init().await;
-
-    hydroflow::util::cli::launch_flow(hydroflow_plus_test::cluster::simple_cluster_runtime!(
-        &ports
-    ))
+    hydroflow_plus::util::cli::launch(|ports| {
+        hydroflow_plus_test::cluster::simple_cluster_runtime!(ports)
+    })
     .await;
 }
