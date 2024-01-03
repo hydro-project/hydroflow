8f2c54166ddcbe971a33b6928bf46137aee26e5d
Shadaj Laddad <shadaj@users.noreply.github.com>
2024-01-03 15:40:13 -0800

    feat(hydroflow_plus): improve API naming and polish docs
    Node -> Process
    GraphBuilder -> FlowBuilder
    {Process/Cluster}Builder -> {Process/Cluster}Spec

--- hydroflow_plus_test/src/networked.rs
+++ hydroflow_plus_test/src/networked.rs
@@ -1,23 +1,23 @@
 use hydroflow_plus::bytes::BytesMut;
-use hydroflow_plus::node::{Deploy, HfNode, NodeBuilder};
+use hydroflow_plus::node::{Deploy, HfNode, ProcessSpec};
 use hydroflow_plus::scheduled::graph::Hydroflow;
 use hydroflow_plus::util::cli::HydroCLI;
-use hydroflow_plus::GraphBuilder;
+use hydroflow_plus::FlowBuilder;
 use hydroflow_plus_cli_integration::{CLIRuntime, HydroflowPlusMeta};
 use stageleft::{q, Quoted, RuntimeData};
 
 pub fn networked_basic<'a, D: Deploy<'a>>(
-    graph: &'a GraphBuilder<'a, D>,
-    node_builder: &impl NodeBuilder<'a, D>,
-) -> (D::NodePort, D::Node, D::Node) {
-    let node_zero = graph.node(node_builder);
-    let node_one = graph.node(node_builder);
+    flow: &'a FlowBuilder<'a, D>,
+    process_spec: &impl ProcessSpec<'a, D>,
+) -> (D::ProcessPort, D::Process, D::Process) {
+    let process_zero = flow.process(process_spec);
+    let process_one = flow.process(process_spec);
 
-    let (source_zero_port, source_zero) = node_zero.source_external();
+    let (source_zero_port, source_zero) = process_zero.source_external();
 
     source_zero
         .map(q!(|v| v.unwrap().freeze()))
-        .send_bytes(&node_one)
+        .send_bytes(&process_one)
         .for_each(q!(|v: Result<BytesMut, _>| {
             println!(
                 "node one received: {:?}",
@@ -25,16 +25,16 @@ pub fn networked_basic<'a, D: Deploy<'a>>(
             );
         }));
 
-    (source_zero_port, node_zero, node_one)
+    (source_zero_port, process_zero, process_one)
 }
 
 #[stageleft::entry]
 pub fn networked_basic_runtime<'a>(
-    graph: &'a GraphBuilder<'a, CLIRuntime>,
+    flow: &'a FlowBuilder<'a, CLIRuntime>,
     cli: RuntimeData<&'a HydroCLI<HydroflowPlusMeta>>,
 ) -> impl Quoted<'a, Hydroflow<'a>> {
-    let _ = networked_basic(graph, &cli);
-    graph.build(q!(cli.meta.subgraph_id))
+    let _ = networked_basic(flow, &cli);
+    flow.build(q!(cli.meta.subgraph_id))
 }
 
 #[stageleft::runtime]
@@ -43,17 +43,17 @@ mod tests {
     use hydro_deploy::{Deployment, HydroflowCrate};
     use hydroflow_plus::futures::SinkExt;
     use hydroflow_plus::util::cli::ConnectedSink;
-    use hydroflow_plus_cli_integration::{CLIDeployNodeBuilder, DeployCrateWrapper};
+    use hydroflow_plus_cli_integration::{DeployCrateWrapper, DeployProcessSpec};
 
     #[tokio::test]
     async fn networked_basic() {
         let mut deployment = Deployment::new();
         let localhost = deployment.Localhost();
 
-        let builder = hydroflow_plus::GraphBuilder::new();
+        let builder = hydroflow_plus::FlowBuilder::new();
         let (source_zero_port, _, node_one) = super::networked_basic(
             &builder,
-            &CLIDeployNodeBuilder::new(|| {
+            &DeployProcessSpec::new(|| {
                 deployment.add_service(
                     HydroflowCrate::new(".", localhost.clone())
                         .bin("networked_basic")
