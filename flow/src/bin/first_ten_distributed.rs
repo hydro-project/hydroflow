554f291aed1e6a01daf017ace0342b70157ddcb2
Shadaj Laddad <shadaj@users.noreply.github.com>
2024-01-03 15:00:57 -0800

    Update dependencies

--- flow/src/bin/first_ten_distributed.rs
+++ flow/src/bin/first_ten_distributed.rs
@@ -1,9 +1,7 @@
 #[tokio::main]
 async fn main() {
-    let ports = hydroflow_plus::util::cli::init().await;
-
-    hydroflow_plus::util::cli::launch_flow(
-        flow::first_ten_distributed::first_ten_distributed_runtime!(&ports),
-    )
+    hydroflow_plus::util::cli::launch(|ports| {
+        flow::first_ten_distributed::first_ten_distributed_runtime!(&ports)
+    })
     .await;
 }
