3b780552f6dba2224f5a0a0e0fd2e52c685d6aea
Discord9 <discord9@163.com>
2024-01-30 10:51:11 +0800

    refactor: make method state_id()

--- hydroflow/src/scheduled/state.rs
+++ hydroflow/src/scheduled/state.rs
@@ -9,14 +9,19 @@ use super::StateId;
 #[must_use]
 #[derive(Debug)]
 pub struct StateHandle<T> {
-    /// A staten handle's ID. Invalid if used in a different [`Hydroflow`](super::graph::Hydroflow)
-    /// instance than the original that created it.
-    pub state_id: StateId,
+    pub(crate) state_id: StateId,
     pub(crate) _phantom: PhantomData<*mut T>,
 }
 impl<T> Copy for StateHandle<T> {}
 impl<T> Clone for StateHandle<T> {
     fn clone(&self) -> Self {
         *self
+    }
+}
+
+impl<T> StateHandle<T> {
+    /// Returns the [`StateId`] of this handle.
+    pub fn state_id(&self) -> StateId {
+        self.state_id
     }
 }
