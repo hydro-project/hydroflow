4585794db189e152fce146b85e06740c49fff578
Shadaj Laddad <shadaj@users.noreply.github.com>
2024-03-11 15:12:26 -0700

    feat(hydroflow_plus): move persist manipulation into a proper optimization

--- hydroflow_plus/src/lib.rs
+++ hydroflow_plus/src/lib.rs
@@ -2,11 +2,13 @@
 
 stageleft::stageleft_no_entry_crate!();
 
+use std::collections::BTreeMap;
 use std::marker::PhantomData;
 
 use hydroflow::scheduled::context::Context;
 pub use hydroflow::scheduled::graph::Hydroflow;
 pub use hydroflow::*;
+use lang::graph::{partition_graph, propagate_flow_props, HydroflowGraph};
 use proc_macro2::TokenStream;
 use quote::quote;
 use stageleft::runtime_support::FreeVariable;
@@ -33,7 +35,7 @@ pub use builder::FlowBuilder;
 
 pub mod ir;
 
-pub mod persist_pushdown;
+pub mod persist_pullup;
 
 #[derive(Clone)]
 pub struct RuntimeContext<'a> {
@@ -48,14 +50,115 @@ impl<'a> FreeVariable<&'a Context> for RuntimeContext<'a> {
     }
 }
 
-pub struct HfBuilt<'a> {
+pub struct HfBuilt<'a, ID> {
+    hydroflow_ir: BTreeMap<usize, HydroflowGraph>,
+    _phantom: PhantomData<&'a mut &'a ID>,
+}
+
+impl<'a, ID> HfBuilt<'a, ID> {
+    pub fn hydroflow_ir(&self) -> &BTreeMap<usize, HydroflowGraph> {
+        &self.hydroflow_ir
+    }
+}
+
+impl<'a> HfBuilt<'a, usize> {
+    pub fn with_dynamic_id(self, id: impl Quoted<'a, usize>) -> HfBuiltWithID<'a> {
+        let hydroflow_crate = proc_macro_crate::crate_name("hydroflow_plus")
+            .expect("hydroflow_plus should be present in `Cargo.toml`");
+        let root = match hydroflow_crate {
+            proc_macro_crate::FoundCrate::Itself => quote! { hydroflow_plus },
+            proc_macro_crate::FoundCrate::Name(name) => {
+                let ident = syn::Ident::new(&name, proc_macro2::Span::call_site());
+                quote! { #ident }
+            }
+        };
+
+        let mut conditioned_tokens = None;
+        for (subgraph_id, flat_graph) in self.hydroflow_ir {
+            let mut partitioned_graph =
+                partition_graph(flat_graph).expect("Failed to partition (cycle detected).");
+
+            let mut diagnostics = Vec::new();
+            // Propagate flow properties throughout the graph.
+            // TODO(mingwei): Should this be done at a flat graph stage instead?
+            let _ = propagate_flow_props::propagate_flow_props(
+                &mut partitioned_graph,
+                &mut diagnostics,
+            );
+
+            let tokens = partitioned_graph.as_code(&root, true, quote::quote!(), &mut diagnostics);
+
+            if let Some(conditioned_tokens) = conditioned_tokens.as_mut() {
+                *conditioned_tokens = syn::parse_quote! {
+                    #conditioned_tokens else if __given_id == #subgraph_id {
+                        #tokens
+                    }
+                };
+            } else {
+                conditioned_tokens = Some(syn::parse_quote! {
+                    if __given_id == #subgraph_id {
+                        #tokens
+                    }
+                });
+            }
+        }
+
+        let conditioned_tokens: TokenStream = conditioned_tokens.unwrap();
+        let id = id.splice();
+        HfBuiltWithID {
+            tokens: syn::parse_quote!({
+                let __given_id = #id;
+                #conditioned_tokens else {
+                    panic!("Invalid node id: {}", __given_id);
+                }
+            }),
+            _phantom: PhantomData,
+        }
+    }
+}
+
+impl<'a> Quoted<'a, Hydroflow<'a>> for HfBuilt<'a, ()> {}
+
+impl<'a> FreeVariable<Hydroflow<'a>> for HfBuilt<'a, ()> {
+    fn to_tokens(mut self) -> (Option<TokenStream>, Option<TokenStream>) {
+        let hydroflow_crate = proc_macro_crate::crate_name("hydroflow_plus")
+            .expect("hydroflow_plus should be present in `Cargo.toml`");
+        let root = match hydroflow_crate {
+            proc_macro_crate::FoundCrate::Itself => quote! { hydroflow_plus },
+            proc_macro_crate::FoundCrate::Name(name) => {
+                let ident = syn::Ident::new(&name, proc_macro2::Span::call_site());
+                quote! { #ident }
+            }
+        };
+
+        if self.hydroflow_ir.len() != 1 {
+            panic!("Expected exactly one subgraph in the Hydroflow IR");
+        }
+
+        let flat_graph = self.hydroflow_ir.remove(&0).unwrap();
+        let mut partitioned_graph =
+            partition_graph(flat_graph).expect("Failed to partition (cycle detected).");
+
+        let mut diagnostics = Vec::new();
+        // Propagate flow properties throughout the graph.
+        // TODO(mingwei): Should this be done at a flat graph stage instead?
+        let _ =
+            propagate_flow_props::propagate_flow_props(&mut partitioned_graph, &mut diagnostics);
+
+        let tokens = partitioned_graph.as_code(&root, true, quote::quote!(), &mut diagnostics);
+
+        (None, Some(tokens))
+    }
+}
+
+pub struct HfBuiltWithID<'a> {
     tokens: TokenStream,
     _phantom: PhantomData<&'a mut &'a ()>,
 }
 
-impl<'a> Quoted<'a, Hydroflow<'a>> for HfBuilt<'a> {}
+impl<'a> Quoted<'a, Hydroflow<'a>> for HfBuiltWithID<'a> {}
 
-impl<'a> FreeVariable<Hydroflow<'a>> for HfBuilt<'a> {
+impl<'a> FreeVariable<Hydroflow<'a>> for HfBuiltWithID<'a> {
     fn to_tokens(self) -> (Option<TokenStream>, Option<TokenStream>) {
         (None, Some(self.tokens))
     }
