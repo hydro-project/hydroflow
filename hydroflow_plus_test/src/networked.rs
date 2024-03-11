b736cf4fb5dd305a13daa5a1915d3e7020f65ff4
Shadaj Laddad <shadaj@users.noreply.github.com>
2024-03-11 15:12:26 -0700

    feat(hydroflow_plus): move persist manipulation into a proper optimization

--- hydroflow_plus_test/src/networked.rs
+++ hydroflow_plus_test/src/networked.rs
@@ -49,7 +49,9 @@ pub fn networked_basic_runtime<'a>(
     cli: RuntimeData<&'a HydroCLI<HydroflowPlusMeta>>,
 ) -> impl Quoted<'a, Hydroflow<'a>> {
     let _ = networked_basic(flow, &cli, &cli);
-    flow.build().emit(q!(cli.meta.subgraph_id))
+    flow.extract()
+        .optimize_default()
+        .with_dynamic_id(q!(cli.meta.subgraph_id))
 }
 
 #[stageleft::runtime]
@@ -89,7 +91,7 @@ mod tests {
             }),
         );
 
-        insta::assert_debug_snapshot!(builder.build().ir);
+        insta::assert_debug_snapshot!(builder.extract().ir());
 
         let mut deployment = deployment.into_inner();
 
