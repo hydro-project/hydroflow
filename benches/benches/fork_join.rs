7b6702e7883e9b5619379a08914c2e4bf4610a6d
Mingwei Samuel <mingwei.samuel@gmail.com>
2024-03-19 12:48:47 -0700

    test: add `fork_join/hydroflow/surface` benchmark, remove old commented-out code pick [ci-bench] (#1112)

--- benches/benches/fork_join.rs
+++ benches/benches/fork_join.rs
@@ -1,4 +1,5 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use hydroflow::hydroflow_syntax;
 use hydroflow::scheduled::graph::Hydroflow;
 use hydroflow::scheduled::graph_ext::GraphExt;
 use hydroflow::scheduled::handoff::{Iter, VecHandoff};
@@ -82,6 +83,17 @@ fn benchmark_hydroflow(c: &mut Criterion) {
     });
 }
 
+fn benchmark_hydroflow_surface(c: &mut Criterion) {
+    c.bench_function("fork_join/hydroflow/surface", |b| {
+        b.iter(|| {
+            let mut hf = hydroflow_syntax! {
+                source_iter(0..NUM_INTS) -> import!("fork_join_20.hf") -> for_each(|x| { black_box(x); });
+            };
+            hf.run_available();
+        })
+    });
+}
+
 fn benchmark_hydroflow_builder(c: &mut Criterion) {
     c.bench_function("fork_join/hydroflow_builder", |b| {
         b.iter(|| {
@@ -151,157 +163,12 @@ fn benchmark_timely(c: &mut Criterion) {
     });
 }
 
-// fn benchmark_spinach(c: &mut Criterion) {
-//     c.bench_function("spinach", |b| {
-//         b.to_async(
-//             tokio::runtime::Builder::new_current_thread()
-//                 .build()
-//                 .unwrap(),
-//         )
-//         .iter(|| {
-//             async {
-//                 use spinachflow::comp::Comp;
-
-//                 type MyLatRepr =
-//                     spinachflow::lattice::set_union::SetUnionRepr<spinachflow::tag::VEC, usize>;
-//                 let op = <spinachflow::op::OnceOp<MyLatRepr>>::new((0..NUM_INTS).collect());
-
-//                 struct Even();
-//                 impl spinachflow::func::unary::Morphism for Even {
-//                     type InLatRepr = MyLatRepr;
-//                     type OutLatRepr = MyLatRepr;
-//                     fn call<Y: spinachflow::hide::Qualifier>(
-//                         &self,
-//                         item: spinachflow::hide::Hide<Y, Self::InLatRepr>,
-//                     ) -> spinachflow::hide::Hide<Y, Self::OutLatRepr> {
-//                         item.filter(|i| 0 == i % 2)
-//                     }
-//                 }
-
-//                 struct Odds();
-//                 impl spinachflow::func::unary::Morphism for Odds {
-//                     type InLatRepr = MyLatRepr;
-//                     type OutLatRepr = MyLatRepr;
-//                     fn call<Y: spinachflow::hide::Qualifier>(
-//                         &self,
-//                         item: spinachflow::hide::Hide<Y, Self::InLatRepr>,
-//                     ) -> spinachflow::hide::Hide<Y, Self::OutLatRepr> {
-//                         item.filter(|i| 1 == i % 2)
-//                     }
-//                 }
-
-//                 ///// MAGIC NUMBER!!!!!!!! is NUM_OPS
-//                 seq_macro::seq!(N in 0..20 {
-//                     let [ op_even, op_odds ] = spinachflow::op::fixed_split::<_, 2>(op);
-//                     let op_even = spinachflow::op::MorphismOp::new(op_even, Even());
-//                     let op_odds = spinachflow::op::MorphismOp::new(op_odds, Odds());
-//                     let op = spinachflow::op::MergeOp::new(op_even, op_odds);
-//                     let op = spinachflow::op::DynOpDelta::new(Box::new(op));
-//                 });
-
-//                 let comp = spinachflow::comp::NullComp::new(op);
-//                 spinachflow::comp::CompExt::run(&comp).await.unwrap_err();
-//             }
-//         });
-//     });
-// }
-
-// fn benchmark_spinach_switch(c: &mut Criterion) {
-//     c.bench_function("spinach w/ switch", |b| {
-//         b.to_async(
-//             tokio::runtime::Builder::new_current_thread()
-//                 .build()
-//                 .unwrap(),
-//         )
-//         .iter(|| {
-//             async {
-//                 use spinachflow::comp::Comp;
-
-//                 type MyLatRepr =
-//                     spinachflow::lattice::set_union::SetUnionRepr<spinachflow::tag::VEC, usize>;
-//                 let op = <spinachflow::op::OnceOp<MyLatRepr>>::new((0..NUM_INTS).collect());
-
-//                 struct SwitchEvenOdd();
-//                 impl spinachflow::func::unary::Morphism for SwitchEvenOdd {
-//                     type InLatRepr = MyLatRepr;
-//                     type OutLatRepr = spinachflow::lattice::pair::PairRepr<MyLatRepr, MyLatRepr>;
-//                     fn call<Y: spinachflow::hide::Qualifier>(
-//                         &self,
-//                         item: spinachflow::hide::Hide<Y, Self::InLatRepr>,
-//                     ) -> spinachflow::hide::Hide<Y, Self::OutLatRepr> {
-//                         let (a, b) = item.switch(|i| 0 == i % 2);
-//                         spinachflow::hide::Hide::zip(a, b)
-//                     }
-//                 }
-
-//                 ///// MAGIC NUMBER!!!!!!!! is NUM_OPS
-//                 seq_macro::seq!(N in 0..20 {
-//                     let op = spinachflow::op::MorphismOp::new(op, SwitchEvenOdd());
-//                     let ( op_even, op_odds ) = spinachflow::op::SwitchOp::new(op);
-//                     let op = spinachflow::op::MergeOp::new(op_even, op_odds);
-//                     let op = spinachflow::op::DynOpDelta::new(Box::new(op));
-//                 });
-
-//                 let comp = spinachflow::comp::NullComp::new(op);
-//                 spinachflow::comp::CompExt::run(&comp).await.unwrap_err();
-//             }
-//         });
-//     });
-// }
-
-// fn benchmark_spinachflow_symm(c: &mut Criterion) {
-//     c.bench_function("spinachflow (symmetric)", |b| {
-//         b.to_async(
-//             tokio::runtime::Builder::new_current_thread()
-//                 .build()
-//                 .unwrap(),
-//         )
-//         .iter(|| {
-//             async {
-//                 use spinachflow::futures::StreamExt;
-//                 use spinachflow::futures::future::ready;
-
-//                 let stream = spinachflow::futures::stream::iter(0..NUM_INTS);
-
-//                 ///// MAGIC NUMBER!!!!!!!! is NUM_OPS
-//                 seq_macro::seq!(N in 0..20 {
-//                     let splitter = spinachflow::stream::Splitter::new(stream);
-//                     let mut i = 0;
-//                     let splits = [(); BRANCH_FACTOR].map(|_| {
-//                         let j = i;
-//                         i += 1;
-//                         splitter.add_split().filter(move |x| ready(j == x % BRANCH_FACTOR))
-//                     });
-//                     let stream = spinachflow::stream::SelectArr::new(splits);
-//                     let stream: std::pin::Pin<Box<dyn spinachflow::futures::Stream<Item = usize>>> = Box::pin(stream);
-//                 });
-
-//                 let mut stream = stream;
-//                 loop {
-//                     let item = stream.next().await;
-//                     if item.is_none() {
-//                         break;
-//                     }
-//                 }
-//             }
-//         });
-//     });
-// }
-
-// criterion_group!(
-//     name = fork_join_dataflow;
-//     config = Criterion::default().with_profiler(PProfProfiler::new(100, Output::Flamegraph(None)));
-//     targets = benchmark_babyflow
-// );
-// criterion_group!(fork_join_dataflow, benchmark_timely,);
 criterion_group!(
     fork_join_dataflow,
     benchmark_hydroflow,
+    benchmark_hydroflow_surface,
     benchmark_hydroflow_builder,
     benchmark_timely,
     benchmark_raw,
-    // benchmark_spinach,
-    // benchmark_spinach_switch,
-    // benchmark_spinachflow_symm,
 );
 criterion_main!(fork_join_dataflow);
---
+++ benches/build.rs
@@ -0,0 +1,31 @@
+use std::fs::File;
+use std::io::{BufWriter, Write};
+use std::path::PathBuf;
+
+const NUM_OPS: usize = 20;
+
+pub fn main() -> std::io::Result<()> {
+    let path = PathBuf::from_iter([
+        env!("CARGO_MANIFEST_DIR"),
+        "benches",
+        &format!("fork_join_{}.hf", NUM_OPS),
+    ]);
+    let file = File::create(path)?;
+    let mut write = BufWriter::new(file);
+
+    writeln!(write, "a0 = mod -> tee();")?;
+
+    for i in 0..NUM_OPS {
+        if i > 0 {
+            writeln!(write, "a{} = union() -> tee();", i)?;
+        }
+        writeln!(write, "a{} -> filter(|x| x % 2 == 0) -> a{};", i, i + 1)?;
+        writeln!(write, "a{} -> filter(|x| x % 2 == 1) -> a{};", i, i + 1)?;
+    }
+
+    writeln!(write, "a{} = union() -> mod;", NUM_OPS)?;
+
+    write.flush()?;
+
+    Ok(())
+}
