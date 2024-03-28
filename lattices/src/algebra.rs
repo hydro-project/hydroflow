83a7a325a8d569598b81685257e860f4351a1637
conor <conorpower@berkeley.edu>
2024-03-28 15:35:33 -0700

    reorganize tests, remove extra dependencies, and add documentation to algebra.rs

--- lattices/src/algebra.rs
+++ lattices/src/algebra.rs
@@ -1,148 +1,12 @@
 use std::collections::HashSet;
 use std::fmt::Debug;
 
-use itertools::{EitherOrBoth, Itertools};
-
 use crate::test::cartesian_power;
 
-static test_items: &[u32; 14] = &[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
-
-#[test]
-fn test_associativity() {
-    //Test that max() is associative and exponentiation isn't
-    associativity(test_items, u32::max);
-    assert!( std::panic::catch_unwind(|| {
-        associativity(test_items, u32::wrapping_pow);
-    }).is_err());
-}
-
-#[test]
-fn test_idempotency() {
-    //Test that max() is idempotent and addition is non-idempotent
-    idempotency(test_items, u32::max);
-
-    assert!( std::panic::catch_unwind(|| {
-        idempotency(test_items, u32::wrapping_add);
-    }).is_err());
-}
-
-#[test]
-fn test_commutativity() {
-    //Test that max() is commutative and division is non-commutative
-    commutativity(test_items, u32::max);
-    assert!( std::panic::catch_unwind(|| {
-        commutativity(test_items, u32::wrapping_div);
-    }).is_err());
-}
-
-#[test]
-fn test_identity() {
-    //Test that 0 is the identity for addition and 5 is not
-    identity(test_items, u32::wrapping_add, 0);
-
-    assert!( std::panic::catch_unwind(|| {
-        identity(test_items, u32::wrapping_add, 5);
-    }).is_err());
-}
-
-
-#[test]
-fn test_inverse() {
-    //Test that subtraction is the inverse of addition and that addition is not the inverse of addition
-    inverse(test_items, u32::wrapping_add, 0, |x| 0u32.wrapping_sub(x));
-
-    assert!( std::panic::catch_unwind(|| {
-        inverse(test_items, u32::wrapping_add, 0, |x| 0u32.wrapping_add(x));
-    }).is_err());
-}
-
-#[test]
-fn test_distributive() {
-    //Test that addition and multiplication are distributive and that addition and max() are not
-    distributive(test_items, &u32::wrapping_add, &u32::wrapping_mul);
-    assert!( std::panic::catch_unwind(|| {
-        distributive(test_items, &u32::wrapping_add, &u32::max);
-    }).is_err());
-}
-
-#[test]
-fn test_absorbing() {
-    // Test that 0 is absorbing for multiplication and 5 is not
-    absorbing_element(test_items, &u32::wrapping_mul, 0);
-    assert!( std::panic::catch_unwind(|| {
-        absorbing_element(test_items, u32::wrapping_mul, 5);
-    }).is_err());
-}
-
-#[test]
-fn test_ring() {
-    //Test that +, x, 0, 1, - are a ring and +, x, 0, 5 are not (5 isn't a multiplicative identity)
-    ring(test_items, &u32::wrapping_add, &u32::wrapping_mul, 0, 1, &|x| {
-        0u32.wrapping_sub(x)
-    });
-    assert!( std::panic::catch_unwind(|| {
-        ring(test_items, &u32::wrapping_add, &u32::wrapping_mul, 0, 5, &|x| {
-            0u32.wrapping_sub(x)
-        });
-    }).is_err());
-}
-
-#[test]
-fn test_semiring(){
-    //Test +, x is a semiring
-    semiring(test_items, &u32::wrapping_add, &u32::wrapping_mul, 0, 1);
-
-    //Test boolean semiring with AND as + and OR as x
-    semiring(&[false, true], &|x, y| x | y, &|x, y| x & y, false, true);
-
-    //Test min plus semiring. + is min and x is plus. Also known as the "tropical semiring"
-    semiring(
-        &[0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, f64::INFINITY],
-        &|x, y| f64::min(x, y),
-        &|x, y| x + y,
-        f64::INFINITY,
-        0.0,
-    );
-
-    //Test max plus semiring. + is max and x is plus.
-    semiring(
-        &[0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, f64::NEG_INFINITY],
-        &|x, y| f64::max(x, y),
-        &|x, y| x + y,
-        f64::NEG_INFINITY,
-        0.0,
-    );
-
-    //Test sets of strings semiring with union as + and concatenation as x
-    semiring(
-        &[
-            HashSet::from([]),
-            HashSet::from(["".to_owned()]),
-            HashSet::from(["a".to_owned()]),
-            HashSet::from(["aa".to_owned(), "bb".to_owned()]),
-            HashSet::from(["ab".to_owned(), "bb".to_owned(), "cc".to_owned()]),
-            HashSet::from(["ba".to_owned()]),
-            HashSet::from(["bb".to_owned()]),
-        ],
-        &|x, y| x.union(&y).cloned().collect(),
-        &|x, y| {
-            let mut new_set = HashSet::new();
-
-            for a in x.iter() {
-                for b in y.iter() {
-                    new_set.insert(format!("{a}{b}"));
-                }
-            }
-
-            new_set
-        },
-        HashSet::from([]),
-        HashSet::from(["".to_owned()]),
-    );
-}
-
-// Algebraic Structures
-fn monoid<S: Debug + PartialEq + Clone, const N: usize>(
+/// Defines a monoid structure.
+/// A monoid is a set of items along with an associative binary operation `f` and an identity element `zero`.
+/// The `f` operation combines two items and the `zero` element acts as the identity for `f`.
+pub fn monoid<S: Debug + PartialEq + Clone, const N: usize>(
     items: &[S; N],
     f: &impl Fn(S, S) -> S,
     zero: S, //zero is the identity element of f
@@ -151,11 +15,18 @@ fn monoid<S: Debug + PartialEq + Clone, const N: usize>(
     identity(items, f, zero);
 }
 
-fn semigroup<S: Debug + PartialEq + Clone, const N: usize>(items: &[S; N], f: &impl Fn(S, S) -> S) {
+/// Defines a semigroup structure.
+/// A semigroup is a set of items along with an associative binary operation `f`.
+pub fn semigroup<S: Debug + PartialEq + Clone, const N: usize>(items: &[S; N], f: &impl Fn(S, S) -> S) {
     associativity(items, f);
 }
 
-fn semiring<S: Debug + PartialEq + Clone, const N: usize>(
+/// Defines a semiring structure.
+/// A semiring is a set of items along with two associative binary operations `f` and `g`,
+/// and two identity elements `zero` and `one`.
+/// f must be commutative and g must distribute over f.
+/// the zero of f must also be absorbing with respect to g.
+pub fn semiring<S: Debug + PartialEq + Clone, const N: usize>(
     items: &[S; N],
     f: &impl Fn(S, S) -> S,
     g: &impl Fn(S, S) -> S,
@@ -170,7 +41,9 @@ fn semiring<S: Debug + PartialEq + Clone, const N: usize>(
     distributive(items, f, g);
 }
 
-fn ring<S: Debug + PartialEq + Clone, const N: usize>(
+/// Defines a ring structure.
+/// A ring is a semiring with an inverse operation on f.
+pub fn ring<S: Debug + PartialEq + Clone, const N: usize>(
     items: &[S; N],
     f: &impl Fn(S, S) -> S,
     g: &impl Fn(S, S) -> S,
@@ -182,7 +55,9 @@ fn ring<S: Debug + PartialEq + Clone, const N: usize>(
     inverse(items, f, zero, b);
 }
 
-fn commutative_ring<S: Debug + PartialEq + Clone, const N: usize>(
+/// defines a commutative ring structure.
+/// a commutative ring is a ring where the multiplication operation g is commutative.
+pub fn commutative_ring<S: Debug + PartialEq + Clone, const N: usize>(
     items: &[S; N],
     f: &impl Fn(S, S) -> S,
     g: &impl Fn(S, S) -> S,
@@ -195,7 +70,9 @@ fn commutative_ring<S: Debug + PartialEq + Clone, const N: usize>(
     commutativity(items, g);
 }
 
-fn field<S: Debug + PartialEq + Clone, const N: usize>(
+/// defines a field structure.
+/// a field is a commutative ring where every element has a multiplicative inverse.
+pub fn field<S: Debug + PartialEq + Clone, const N: usize>(
     items: &[S; N],
     f: &impl Fn(S, S) -> S,
     g: &impl Fn(S, S) -> S,
@@ -207,7 +84,9 @@ fn field<S: Debug + PartialEq + Clone, const N: usize>(
     nonzero_inverse(items, f, one, zero, b);
 }
 
-fn commutative_monoid<S: Debug + PartialEq + Clone, const N: usize>(
+/// defines a commutative monoid structure.
+/// a commutative monoid is a monoid where the operation f is commutative.
+pub fn commutative_monoid<S: Debug + PartialEq + Clone, const N: usize>(
     items: &[S; N],
     f: &impl Fn(S, S) -> S,
     zero: S,
@@ -216,29 +95,36 @@ fn commutative_monoid<S: Debug + PartialEq + Clone, const N: usize>(
     commutativity(items, f);
 }
 
-fn abelian_group<S: Debug + PartialEq + Clone, const N: usize>(
-    items: &[S; N],
-    f: &impl Fn(S, S) -> S,
-    zero: S,
-    b: &impl Fn(S) -> S, //b is the inverse function of f
-) {
-    group(items, f, zero, b);
-    commutativity(items, f);
-}
 
-fn group<S: Debug + PartialEq + Clone, const N: usize>(
+/// defines a group structure.
+/// a group is a set of items along with an associative binary operation `f`
+/// an identity element `zero`
+/// and every element has an inverse element with respect to `f`
+pub fn group<S: Debug + PartialEq + Clone, const N: usize>(
     items: &[S; N],
     f: &impl Fn(S, S) -> S,
     zero: S, //zero is the identity element of f
-    b: &impl Fn(S) -> S, //b is the inverse function of f
+    b: &impl Fn(S) -> S, //b is the function to compute the inverse element of an element with respect to f
 ) {
     monoid(items, f, zero.clone());
     inverse(items, f, zero, b);
 }
 
+/// defines an abelian group structure.
+/// an abelian group is a group where the operation f is commutative.
+pub fn abelian_group<S: Debug + PartialEq + Clone, const N: usize>(
+    items: &[S; N],
+    f: &impl Fn(S, S) -> S,
+    zero: S,
+    b: &impl Fn(S) -> S, //b is the function to compute the inverse element of an element with respect to f
+) {
+    group(items, f, zero, b);
+    commutativity(items, f);
+}
+
 
 // Algebraic Properties
-fn distributive<S: Debug + PartialEq + Clone, const N: usize>(
+pub fn distributive<S: Debug + PartialEq + Clone, const N: usize>(
     items: &[S; N],
     f: &impl Fn(S, S) -> S,
     g: &impl Fn(S, S) -> S,
@@ -247,7 +133,7 @@ fn distributive<S: Debug + PartialEq + Clone, const N: usize>(
     right_distributes(items, f, g);
 }
 
-fn left_distributes<S: Debug + PartialEq + Clone, const N: usize>(
+pub fn left_distributes<S: Debug + PartialEq + Clone, const N: usize>(
     items: &[S; N],
     f: impl Fn(S, S) -> S,
     g: impl Fn(S, S) -> S,
@@ -261,7 +147,7 @@ fn left_distributes<S: Debug + PartialEq + Clone, const N: usize>(
     }
 }
 
-fn right_distributes<S: Debug + PartialEq + Clone, const N: usize>(
+pub fn right_distributes<S: Debug + PartialEq + Clone, const N: usize>(
     items: &[S; N],
     f: impl Fn(S, S) -> S,
     g: impl Fn(S, S) -> S,
@@ -275,7 +161,7 @@ fn right_distributes<S: Debug + PartialEq + Clone, const N: usize>(
     }
 }
 
-fn absorbing_element<S: Debug + PartialEq + Clone, const N: usize>(
+pub fn absorbing_element<S: Debug + PartialEq + Clone, const N: usize>(
     items: &[S; N],
     f: impl Fn(S, S) -> S,
     z: S, //absorbing element (anything multiplied by z is z e.g. 0 in integers)
@@ -289,11 +175,11 @@ fn absorbing_element<S: Debug + PartialEq + Clone, const N: usize>(
     }
 }
 
-fn inverse<S: Debug + PartialEq + Clone, const N: usize>(
+pub fn inverse<S: Debug + PartialEq + Clone, const N: usize>(
     items: &[S; N],
     f: impl Fn(S, S) -> S,
     e: S, //e is the identity element of f
-    b: impl Fn(S) -> S, //b is the inverse function of f
+    b: impl Fn(S) -> S, //b is the function to compute the inverse element of an element with respect to f
 ) {
     // ∃b: ab = e, ba = e
     for [a] in cartesian_power(items) {
@@ -302,7 +188,7 @@ fn inverse<S: Debug + PartialEq + Clone, const N: usize>(
     }
 }
 
-fn nonzero_inverse<S: Debug + PartialEq + Clone, const N: usize>(
+pub fn nonzero_inverse<S: Debug + PartialEq + Clone, const N: usize>(
     items: &[S; N],
     f: impl Fn(S, S) -> S,
     e: S,
@@ -318,7 +204,7 @@ fn nonzero_inverse<S: Debug + PartialEq + Clone, const N: usize>(
     }
 }
 
-fn identity<S: Debug + PartialEq + Clone, const N: usize>(
+pub fn identity<S: Debug + PartialEq + Clone, const N: usize>(
     items: &[S; N],
     f: impl Fn(S, S) -> S,
     e: S,
@@ -330,7 +216,7 @@ fn identity<S: Debug + PartialEq + Clone, const N: usize>(
     }
 }
 
-fn associativity<S: Debug + PartialEq + Clone, const N: usize>(
+pub fn associativity<S: Debug + PartialEq + Clone, const N: usize>(
     items: &[S; N],
     f: impl Fn(S, S) -> S,
 ) {
@@ -343,7 +229,7 @@ fn associativity<S: Debug + PartialEq + Clone, const N: usize>(
     }
 }
 
-fn commutativity<S: Debug + PartialEq + Clone, const N: usize>(
+pub fn commutativity<S: Debug + PartialEq + Clone, const N: usize>(
     items: &[S; N],
     f: impl Fn(S, S) -> S,
 ) {
@@ -353,7 +239,7 @@ fn commutativity<S: Debug + PartialEq + Clone, const N: usize>(
     }
 }
 
-fn idempotency<S: Debug + PartialEq + Clone, const N: usize>(
+pub fn idempotency<S: Debug + PartialEq + Clone, const N: usize>(
     items: &[S; N],
     f: impl Fn(S, S) -> S,
 ) {
@@ -363,3 +249,145 @@ fn idempotency<S: Debug + PartialEq + Clone, const N: usize>(
     }
 }
 
+//Tests
+
+#[cfg(test)]
+    mod test {
+    use crate::algebra::*;
+
+    static TEST_ITEMS: &[u32; 14] = &[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+    #[test]
+    fn test_associativity() {
+        //Test that max() is associative and exponentiation isn't
+        associativity(TEST_ITEMS, u32::max);
+        assert!( std::panic::catch_unwind(|| {
+            associativity(TEST_ITEMS, u32::wrapping_pow);
+        }).is_err());
+    }
+
+    #[test]
+    fn test_idempotency() {
+        //Test that max() is idempotent and addition is non-idempotent
+        idempotency(TEST_ITEMS, u32::max);
+
+        assert!( std::panic::catch_unwind(|| {
+            idempotency(TEST_ITEMS, u32::wrapping_add);
+        }).is_err());
+    }
+
+    #[test]
+    fn test_commutativity() {
+        //Test that max() is commutative and division is non-commutative
+        commutativity(TEST_ITEMS, u32::max);
+        assert!( std::panic::catch_unwind(|| {
+            commutativity(TEST_ITEMS, u32::wrapping_div);
+        }).is_err());
+    }
+
+    #[test]
+    fn test_identity() {
+        //Test that 0 is the identity for addition and 5 is not
+        identity(TEST_ITEMS, u32::wrapping_add, 0);
+
+        assert!( std::panic::catch_unwind(|| {
+            identity(TEST_ITEMS, u32::wrapping_add, 5);
+        }).is_err());
+    }
+
+
+    #[test]
+    fn test_inverse() {
+        //Test that subtraction is the inverse of addition and that addition is not the inverse of addition
+        inverse(TEST_ITEMS, u32::wrapping_add, 0, |x| 0u32.wrapping_sub(x));
+
+        assert!( std::panic::catch_unwind(|| {
+            inverse(TEST_ITEMS, u32::wrapping_add, 0, |x| 0u32.wrapping_add(x));
+        }).is_err());
+    }
+
+    #[test]
+    fn test_distributive() {
+        //Test that addition and multiplication are distributive and that addition and max() are not
+        distributive(TEST_ITEMS, &u32::wrapping_add, &u32::wrapping_mul);
+        assert!( std::panic::catch_unwind(|| {
+            distributive(TEST_ITEMS, &u32::wrapping_add, &u32::max);
+        }).is_err());
+    }
+
+    #[test]
+    fn test_absorbing() {
+        // Test that 0 is absorbing for multiplication and 5 is not
+        absorbing_element(TEST_ITEMS, &u32::wrapping_mul, 0);
+        assert!( std::panic::catch_unwind(|| {
+            absorbing_element(TEST_ITEMS, u32::wrapping_mul, 5);
+        }).is_err());
+    }
+
+    #[test]
+    fn test_ring() {
+        //Test that +, x, 0, 1, - are a ring and +, x, 0, 5 are not (5 isn't a multiplicative identity)
+        ring(TEST_ITEMS, &u32::wrapping_add, &u32::wrapping_mul, 0, 1, &|x| {
+            0u32.wrapping_sub(x)
+        });
+        assert!( std::panic::catch_unwind(|| {
+            ring(TEST_ITEMS, &u32::wrapping_add, &u32::wrapping_mul, 0, 5, &|x| {
+                0u32.wrapping_sub(x)
+            });
+        }).is_err());
+    }
+
+    #[test]
+    fn test_semiring(){
+        //Test +, x is a semiring
+        semiring(TEST_ITEMS, &u32::wrapping_add, &u32::wrapping_mul, 0, 1);
+
+        //Test boolean semiring with AND as + and OR as x
+        semiring(&[false, true], &|x, y| x | y, &|x, y| x & y, false, true);
+
+        //Test min plus semiring. + is min and x is plus. Also known as the "tropical semiring"
+        semiring(
+            &[0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, f64::INFINITY],
+            &|x, y| f64::min(x, y),
+            &|x, y| x + y,
+            f64::INFINITY,
+            0.0,
+        );
+
+        //Test max plus semiring. + is max and x is plus.
+        semiring(
+            &[0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, f64::NEG_INFINITY],
+            &|x, y| f64::max(x, y),
+            &|x, y| x + y,
+            f64::NEG_INFINITY,
+            0.0,
+        );
+
+        //Test sets of strings semiring with union as + and concatenation as x
+        semiring(
+            &[
+                HashSet::from([]),
+                HashSet::from(["".to_owned()]),
+                HashSet::from(["a".to_owned()]),
+                HashSet::from(["aa".to_owned(), "bb".to_owned()]),
+                HashSet::from(["ab".to_owned(), "bb".to_owned(), "cc".to_owned()]),
+                HashSet::from(["ba".to_owned()]),
+                HashSet::from(["bb".to_owned()]),
+            ],
+            &|x, y| x.union(&y).cloned().collect(),
+            &|x, y| {
+                let mut new_set = HashSet::new();
+
+                for a in x.iter() {
+                    for b in y.iter() {
+                        new_set.insert(format!("{a}{b}"));
+                    }
+                }
+
+                new_set
+            },
+            HashSet::from([]),
+            HashSet::from(["".to_owned()]),
+        );
+    }
+}
