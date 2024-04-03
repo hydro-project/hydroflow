305d9f5c59f062dfcf57234c298645b6bd308b9c
Shadaj Laddad <shadaj@users.noreply.github.com>
2024-04-03 11:11:27 -0700

    feat(hydroflow_plus): switch launch to be a macro to enable borrowing locals
    Summary:
    
    Test Plan:

--- hydroflow_plus_test/src/bin/map_reduce.rs
+++ hydroflow_plus_test/src/bin/map_reduce.rs
@@ -1,7 +1,7 @@
 // cannot use hydroflow::main because connect_local_blocking causes a deadlock
 #[tokio::main]
 async fn main() {
-    hydroflow_plus::util::cli::launch(|ports| {
+    hydroflow_plus::util::cli::launch!(|ports| {
         hydroflow_plus_test::cluster::map_reduce_runtime!(ports)
     })
     .await;
