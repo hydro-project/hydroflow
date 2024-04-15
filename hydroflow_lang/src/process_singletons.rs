1057b273cbd941c1a12a287580c15e264d797f2c
Mingwei Samuel <mingwei.samuel@gmail.com>
2024-04-15 14:07:39 -0700

    refactor(hydroflow_lang): unify `process_singletons.rs` code (#1159)

--- hydroflow_lang/src/process_singletons.rs
+++ hydroflow_lang/src/process_singletons.rs
@@ -15,37 +15,10 @@ use crate::parse::parse_terminated;
 /// OK. However the returned tokens are not used in the codegen as we need to use [`postprocess_singletons`]
 /// later to substitute-in the context referencing code for each singleton
 pub fn preprocess_singletons(tokens: TokenStream, found_idents: &mut Vec<Ident>) -> TokenStream {
-    tokens
-        .into_iter()
-        .peekable()
-        .batching(|iter| {
-            let out = match iter.next()? {
-                TokenTree::Group(group) => {
-                    let mut new_group = Group::new(
-                        group.delimiter(),
-                        preprocess_singletons(group.stream(), found_idents),
-                    );
-                    new_group.set_span(group.span());
-                    TokenTree::Group(new_group)
-                }
-                TokenTree::Ident(ident) => TokenTree::Ident(ident),
-                TokenTree::Punct(punct) => {
-                    if '#' == punct.as_char() && matches!(iter.peek(), Some(TokenTree::Ident(_))) {
-                        // Found a singleton.
-                        let Some(TokenTree::Ident(singleton_ident)) = iter.next() else {
-                            unreachable!()
-                        };
-                        found_idents.push(singleton_ident.clone());
-                        TokenTree::Ident(singleton_ident)
-                    } else {
-                        TokenTree::Punct(punct)
-                    }
-                }
-                TokenTree::Literal(lit) => TokenTree::Literal(lit),
-            };
-            Some(out)
-        })
-        .collect()
+    process_singletons(tokens, &mut |singleton_ident| {
+        found_idents.push(singleton_ident.clone());
+        TokenTree::Ident(singleton_ident)
+    })
 }
 
 /// Replaces singleton references `#my_var` with the code needed to actually get the value inside.
@@ -57,14 +30,24 @@ pub fn postprocess_singletons(
     tokens: TokenStream,
     resolved_idents: impl IntoIterator<Item = Ident>,
 ) -> Punctuated<Expr, Token![,]> {
-    let processed = postprocess_singletons_helper(tokens, resolved_idents.into_iter().by_ref());
+    let mut resolved_idents_iter = resolved_idents.into_iter();
+    let processed = process_singletons(tokens, &mut |_singleton_ident| {
+        let resolved_ident = resolved_idents_iter.next().unwrap();
+        TokenTree::Group(Group::new(
+            proc_macro2::Delimiter::Parenthesis,
+            quote! {
+                *context.state_ref(#resolved_ident).borrow_mut()
+            },
+        ))
+    });
     parse_terminated(processed).unwrap()
 }
 
-/// Internal recursive helper for [`postprocess_singletons`].
-fn postprocess_singletons_helper(
+/// Traverse the token stream, applying the `map_singleton_fn` whenever a singleton is found,
+/// returning the transformed token stream.
+fn process_singletons(
     tokens: TokenStream,
-    resolved_idents_iter: &mut impl Iterator<Item = Ident>,
+    map_singleton_fn: &mut impl FnMut(Ident) -> TokenTree,
 ) -> TokenStream {
     tokens
         .into_iter()
@@ -74,7 +57,7 @@ fn postprocess_singletons_helper(
                 TokenTree::Group(group) => {
                     let mut new_group = Group::new(
                         group.delimiter(),
-                        postprocess_singletons_helper(group.stream(), resolved_idents_iter),
+                        process_singletons(group.stream(), map_singleton_fn),
                     );
                     new_group.set_span(group.span());
                     TokenTree::Group(new_group)
@@ -83,15 +66,10 @@ fn postprocess_singletons_helper(
                 TokenTree::Punct(punct) => {
                     if '#' == punct.as_char() && matches!(iter.peek(), Some(TokenTree::Ident(_))) {
                         // Found a singleton.
-                        let _singleton_ident = iter.next();
-                        let resolved_ident = resolved_idents_iter.next().unwrap();
-                        TokenTree::Group(Group::new(
-                            proc_macro2::Delimiter::Parenthesis,
-                            quote! {
-                                *context.state_ref(#resolved_ident).borrow_mut()
-                            },
-                        ))
-                        // TokenTree::Ident(resolved_idents_iter.next().unwrap())
+                        let Some(TokenTree::Ident(singleton_ident)) = iter.next() else {
+                            unreachable!()
+                        };
+                        (map_singleton_fn)(singleton_ident)
                     } else {
                         TokenTree::Punct(punct)
                     }
