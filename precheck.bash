c901740680e267cb87a7c3fa1d2c729f81b6bf19
Mingwei Samuel <mingwei.samuel@gmail.com>
2024-04-19 18:24:56 -0700

    chore: update precheck.bash

--- precheck.bash
+++ precheck.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -euxo pipefail
 
-cargo +nightly fmt
-cargo clippy --all-targets --features python
+cargo +nightly fmt --all
+cargo clippy --all-targets --features python -- -D warnings
 
 INSTA_FORCE_PASS=1 INSTA_UPDATE=always TRYBUILD=overwrite cargo test --all-targets --no-fail-fast --features python
 cargo test --doc
