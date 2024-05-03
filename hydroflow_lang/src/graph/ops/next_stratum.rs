af810bb2928d0e6e6d907ea3e981229efa658540
Mingwei Samuel <mingwei.samuel@gmail.com>
2024-05-02 17:02:47 -0700

    feat(hydroflow_lang): allow `defer_tick()`, `next_stratum()` to have a type hint arg

--- hydroflow_lang/src/graph/ops/next_stratum.rs
+++ hydroflow_lang/src/graph/ops/next_stratum.rs
@@ -5,6 +5,9 @@ use super::{
 
 /// Delays all elements which pass through to the next stratum (in the same
 /// tick).
+///
+/// You can also supply a type parameter `next_stratum::<MyType>()` to specify what items flow
+/// through the the pipeline. This can be useful for helping the compiler infer types.
 pub const NEXT_STRATUM: OperatorConstraints = OperatorConstraints {
     name: "next_stratum",
     categories: &[OperatorCategory::Control],
@@ -13,7 +16,7 @@ pub const NEXT_STRATUM: OperatorConstraints = OperatorConstraints {
     hard_range_out: RANGE_1,
     soft_range_out: RANGE_1,
     num_args: 0,
-    persistence_args: RANGE_0,
+    persistence_args: &(0..=1),
     type_args: RANGE_0,
     is_external_input: false,
     has_singleton_output: false,
