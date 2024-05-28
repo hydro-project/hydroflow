484474f828f50f46e4a008f85d4449b04e1e306d
Mingwei Samuel <mingwei.samuel@gmail.com>
2024-05-28 13:46:37 -0700

    basic generalized hash trie

--- hydroflow/src/util/generalized_hash_trie.rs
+++ hydroflow/src/util/generalized_hash_trie.rs
@@ -2,24 +2,23 @@ use std::collections::HashMap;
 use std::fmt::Debug;
 use std::hash::Hash;
 
-use variadics::{var_expr, var_type, AsRefVariadic, Variadic, VariadicExt};
+use sealed::sealed;
+use variadics::{var_args, var_expr, var_type, Variadic, VariadicExt};
 
 /// node of a HashTrie
+#[sealed]
 pub trait HashTrieNode: Default {
-    type Items<'a>: Variadic + AsRefVariadic<'a>;
+    /// Row variadic.
+    type Row: VariadicExt;
 
     /// Inserts items into the hash trie.
-    /// Returns `true` if the items were successfully inserted, `false` otherwise.
-    fn insert(&mut self, items: &[Self::Items<'static>]) -> bool;
+    fn insert(&mut self, row: Self::Row);
 
-    /// Performs a prefix search on the hash trie.
-    /// Returns `true` if the prefix is found, `false` otherwise.
-    fn prefix_search(&self, prefix: &[&T]) -> bool;
+    /// Returns `true` if the row is found, `false` otherwise.
+    fn contains(&self, row: <Self::Row as VariadicExt>::AsRefVar<'_>) -> bool;
 
-    /// Item for [`Self::Iter`].
-    type Item;
     /// Iterator for [`Self::iter`].
-    type Iter<'a>: Iterator<Item = Self::Item>
+    type Iter<'a>: Iterator<Item = <Self::Row as VariadicExt>::AsRefVar<'a>>
     where
         Self: 'a;
     /// Iterate the sub-tree.
@@ -28,107 +27,102 @@ pub trait HashTrieNode: Default {
 
 /// internal node of a HashTrie
 #[derive(Debug)]
-pub struct HTInner<H: 'static, T: HashTrieNode<H>> {
-    children: HashMap<H, T>,
+pub struct HtInner<Key, Node>
+where
+    Node: HashTrieNode,
+{
+    children: HashMap<Key, Node>,
 }
-impl<H: 'static, T: HashTrieNode<H>> Default for HTInner<H, T> {
+impl<Key, Node: HashTrieNode> Default for HtInner<Key, Node>
+where
+    Node: HashTrieNode,
+{
     fn default() -> Self {
         let children = Default::default();
         Self { children }
     }
 }
-
-impl<H, T: 'static + HashTrieNode<H>> HashTrieNode<H> for HTInner<H, T>
+#[sealed]
+impl<Key, Node> HashTrieNode for HtInner<Key, Node>
 where
-    H: 'static + Eq + Hash + Clone,
+    Key: 'static + Hash + Eq,
+    Node: 'static + HashTrieNode,
 {
-    fn prefix_search(&self, prefix: &[&H]) -> bool {
-        if prefix.is_empty() {
-            return true;
-        }
-        let head = prefix[0];
-        if let Some(node) = self.children.get(head) {
-            node.prefix_search(&prefix[1..])
+    type Row = var_type!(Key, ...Node::Row);
+
+    fn insert(&mut self, row: Self::Row) {
+        let var_args!(key, ...rest) = row;
+
+        self.children.entry(key).or_default().insert(rest);
+    }
+
+    fn contains(&self, row: <Self::Row as VariadicExt>::AsRefVar<'_>) -> bool {
+        let var_args!(key, ...rest) = row;
+        if let Some(node) = self.children.get(key) {
+            node.contains(rest)
         } else {
             false
         }
     }
 
-    fn insert(&mut self, items: &[&H]) -> bool {
-        if items.is_empty() {
-            return false;
-        }
-
-        let head = items[0];
-        self.children
-            .entry(head.clone())
-            .or_default()
-            .insert(&items[1..])
-    }
-
-    type Item = var_type!(H, ...T::Item);
-    type Iter<'a> = HTInnerIter<'a, H, T>;
+    type Iter<'a> = HtInnerIter<'a, Key, Node>;
     fn iter(&self) -> Self::Iter<'_> {
         self.children
             .iter()
-            .flat_map(|(k, vs)| vs.iter().map(move |v| var_expr!(k.clone(), ...v)))
+            .flat_map(|(k, vs)| vs.iter().map(move |v| var_expr!(k, ...v)))
     }
 }
 /// Iter for HTLeaf.
-pub type HTInnerIter<'a, H: 'static + Eq + Hash + Clone, T: 'static + HashTrieNode<H>> =
-    impl 'a + Iterator<Item = var_type!(H, ...T::Item)>;
+pub type HtInnerIter<'a, Key, Node>
+where
+    Key: 'static + Eq + Hash,
+    Node: 'static + HashTrieNode,
+= impl 'a
+    + Iterator<Item = <<HtInner<Key, Node> as HashTrieNode>::Row as VariadicExt>::AsRefVar<'a>>;
 
 /// leaf node of a HashTrie
 #[derive(Debug)]
-pub struct HTLeaf<T> {
+pub struct HtLeaf<T> {
     elements: Vec<T>,
 }
-impl<T> Default for HTLeaf<T> {
+impl<T> Default for HtLeaf<T> {
     fn default() -> Self {
         let elements = Default::default();
         Self { elements }
     }
 }
-
-impl<T> HashTrieNode<T> for HTLeaf<T>
+#[sealed]
+impl<T> HashTrieNode for HtLeaf<T>
 where
-    T: 'static + Clone + PartialEq,
+    T: 'static + Eq,
 {
-    fn prefix_search(&self, prefix: &[&T]) -> bool {
-        if prefix.is_empty() {
-            return true;
-        }
-        let head = prefix[0];
-        self.elements.contains(head)
+    type Row = var_expr!(T);
+
+    fn insert(&mut self, row: Self::Row) {
+        let var_args!(key) = row;
+        self.elements.push(key);
     }
 
-    fn insert(&mut self, items: &[&T]) -> bool {
-        if !items.is_empty() {
-            let val = items[0];
-            if !self.elements.contains(val) {
-                self.elements.push(val.clone());
-                return true;
-            }
-            return false;
-        }
-        false
+    fn contains(&self, row: <Self::Row as VariadicExt>::AsRefVar<'_>) -> bool {
+        let var_args!(key) = row;
+        self.elements.contains(key)
     }
 
-    type Item = var_type!(T);
     type Iter<'a> = HTLeafIter<'a, T>;
     fn iter(&self) -> Self::Iter<'_> {
-        self.elements.iter().map(|item| var_expr!(item.clone()))
+        self.elements.iter().map(|item| var_expr!(item))
     }
 }
 /// Iter for HTLeaf.
-pub type HTLeafIter<'a, T: 'static + Clone + PartialEq> = impl 'a + Iterator<Item = var_type!(T)>;
+pub type HTLeafIter<'a, T>
+where
+    T: 'static + Eq,
+= impl 'a + Iterator<Item = <<HtLeaf<T> as HashTrieNode>::Row as VariadicExt>::AsRefVar<'a>>;
 
 /// Trait to convert a tuple to a HashTrieList
-pub trait ToHashTrie {
+pub trait ToHashTrie: Variadic {
     /// The output type of the hash trie conversion.
-    type Output: HashTrieNode<Self::Item>;
-    /// This allows us to avoid using dyn
-    type Item;
+    type Output: HashTrieNode<Row = Self>;
 
     /// Converts the object into a hash trie.
     fn to_hash_trie(self) -> Self::Output;
@@ -138,11 +132,10 @@ impl<Head> ToHashTrie for var_type!(Head)
 where
     Head: 'static + Eq + Hash + Clone,
 {
-    type Output = HTLeaf<Head>;
-    type Item = Head;
+    type Output = HtLeaf<Head>;
 
     fn to_hash_trie(self) -> Self::Output {
-        let mut result = HTLeaf {
+        let mut result = HtLeaf {
             elements: Vec::new(),
         };
         result.elements.push(self.0.clone());
@@ -152,16 +145,14 @@ where
 
 impl<Head, Tail> ToHashTrie for var_type!(Head, ...Tail)
 where
-    Head: 'static + Eq + Hash + Clone + Debug,
-    Tail: 'static + Variadic + ToHashTrie + VariadicExt,
-    <Tail as ToHashTrie>::Output: HashTrieNode<Head>,
+    Head: 'static + Eq + Hash,
+    Tail: 'static + ToHashTrie,
 {
-    type Output = HTInner<Head, Tail::Output>;
-    type Item = Head;
+    type Output = HtInner<Head, Tail::Output>;
 
     fn to_hash_trie(self) -> Self::Output {
-        let var_expr!(head, ...tail) = self;
-        let mut result = HTInner::<Head, Tail::Output> {
+        let var_args!(head, ...tail) = self;
+        let mut result = HtInner::<Head, Tail::Output> {
             children: HashMap::<Head, Tail::Output>::new(),
         };
         result.children.insert(head, tail.to_hash_trie());
@@ -193,40 +184,42 @@ mod tests {
     // How can I insert non-atomic objects?
     #[test]
     fn test_search_prefix() {
-        let htrie = var_expr!(42, 314, 43770).to_hash_trie();
+        let htrie = var_expr!(42_u16, 314_u32, 43770_u64).to_hash_trie();
         println!("HTrie: {:?}", htrie);
-        assert!(htrie.prefix_search(&[&42]));
-        assert!(!htrie.prefix_search(&[&43]));
-        assert!(htrie.prefix_search(&[&42, &314]));
-        assert!(!htrie.prefix_search(&[&42, &315]));
-        assert!(!htrie.prefix_search(&[&43, &314]));
-        assert!(htrie.prefix_search(&[&42, &314, &43770]));
-        assert!(!htrie.prefix_search(&[&42, &314, &30619]));
-        assert!(!htrie.prefix_search(&[&42, &315, &43770]));
-        assert!(!htrie.prefix_search(&[&43, &314, &43770]));
+        // assert!(htrie.prefix_search(&[&42]));
+        // assert!(!htrie.prefix_search(&[&43]));
+        // assert!(htrie.prefix_search(&[&42, &314]));
+        // assert!(!htrie.prefix_search(&[&42, &315]));
+        // assert!(!htrie.prefix_search(&[&43, &314]));
+        assert!(htrie.contains(var_expr!(&42, &314, &43770)));
+        assert!(htrie.contains(var_expr!(42, 314, 43770).as_ref_var()));
+        assert!(!htrie.contains(var_expr!(&42, &314, &30619)));
+        assert!(!htrie.contains(var_expr!(42, 314, 30619).as_ref_var()));
+        assert!(!htrie.contains(var_expr!(&42, &315, &43770)));
+        assert!(!htrie.contains(var_expr!(&43, &314, &43770)));
     }
 
     #[test]
     fn test_insert() {
         let mut htrie = var_expr!(42, 314, 43770).to_hash_trie();
-        assert!(htrie.insert(&[&42, &315, &43770]));
-        assert!(htrie.insert(&[&42, &314, &30619]));
-        assert!(htrie.insert(&[&43, &10, &600]));
-        assert!(htrie.prefix_search(&[&42, &314, &30619]));
-        assert!(!htrie.prefix_search(&[&42, &314, &600]));
-        assert!(htrie.prefix_search(&[&42, &315]));
-        assert!(htrie.prefix_search(&[&42, &315, &43770]));
-        assert!(htrie.prefix_search(&[&43]));
-        assert!(htrie.prefix_search(&[&43, &10]));
-        assert!(htrie.prefix_search(&[&43, &10, &600]));
+        htrie.insert(var_expr!(42, 315, 43770));
+        htrie.insert(var_expr!(42, 314, 30619));
+        htrie.insert(var_expr!(43, 10, 600));
+        assert!(htrie.contains(var_expr!(&42, &314, &30619)));
+        // assert!(!htrie.prefix_search(&[&42, &314, &600]));
+        // assert!(htrie.prefix_search(&[&42, &315]));
+        assert!(htrie.contains(var_expr!(&42, &315, &43770)));
+        // assert!(htrie.prefix_search(&[&43]));
+        // assert!(htrie.prefix_search(&[&43, &10]));
+        assert!(htrie.contains(var_expr!(&43, &10, &600)));
     }
 
     #[test]
     fn test_iter() {
         let mut htrie = var_expr!(42, 314, 43770).to_hash_trie();
-        assert!(htrie.insert(&[&42, &315, &43770]));
-        assert!(htrie.insert(&[&42, &314, &30619]));
-        assert!(htrie.insert(&[&43, &10, &600]));
+        htrie.insert(var_expr!(42, 315, 43770));
+        htrie.insert(var_expr!(42, 314, 30619));
+        htrie.insert(var_expr!(43, 10, 600));
         for row in htrie.iter() {
             println!("{:?}", row);
         }
