40f187b490f166278458f7bace4e44d34d1a4f1c
Rohit Kulshreshtha <rohit.in.pune@gmail.com>
2024-05-29 15:42:26 -0700

    fix(anna): Get build working.

--- datastores/gossip_kv/server/model.rs
+++ datastores/gossip_kv/server/model.rs
@@ -1,7 +1,6 @@
-use hydroflow::lattices::{DomPair, Max, WithBot};
 use hydroflow::lattices::map_union::MapUnionHashMap;
 use hydroflow::lattices::set_union::SetUnionHashSet;
-
+use hydroflow::lattices::{DomPair, Max};
 
 /// Primary key for entries in a table.
 pub type RowKey = String;
@@ -22,7 +21,7 @@ pub type TableName = String;
 pub type TableMap<C> = MapUnionHashMap<TableName, Table<C>>;
 
 /// Timestamps used in the model.
-/// TODO: This will be updated to use a more sophisticated clock type with https://github.com/hydro-project/hydroflow/issues/1207.
+// TODO: This will be updated to use a more sophisticated clock type with https://github.com/hydro-project/hydroflow/issues/1207.
 pub type Clock = Max<usize>;
 
 /// TableMap element to upsert a row in an existing TableMap.
@@ -36,8 +35,8 @@ pub type Clock = Max<usize>;
 /// - `key`: Primary key of the row.
 /// - `val`: Row value.
 pub fn upsert_row<C>(row_ts: C, table_name: TableName, key: RowKey, val: String) -> TableMap<C> {
-    let value : RowValue<C> = RowValue::new_from(row_ts, SetUnionHashSet::new_from([val]));
-    let row : Table<C> = Table::new_from([(key, value)]);
+    let value: RowValue<C> = RowValue::new_from(row_ts, SetUnionHashSet::new_from([val]));
+    let row: Table<C> = Table::new_from([(key, value)]);
     TableMap::new_from([(table_name, row)])
 }
 
@@ -50,45 +49,58 @@ pub fn upsert_row<C>(row_ts: C, table_name: TableName, key: RowKey, val: String)
 /// - `table_name`: Name of the table.
 /// - `key`: Primary key of the row.
 pub fn delete_row<C>(row_ts: C, table_name: TableName, key: RowKey) -> TableMap<C> {
-    let value : RowValue<C> = RowValue::new_from(row_ts, SetUnionHashSet::new_from([]));
-    let row : Table<C> = Table::new_from([(key, value)]);
+    let value: RowValue<C> = RowValue::new_from(row_ts, SetUnionHashSet::new_from([]));
+    let row: Table<C> = Table::new_from([(key, value)]);
     TableMap::new_from([(table_name, row)])
 }
 
 #[cfg(test)]
 mod tests {
     use std::collections::HashSet;
+
     use hydroflow::lattices::map_union::MapUnionHashMap;
     use hydroflow::lattices::Merge;
 
-    use crate::model::{Clock, delete_row, upsert_row, TableMap, TableName, RowKey};
+    use crate::model::{delete_row, upsert_row, Clock, RowKey, TableMap, TableName};
 
     #[test]
-    fn test_table_set() {
+    fn test_table_map() {
         let mut table_map: TableMap<Clock> = MapUnionHashMap::default();
 
         let first_tick: Clock = Clock::new(0);
         let second_tick: Clock = Clock::new(1);
-        let third_tick: Clock = Clock::new(2);
 
         let members_table = TableName::from("members");
         let key_1 = RowKey::from("key1");
-        let value_1 : String = "value1".to_string();
+        let value_1: String = "value1".to_string();
 
         // Table starts out empty.
         assert_eq!(table_map.as_reveal_ref().len(), 0, "Expected empty table.");
 
-        let insert = upsert_row(first_tick, members_table.clone(), key_1.clone(), value_1.clone());
+        let insert = upsert_row(
+            first_tick,
+            members_table.clone(),
+            key_1.clone(),
+            value_1.clone(),
+        );
         Merge::merge(&mut table_map, insert);
         {
             let table = table_map.as_reveal_ref().get(&members_table).unwrap();
 
             let row = table.as_reveal_ref().get(&key_1);
             assert!(row.is_some(), "Row should exist");
-            assert_eq!(*row.unwrap().as_reveal_ref().0, first_tick, "Unexpected row timestamp");
+            assert_eq!(
+                *row.unwrap().as_reveal_ref().0,
+                first_tick,
+                "Unexpected row timestamp"
+            );
 
             let value = row.unwrap().as_reveal_ref().1.as_reveal_ref();
-            assert_eq!(value, &HashSet::from([value_1.to_string()]), "Unexpected row value");
+            assert_eq!(
+                value,
+                &HashSet::from([value_1.to_string()]),
+                "Unexpected row value"
+            );
         }
 
         let delete_row = delete_row(second_tick, members_table.clone(), key_1.to_string());
@@ -100,7 +112,11 @@ mod tests {
             let row = table.as_reveal_ref().get(&key_1);
 
             assert!(row.is_some(), "Row should exist");
-            assert_eq!(*row.unwrap().as_reveal_ref().0, second_tick, "Unexpected row timestamp");
+            assert_eq!(
+                *row.unwrap().as_reveal_ref().0,
+                second_tick,
+                "Unexpected row timestamp"
+            );
 
             let value = row.unwrap().as_reveal_ref().1.as_reveal_ref();
             assert_eq!(value, &HashSet::from([]), "Row should be empty");
