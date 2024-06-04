hydro-project/hydroflow

commit e5bdd12e32d6ea72fd91a55c12e09f07a0edaa5c

feat(hydroflow): add initial test using Hydro CLI from Hydroflow+ (#978)
This also required a change to Hydroflow core to make it possible to run the dataflow itself on a single thread (using a LocalSet), even if the surrounding runtime is not single-threaded (required to work around deadlocks because we can't use async APIs inside Hydroflow+). This requires us to spawn any Hydroflow tasks (only for `dest_sink` at the moment) right next to when we run the dataflow rather than when the Hydroflow graph is initialized. From a conceptual perspective, this seems _more right_, since now creating a Hydroflow program will not result in any actual tasks running.

In the third PR of this series, I aim to add a new Hydroflow+ operator that will automate the setup of a `dest_sink`/`source_stream` pair that span nodes.

hydroflow/src/scheduled/graph.rs
@@ -61,6 +61,7 @@ impl<'a> Default for Hydroflow<'a> {
 
             subgraph_id: SubgraphId(0),
 
+            tasks_to_spawn: Vec::new(),
             task_join_handles: Vec::new(),
         };
         Self {
@@ -351,6 +352,7 @@ impl<'a> Hydroflow<'a> {
     /// TODO(mingwei): Currently blockes forever, no notion of "completion."
     #[tracing::instrument(level = "trace", skip(self), ret)]
     pub async fn run_async(&mut self) -> Option<Never> {
+        self.context.spawn_tasks();
         loop {
             // Run any work which is immediately available.
             self.run_available_async().await;
@@ -681,12 +683,12 @@ impl<'a> Hydroflow<'a> {
 }
 
 impl<'a> Hydroflow<'a> {
-    /// Alias for [`Context::spawn_task`].
-    pub fn spawn_task<Fut>(&mut self, future: Fut)
+    /// Alias for [`Context::request_task`].
+    pub fn request_task<Fut>(&mut self, future: Fut)
     where
         Fut: Future<Output = ()> + 'static,
     {
-        self.context.spawn_task(future);
+        self.context.request_task(future);
     }
 
     /// Alias for [`Context::abort_tasks`].

.github/workflows/ci.yml
@@ -216,11 +216,6 @@ jobs:
           pip install maturin
           maturin develop
 
-      - name: Configure core dumps
-        run: |
-          mkdir cli-core-dumps
-          echo "$PWD/cli-core-dumps/corefile-%e-%p-%t" | sudo tee /proc/sys/kernel/core_pattern
-
       - name: Run Python tests
         run: |
           ulimit -c unlimited
@@ -229,24 +224,16 @@ jobs:
           cd python_tests
           pip install -r requirements.txt
           RUST_BACKTRACE=1 pytest *.py
+          cd ../..
 
-      - name: Print backtraces
-        if: ${{ failure() }}
+      - name: Run Hydroflow+ Python tests
         run: |
-          cd cli-core-dumps
-          sudo apt-get install gdb
-          for file in $(ls); do
-            echo "Backtrace for $file"
-            rust-gdb -ex "thread apply all bt" -batch $PWD/../.venv/bin/python $file
-          done
-          cd ..
-
-      - name: Upload core dumps
-        if: ${{ failure() }}
-        uses: actions/upload-artifact@master
-        with:
-          name: cli-core-dumps
-          path: ./cli-core-dumps/*
+          ulimit -c unlimited
+          cd hydro_cli
+          source .venv/bin/activate
+          cd ../hydroflow_plus_test/python_tests
+          pip install -r requirements.txt
+          RUST_BACKTRACE=1 pytest *.py
 
   lints:
     name: Lints

hydro_cli_examples/examples/dedalus_2pc_coordinator/main.rs
@@ -4,7 +4,7 @@ use hydroflow_datalog::datalog;
 
 #[hydroflow::main]
 async fn main() {
-    let mut ports = hydroflow::util::cli::init().await;
+    let ports = hydroflow::util::cli::init().await;
     let vote_to_participant_port = ports
         .port("vote_to_participant")
         .connect::<ConnectedDemux<ConnectedDirect>>()

hydro_cli_examples/examples/dedalus_2pc_participant/main.rs
@@ -4,7 +4,7 @@ use hydroflow_datalog::datalog;
 
 #[hydroflow::main]
 async fn main() {
-    let mut ports = hydroflow::util::cli::init().await;
+    let ports = hydroflow::util::cli::init().await;
     let vote_to_participant_source = ports
         .port("vote_to_participant")
         .connect::<ConnectedDirect>()

hydro_cli_examples/examples/dedalus_receiver/main.rs
@@ -4,7 +4,7 @@ use hydroflow_datalog::datalog;
 
 #[hydroflow::main]
 async fn main() {
-    let mut ports = hydroflow::util::cli::init().await;
+    let ports = hydroflow::util::cli::init().await;
     let broadcast_recv = ports
         .port("broadcast")
         .connect::<ConnectedDirect>()

hydro_cli_examples/examples/dedalus_sender/main.rs
@@ -5,7 +5,7 @@ use hydroflow_datalog::datalog;
 
 #[hydroflow::main]
 async fn main() {
-    let mut ports = hydroflow::util::cli::init().await;
+    let ports = hydroflow::util::cli::init().await;
     let broadcast_port = ports
         .port("broadcast")
         .connect::<ConnectedDemux<ConnectedDirect>>()

hydro_cli_examples/examples/dedalus_vote_leader/main.rs
@@ -4,7 +4,7 @@ use hydroflow_datalog::datalog;
 
 #[hydroflow::main]
 async fn main() {
-    let mut ports = hydroflow::util::cli::init().await;
+    let ports = hydroflow::util::cli::init().await;
     let to_replica_port = ports
         .port("to_replica")
         .connect::<ConnectedDemux<ConnectedDirect>>()

hydro_cli_examples/examples/dedalus_vote_participant/main.rs
@@ -4,7 +4,7 @@ use hydroflow_datalog::datalog;
 
 #[hydroflow::main]
 async fn main() {
-    let mut ports = hydroflow::util::cli::init().await;
+    let ports = hydroflow::util::cli::init().await;
     let to_replica_source = ports
         .port("to_replica")
         .connect::<ConnectedDirect>()

commit 39ad0d68de5281c1ee4ecc97e05a1d14c602538b

fix(hydroflow): make sure tasks are spawned!!!! (#1273)
fix bug introduced in #978

hydroflow/src/scheduled/graph.rs
@@ -282,6 +282,10 @@ impl<'a> Hydroflow<'a> {
     /// Returns true if any work was done.
     #[tracing::instrument(level = "trace", skip(self), fields(tick = u64::from(self.context.current_tick), stratum = self.context.current_stratum), ret)]
     pub fn run_stratum(&mut self) -> bool {
+        // Make sure to spawn tasks once hydroflow is running!
+        // This drains the task buffer, so becomes a no-op after first call.
+        self.context.spawn_tasks();
+
         let current_tick = self.context.current_tick;
 
         let mut work_done = false;
@@ -445,7 +449,6 @@ impl<'a> Hydroflow<'a> {
     /// TODO(mingwei): Currently blocks forever, no notion of "completion."
     #[tracing::instrument(level = "trace", skip(self), ret)]
     pub async fn run_async(&mut self) -> Option<Never> {
-        self.context.spawn_tasks();
         loop {
             // Run any work which is immediately available.
             self.run_available_async().await;