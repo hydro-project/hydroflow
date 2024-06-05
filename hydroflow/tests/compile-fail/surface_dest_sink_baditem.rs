7c4369841bc4131aced8ffd8b1fe688d140c5ac1
Mingwei Samuel <mingwei.samuel@gmail.com>
2024-06-05 16:41:34 -0700

    test(hydroflow): re-enable (some?) old surface compile-fail tests

--- hydroflow/tests/compile-fail/surface_dest_sink_baditem.rs
+++ hydroflow/tests/compile-fail/surface_dest_sink_baditem.rs
@@ -1,6 +1,5 @@
 use std::net::{Ipv4Addr, SocketAddr};
 
-use hydroflow::hydroflow_syntax;
 use hydroflow::util::bind_udp_bytes;
 
 #[hydroflow::main]
@@ -8,7 +7,7 @@ async fn main() {
     let socket_addr: SocketAddr = (Ipv4Addr::LOCALHOST, 0).into();
     let (outbound, _inbound, _) = bind_udp_bytes(socket_addr).await;
 
-    let mut df = hydroflow_syntax! {
+    let mut df = hydroflow::hydroflow_syntax! {
         source_iter(0..10)
             -> map(|i| bytes::Bytes::from_static(&b"hello world!"[i..(i + 2)]))
             -> dest_sink(outbound);
