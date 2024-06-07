b3c233dadd7ebc57eb7e8ee111e43e8dddb9c882
Mingwei Samuel <mingwei.samuel@gmail.com>
2024-06-07 09:08:11 -0700

    test(hydroflow): fix `fold_keyed()` and add `reduce_keyed()` compile-fail tests, fix #1279 (#1283)

--- hydroflow/tests/compile-fail/surface_reduce_keyed_badtype_int.rs
+++ hydroflow/tests/compile-fail/surface_reduce_keyed_badtype_int.rs
@@ -1,7 +1,7 @@
 fn main() {
     let mut df = hydroflow::hydroflow_syntax! {
         source_iter(0..1)
-            -> reduce_keyed(|old: &mut u32, val: u32| { *old += val; })
+            -> fold_keyed(|| 0, |old: &mut u32, val: u32| { *old += val; })
             -> for_each(std::mem::drop);
     };
     df.run_available();
