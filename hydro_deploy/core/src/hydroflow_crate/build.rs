12f2aea96b746b7534be13dc44922a578c20a0b5
Shadaj Laddad <shadaj@users.noreply.github.com>
2024-08-14 15:22:59 -0700

    feat(hydroflow_plus): use trybuild to compile subgraph binaries

--- hydro_deploy/core/src/hydroflow_crate/build.rs
+++ hydro_deploy/core/src/hydroflow_crate/build.rs
@@ -25,6 +25,8 @@ pub struct BuildParams {
     example: Option<String>,
     /// `--profile` parameter.
     profile: Option<String>,
+    target_dir: Option<PathBuf>,
+    no_default_features: bool,
     /// `--target <linux>` if cross-compiling for linux ([`HostTargetType::Linux`]).
     target_type: HostTargetType,
     /// `--features` flags, will be comma-delimited.
@@ -32,11 +34,14 @@ pub struct BuildParams {
 }
 impl BuildParams {
     /// Creates a new `BuildParams` and canonicalizes the `src` path.
+    #[allow(clippy::too_many_arguments)]
     pub fn new(
         src: impl AsRef<Path>,
         bin: Option<String>,
         example: Option<String>,
         profile: Option<String>,
+        target_dir: Option<PathBuf>,
+        no_default_features: bool,
         target_type: HostTargetType,
         features: Option<Vec<String>>,
     ) -> Self {
@@ -53,6 +58,8 @@ impl BuildParams {
             bin,
             example,
             profile,
+            target_dir,
+            no_default_features,
             target_type,
             features,
         }
@@ -101,11 +108,19 @@ pub async fn build_crate_memoized(params: BuildParams) -> Result<&'static BuildO
                         }
                     }
 
+                    if params.no_default_features {
+                        command.arg("--no-default-features");
+                    }
+
                     if let Some(features) = params.features {
                         command.args(["--features", &features.join(",")]);
                     }
 
                     command.arg("--message-format=json-diagnostic-rendered-ansi");
+
+                    if let Some(target_dir) = params.target_dir.as_ref() {
+                        command.env("CARGO_TARGET_DIR", target_dir);
+                    }
 
                     let mut spawned = command
                         .current_dir(&params.src)
