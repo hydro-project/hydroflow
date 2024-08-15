791eede7342acff17c08bdff0480cc2987aa3816
Shadaj Laddad <shadaj@users.noreply.github.com>
2024-08-15 15:20:59 -0700

    feat(hydroflow_plus): use trybuild to compile subgraph binaries

---
+++ template/hydroflow_plus/examples/first_ten_distributed_gcp.rs
@@ -0,0 +1,53 @@
+use std::sync::Arc;
+
+use hydro_deploy::gcp::GcpNetwork;
+use hydro_deploy::Deployment;
+use hydroflow_plus_cli_integration::TrybuildHost;
+use tokio::sync::RwLock;
+
+static RELEASE_RUSTFLAGS: &str = "-C opt-level=3 -C codegen-units=1 -C strip=none -C debuginfo=2 -C lto=off";
+
+#[tokio::main]
+async fn main() {
+    let gcp_project = std::env::args()
+        .nth(1)
+        .expect("Expected GCP project as first argument");
+
+    let mut deployment = Deployment::new();
+    let vpc = Arc::new(RwLock::new(GcpNetwork::new(&gcp_project, None)));
+
+    let flow = hydroflow_plus::FlowBuilder::new();
+    let (p1, p2) = hydroflow_plus_template::first_ten_distributed::first_ten_distributed(&flow);
+
+    let _nodes = flow
+        .with_default_optimize()
+        .with_process(
+            &p1,
+            TrybuildHost::new(
+                deployment
+                    .GcpComputeEngineHost()
+                    .project(gcp_project.clone())
+                    .machine_type("e2-micro")
+                    .image("debian-cloud/debian-11")
+                    .region("us-west1-a")
+                    .network(vpc.clone())
+                    .add(),
+            ).rustflags(RELEASE_RUSTFLAGS),
+        )
+        .with_process(
+            &p2,
+            TrybuildHost::new(
+                deployment
+                    .GcpComputeEngineHost()
+                    .project(gcp_project.clone())
+                    .machine_type("e2-micro")
+                    .image("debian-cloud/debian-11")
+                    .region("us-west1-a")
+                    .network(vpc.clone())
+                    .add(),
+            ).rustflags(RELEASE_RUSTFLAGS),
+        )
+        .deploy(&mut deployment);
+
+    deployment.run_ctrl_c().await.unwrap();
+}
