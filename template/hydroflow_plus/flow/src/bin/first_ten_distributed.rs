d4134d2ce3110b2aa90e62af6dff4561463fec55
Shadaj Laddad <shadaj@users.noreply.github.com>
2024-08-21 13:34:39 -0700

    feat(hydroflow_plus): use trybuild to compile subgraph binaries
    Fixes #1307

--- template/hydroflow_plus/flow/src/bin/first_ten_distributed.rs
+++
@@ -1,7 +0,0 @@
-#[tokio::main]
-async fn main() {
-    hydroflow_plus::util::cli::launch!(|ports| {
-        flow::first_ten_distributed::first_ten_distributed_runtime!(ports)
-    })
-    .await;
-}
