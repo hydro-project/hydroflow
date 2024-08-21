59ccd552f04b0a4c61d7d95634d96a0623a007b3
Mingwei Samuel <mingwei.samuel@gmail.com>
2024-08-21 15:04:16 -0700

    feat(hydroflow_lang): allow `demux_enum` to have any number of outputs, fix #1329

--- hydroflow/src/util/demux_enum.rs
+++ hydroflow/src/util/demux_enum.rs
@@ -9,14 +9,23 @@ pub use hydroflow_macro::DemuxEnum;
 /// The derive will implement this such that `Outputs` can be any tuple where each item is a
 /// `Pusherator` that corresponds to each of the variants of the tuple, in alphabetic order.
 #[diagnostic::on_unimplemented(
-    note = "Ensure there is exactly one output for each enum variant.",
-    note = "Ensure that the type for each output is a tuple of the field for the variant: `()`, `(a,)`, or `(a, b, ...)`."
+    note = "ensure there is exactly one output for each enum variant.",
+    note = "ensure that the type for each output is a tuple of the field for the variant: `()`, `(a,)`, or `(a, b, ...)`."
 )]
 pub trait DemuxEnum<Outputs>: DemuxEnumBase {
     /// Pushes self into the corresponding output pusherator in `outputs`.
     fn demux_enum(self, outputs: &mut Outputs);
 }
 
-/// Base implementation to constrain that `DemuxEnum<SOMETHING>` is implemented.
-#[diagnostic::on_unimplemented(note = "Use `#[derive(hydroflow::DemuxEnum)]`")]
+/// Special case of [`DemuxEnum`] for when there is only one variant.
+#[diagnostic::on_unimplemented(note = "requires that the enum only have one variant.")]
+pub trait SingleVariant: DemuxEnumBase {
+    /// Output tuple type.
+    type Output;
+    /// Convert self into it's single variant tuple Output.
+    fn single_variant(self) -> Self::Output;
+}
+
+/// Base implementation to constrain that [`DemuxEnum<SOMETHING>`] is implemented.
+#[diagnostic::on_unimplemented(note = "use `#[derive(hydroflow::DemuxEnum)]`")]
 pub trait DemuxEnumBase {}
