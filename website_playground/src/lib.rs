d5febc33b695ea45419599c948d7abcda43d2177
Shadaj Laddad <shadaj@users.noreply.github.com>
2024-08-22 15:26:00 -0700

    chore: update dependencies

--- website_playground/src/lib.rs
+++ website_playground/src/lib.rs
@@ -15,12 +15,6 @@ use quote::quote;
 use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 #[wasm_bindgen]
 extern "C" {
     fn alert(s: &str);
