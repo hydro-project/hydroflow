4289e64e718ea4d9d2cfb13efc8d06d88ba9292a
Shadaj Laddad <shadaj@users.noreply.github.com>
2024-08-24 16:18:49 -0700

    feat(hydroflow_plus): add unbounded top-level singletons

--- hydroflow_plus_test_local/src/local/compute_pi.rs
+++ hydroflow_plus_test_local/src/local/compute_pi.rs
@@ -24,13 +24,10 @@ pub fn compute_pi(flow: &FlowBuilder, batch_size: RuntimeData<usize>) -> Process
         .all_ticks();
 
     trials
-        .tick_batch()
-        .persist()
         .reduce(q!(|(inside, total), (inside_batch, total_batch)| {
             *inside += inside_batch;
             *total += total_batch;
         }))
-        .all_ticks()
         .sample_every(q!(Duration::from_secs(1)))
         .for_each(q!(|(inside, total)| {
             println!(
