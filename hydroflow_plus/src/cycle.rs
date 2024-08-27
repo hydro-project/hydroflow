87883c3381b5bea4253a62c01098f76f03f18200
Shadaj Laddad <shadaj@users.noreply.github.com>
2024-08-27 16:16:33 -0700

    feat(hydroflow_plus): add API for cycle with initial value

--- hydroflow_plus/src/cycle.rs
+++ hydroflow_plus/src/cycle.rs
@@ -3,24 +3,37 @@ use std::marker::PhantomData;
 use crate::builder::FlowLeaves;
 use crate::location::{Location, LocationId};
 
-pub trait CycleCollection<'a> {
+pub trait CycleComplete<'a> {
+    fn complete(self, ident: syn::Ident);
+}
+
+pub trait CycleCollection<'a>: CycleComplete<'a> {
     type Location: Location;
 
     fn create_source(ident: syn::Ident, ir_leaves: FlowLeaves<'a>, l: LocationId) -> Self;
+}
 
-    fn complete(self, ident: syn::Ident);
+pub trait CycleCollectionWithInitial<'a>: CycleComplete<'a> {
+    type Location: Location;
+
+    fn create_source(
+        ident: syn::Ident,
+        ir_leaves: FlowLeaves<'a>,
+        initial: Self,
+        l: LocationId,
+    ) -> Self;
 }
 
 /// Represents a fixpoint cycle in the graph that will be fulfilled
 /// by a stream that is not yet known.
 ///
 /// See [`crate::FlowBuilder`] for an explainer on the type parameters.
-pub struct HfCycle<'a, S: CycleCollection<'a>> {
+pub struct HfCycle<'a, S: CycleComplete<'a>> {
     pub(crate) ident: syn::Ident,
     pub(crate) _phantom: PhantomData<(&'a mut &'a (), S)>,
 }
 
-impl<'a, S: CycleCollection<'a>> HfCycle<'a, S> {
+impl<'a, S: CycleComplete<'a>> HfCycle<'a, S> {
     pub fn complete(self, stream: S) {
         let ident = self.ident;
         S::complete(stream, ident)
