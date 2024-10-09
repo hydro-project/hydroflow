5a89fa73c66194025d4ed7208e1843f6475a3b55
Chae <admin@MacBook-Pro.local>
2024-10-08 22:33:53 -0700

    Updating

--- lattices/fuzz/fuzz_targets/all_fuzz.rs
+++ lattices/fuzz/fuzz_targets/all_fuzz.rs
@@ -1,47 +1,4 @@
-
-
-// #[macro_use]
-// extern crate afl;
-// extern crate url;
-
-// mod associativity_fuzz;
-// // mod commutativity_fuzz;
-// // mod idempotence_fuzz;
-// // mod linearity_fuzz;
-// // mod distributivity_fuzz;
-
-// fn main() {
-//     // Define your function f here
-//     let f: fn(u8, u8) -> u8 = |x, y| x.wrapping_add(y); // Example: addition
-
-//     // Define q as a transformation function
-//     let q: fn(u8) -> u8 = |x| x; // Example: identity function
-
-//     // Example g function definition
-//     let g: fn(u8, u8) -> u8 = |x, y| x + y; // Example g function
-
-//     fuzz!(|data: &[u8]| {
-//         if data.len() < 2 {
-//             return; // Ensure there's enough data for the fuzz target
-//         }
-
-//         println!("Running associativity fuzz test");
-//         associativity_fuzz::fuzz_target(data);
-
-//         // println!("Running commutativity fuzz test");
-//         // commutativity_fuzz::fuzz_target(data, f);
-
-//         // println!("Running idempotency fuzz test");
-//         // idempotence_fuzz::fuzz_target(data, f);
-
-//         // println!("Running linearity fuzz test");
-//         // linearity_fuzz::fuzz_target(data, f, q, g);
-
-//         // println!("Running distributivity fuzz test");
-//         // distributivity_fuzz::fuzz_target(data, f, g);
-//     });
-// }
-
+ 
 
 #![no_main]
 
@@ -54,19 +11,7 @@ use lattices::algebra::commutativity_single;
 use lattices::algebra::distributive_single;
 
 
-
-
-// mod associativity_fuzz;
-// mod commutativity_fuzz;
-// mod idempotence_fuzz;
-// mod linearity_fuzz;
-// mod distributivity_fuzz;
-// Import the necessary fuzzing macro
-// Import the necessary fuzzing macro
-
-// Ensure your function definitions are included
-// fn associativity_single(...), fn commutativity_single(...), etc.
-
+ 
 fuzz_target!(|data: &[u8]| {
     // Define your function f here inside the fuzz target
     let f = |x: u8, y: u8| x.wrapping_add(y); // Use wrapping addition
@@ -91,7 +36,6 @@ fuzz_target!(|data: &[u8]| {
     let commutativity_result = commutativity_single(a, b, f);
     println!("Commutativity test result: {}", commutativity_result);
  
-
     println!("Running linearity fuzz test");
     let linearity_result = linearity_single(a, b, f, q, g);
     println!("Linearity test result: {}", linearity_result);
@@ -99,5 +43,4 @@ fuzz_target!(|data: &[u8]| {
     println!("Running distributivity fuzz test");
     let distributivity_result = distributive_single(a, b, c, f, g);
     println!("Distributivity test result: {}", distributivity_result);
-
 });
