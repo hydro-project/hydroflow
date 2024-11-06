e9d05bf11a0e85da8ed1a0fe00be7769298308c2
Shadaj Laddad <shadaj@users.noreply.github.com>
2024-11-06 14:42:07 -0800

    refactor(hydroflow_plus): move `HfCompiled` and friends to a module (#1542)

---
+++ hydroflow_plus/src/builder/compiled.rs
@@ -0,0 +1,123 @@
+use std::collections::BTreeMap;
+use std::marker::PhantomData;
+
+use hydroflow::scheduled::graph::Hydroflow;
+use hydroflow_lang::graph::{partition_graph, HydroflowGraph};
+use proc_macro2::TokenStream;
+use quote::quote;
+use stageleft::runtime_support::FreeVariable;
+use stageleft::Quoted;
+
+pub struct HfCompiled<'a, ID> {
+    pub(super) hydroflow_ir: BTreeMap<usize, HydroflowGraph>,
+    pub(super) extra_stmts: BTreeMap<usize, Vec<syn::Stmt>>,
+    pub(super) _phantom: PhantomData<&'a mut &'a ID>,
+}
+
+impl<ID> HfCompiled<'_, ID> {
+    pub fn hydroflow_ir(&self) -> &BTreeMap<usize, HydroflowGraph> {
+        &self.hydroflow_ir
+    }
+
+    pub fn take_ir(self) -> BTreeMap<usize, HydroflowGraph> {
+        self.hydroflow_ir
+    }
+}
+
+impl<'a> HfCompiled<'a, usize> {
+    pub fn with_dynamic_id(self, id: impl Quoted<'a, usize>) -> HfBuiltWithId<'a> {
+        let hydroflow_crate = proc_macro_crate::crate_name("hydroflow_plus")
+            .expect("hydroflow_plus should be present in `Cargo.toml`");
+        let root = match hydroflow_crate {
+            proc_macro_crate::FoundCrate::Itself => quote! { hydroflow_plus },
+            proc_macro_crate::FoundCrate::Name(name) => {
+                let ident = syn::Ident::new(&name, proc_macro2::Span::call_site());
+                quote! { #ident }
+            }
+        };
+
+        let mut conditioned_tokens = None;
+        for (subgraph_id, flat_graph) in self.hydroflow_ir {
+            let partitioned_graph =
+                partition_graph(flat_graph).expect("Failed to partition (cycle detected).");
+
+            let mut diagnostics = Vec::new();
+            let tokens = partitioned_graph.as_code(&root, true, quote::quote!(), &mut diagnostics);
+            let my_extra_stmts = self
+                .extra_stmts
+                .get(&subgraph_id)
+                .cloned()
+                .unwrap_or_default();
+
+            if let Some(conditioned_tokens) = conditioned_tokens.as_mut() {
+                *conditioned_tokens = syn::parse_quote! {
+                    #conditioned_tokens else if __given_id == #subgraph_id {
+                        #(#my_extra_stmts)*
+                        #tokens
+                    }
+                };
+            } else {
+                conditioned_tokens = Some(syn::parse_quote! {
+                    if __given_id == #subgraph_id {
+                        #(#my_extra_stmts)*
+                        #tokens
+                    }
+                });
+            }
+        }
+
+        let conditioned_tokens: TokenStream = conditioned_tokens.unwrap();
+        let id = id.splice_untyped();
+        HfBuiltWithId {
+            tokens: syn::parse_quote!({
+                let __given_id = #id;
+                #conditioned_tokens else {
+                    panic!("Invalid node id: {}", __given_id);
+                }
+            }),
+            _phantom: PhantomData,
+        }
+    }
+}
+
+impl<'a> Quoted<'a, Hydroflow<'a>> for HfCompiled<'a, ()> {}
+
+impl<'a> FreeVariable<Hydroflow<'a>> for HfCompiled<'a, ()> {
+    fn to_tokens(mut self) -> (Option<TokenStream>, Option<TokenStream>) {
+        let hydroflow_crate = proc_macro_crate::crate_name("hydroflow_plus")
+            .expect("hydroflow_plus should be present in `Cargo.toml`");
+        let root = match hydroflow_crate {
+            proc_macro_crate::FoundCrate::Itself => quote! { hydroflow_plus },
+            proc_macro_crate::FoundCrate::Name(name) => {
+                let ident = syn::Ident::new(&name, proc_macro2::Span::call_site());
+                quote! { #ident }
+            }
+        };
+
+        if self.hydroflow_ir.len() != 1 {
+            panic!("Expected exactly one subgraph in the Hydroflow IR");
+        }
+
+        let flat_graph = self.hydroflow_ir.remove(&0).unwrap();
+        let partitioned_graph =
+            partition_graph(flat_graph).expect("Failed to partition (cycle detected).");
+
+        let mut diagnostics = Vec::new();
+        let tokens = partitioned_graph.as_code(&root, true, quote::quote!(), &mut diagnostics);
+
+        (None, Some(tokens))
+    }
+}
+
+pub struct HfBuiltWithId<'a> {
+    tokens: TokenStream,
+    _phantom: PhantomData<&'a mut &'a ()>,
+}
+
+impl<'a> Quoted<'a, Hydroflow<'a>> for HfBuiltWithId<'a> {}
+
+impl<'a> FreeVariable<Hydroflow<'a>> for HfBuiltWithId<'a> {
+    fn to_tokens(self) -> (Option<TokenStream>, Option<TokenStream>) {
+        (None, Some(self.tokens))
+    }
+}
--- hydroflow_plus/src/lib.rs
+++ hydroflow_plus/src/lib.rs
@@ -2,21 +2,15 @@
 
 stageleft::stageleft_no_entry_crate!();
 
-use std::collections::BTreeMap;
-use std::marker::PhantomData;
-
-use hydroflow::scheduled::context::Context;
 pub use hydroflow::scheduled::graph::Hydroflow;
 pub use hydroflow::*;
-use lang::graph::{partition_graph, HydroflowGraph};
-use proc_macro2::TokenStream;
-use quote::quote;
-use stageleft::runtime_support::FreeVariable;
-use stageleft::Quoted;
 
 pub mod runtime_support {
     pub use bincode;
 }
+
+pub mod runtime_context;
+pub use runtime_context::RuntimeContext;
 
 pub mod stream;
 pub use stream::{Bounded, Stream, Unbounded};
@@ -46,147 +40,6 @@ pub mod profiler;
 pub mod properties;
 
 mod staging_util;
-
-#[derive(Clone)]
-pub struct RuntimeContext<'a> {
-    _phantom: PhantomData<&'a mut &'a ()>,
-}
-
-impl RuntimeContext<'_> {
-    pub fn new() -> Self {
-        Self {
-            _phantom: PhantomData,
-        }
-    }
-}
-
-impl Copy for RuntimeContext<'_> {}
-
-impl Default for RuntimeContext<'_> {
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
-impl<'a> FreeVariable<&'a Context> for RuntimeContext<'a> {
-    fn to_tokens(self) -> (Option<TokenStream>, Option<TokenStream>) {
-        (None, Some(quote!(&context)))
-    }
-}
-
-pub struct HfCompiled<'a, ID> {
-    hydroflow_ir: BTreeMap<usize, HydroflowGraph>,
-    extra_stmts: BTreeMap<usize, Vec<syn::Stmt>>,
-    _phantom: PhantomData<&'a mut &'a ID>,
-}
-
-impl<ID> HfCompiled<'_, ID> {
-    pub fn hydroflow_ir(&self) -> &BTreeMap<usize, HydroflowGraph> {
-        &self.hydroflow_ir
-    }
-
-    pub fn take_ir(self) -> BTreeMap<usize, HydroflowGraph> {
-        self.hydroflow_ir
-    }
-}
-
-impl<'a> HfCompiled<'a, usize> {
-    pub fn with_dynamic_id(self, id: impl Quoted<'a, usize>) -> HfBuiltWithId<'a> {
-        let hydroflow_crate = proc_macro_crate::crate_name("hydroflow_plus")
-            .expect("hydroflow_plus should be present in `Cargo.toml`");
-        let root = match hydroflow_crate {
-            proc_macro_crate::FoundCrate::Itself => quote! { hydroflow_plus },
-            proc_macro_crate::FoundCrate::Name(name) => {
-                let ident = syn::Ident::new(&name, proc_macro2::Span::call_site());
-                quote! { #ident }
-            }
-        };
-
-        let mut conditioned_tokens = None;
-        for (subgraph_id, flat_graph) in self.hydroflow_ir {
-            let partitioned_graph =
-                partition_graph(flat_graph).expect("Failed to partition (cycle detected).");
-
-            let mut diagnostics = Vec::new();
-            let tokens = partitioned_graph.as_code(&root, true, quote::quote!(), &mut diagnostics);
-            let my_extra_stmts = self
-                .extra_stmts
-                .get(&subgraph_id)
-                .cloned()
-                .unwrap_or_default();
-
-            if let Some(conditioned_tokens) = conditioned_tokens.as_mut() {
-                *conditioned_tokens = syn::parse_quote! {
-                    #conditioned_tokens else if __given_id == #subgraph_id {
-                        #(#my_extra_stmts)*
-                        #tokens
-                    }
-                };
-            } else {
-                conditioned_tokens = Some(syn::parse_quote! {
-                    if __given_id == #subgraph_id {
-                        #(#my_extra_stmts)*
-                        #tokens
-                    }
-                });
-            }
-        }
-
-        let conditioned_tokens: TokenStream = conditioned_tokens.unwrap();
-        let id = id.splice_untyped();
-        HfBuiltWithId {
-            tokens: syn::parse_quote!({
-                let __given_id = #id;
-                #conditioned_tokens else {
-                    panic!("Invalid node id: {}", __given_id);
-                }
-            }),
-            _phantom: PhantomData,
-        }
-    }
-}
-
-impl<'a> Quoted<'a, Hydroflow<'a>> for HfCompiled<'a, ()> {}
-
-impl<'a> FreeVariable<Hydroflow<'a>> for HfCompiled<'a, ()> {
-    fn to_tokens(mut self) -> (Option<TokenStream>, Option<TokenStream>) {
-        let hydroflow_crate = proc_macro_crate::crate_name("hydroflow_plus")
-            .expect("hydroflow_plus should be present in `Cargo.toml`");
-        let root = match hydroflow_crate {
-            proc_macro_crate::FoundCrate::Itself => quote! { hydroflow_plus },
-            proc_macro_crate::FoundCrate::Name(name) => {
-                let ident = syn::Ident::new(&name, proc_macro2::Span::call_site());
-                quote! { #ident }
-            }
-        };
-
-        if self.hydroflow_ir.len() != 1 {
-            panic!("Expected exactly one subgraph in the Hydroflow IR");
-        }
-
-        let flat_graph = self.hydroflow_ir.remove(&0).unwrap();
-        let partitioned_graph =
-            partition_graph(flat_graph).expect("Failed to partition (cycle detected).");
-
-        let mut diagnostics = Vec::new();
-        let tokens = partitioned_graph.as_code(&root, true, quote::quote!(), &mut diagnostics);
-
-        (None, Some(tokens))
-    }
-}
-
-pub struct HfBuiltWithId<'a> {
-    tokens: TokenStream,
-    _phantom: PhantomData<&'a mut &'a ()>,
-}
-
-impl<'a> Quoted<'a, Hydroflow<'a>> for HfBuiltWithId<'a> {}
-
-impl<'a> FreeVariable<Hydroflow<'a>> for HfBuiltWithId<'a> {
-    fn to_tokens(self) -> (Option<TokenStream>, Option<TokenStream>) {
-        (None, Some(self.tokens))
-    }
-}
 
 #[stageleft::runtime]
 #[cfg(test)]
