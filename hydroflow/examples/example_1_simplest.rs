5c53e12d6845f60c95248164eea3e2c48a8ff270
Mingwei Samuel <mingwei.samuel@gmail.com>
2024-11-08 13:20:24 -0800

    docs: `getLines` support named section markers, fix #1320 (#1555)
    Fix #1320 suggested in
    https://github.com/hydro-project/hydroflow/pull/1318#pullrequestreview-2137476550

--- hydroflow/examples/example_1_simplest.rs
+++ hydroflow/examples/example_1_simplest.rs
@@ -1,9 +1,15 @@
+//[use]//
 use hydroflow::hydroflow_syntax;
+//[/use]//
 
+//[macro_call]//
 pub fn main() {
     let mut flow = hydroflow_syntax! {
         source_iter(0..10) -> for_each(|n| println!("Hello {}", n));
     };
+    //[/macro_call]//
 
+    //[run]//
     flow.run_available();
+    //[/run]//
 }
