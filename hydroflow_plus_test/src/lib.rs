849d46bce60a26c8608b41f66cdb9a2312363e34
Shadaj Laddad <shadaj@users.noreply.github.com>
2024-12-03 16:35:53 -0800

    docs(hydroflow_plus): add initial docs on consistency and safety

--- hydroflow_plus_test/src/lib.rs
+++ hydroflow_plus_test/src/lib.rs
@@ -2,3 +2,10 @@ stageleft::stageleft_no_entry_crate!();
 
 pub mod cluster;
 pub mod distributed;
+
+#[doc(hidden)]
+#[stageleft::runtime]
+mod docs {
+    #[doc = include_str!("../../docs/docs/hydroflow_plus/consistency.md")]
+    mod consistency {}
+}
