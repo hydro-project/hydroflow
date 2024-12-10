1a3c6908bcb2a430f4c8c1c4ed7a4b0a7612fba9
Mingwei Samuel <mingwei.samuel@gmail.com>
2024-12-10 15:48:16 -0800

    nightly on hydroflow_plus

--- hydroflow_plus/build.rs
+++ hydroflow_plus/build.rs
@@ -1,3 +1,13 @@
+use rustc_version::{version_meta, Channel};
+
 fn main() {
+    println!("cargo::rustc-check-cfg=cfg(nightly)");
+    if matches!(
+        version_meta().map(|meta| meta.channel),
+        Ok(Channel::Nightly)
+    ) {
+        println!("cargo:rustc-cfg=nightly");
+    }
+
     stageleft_tool::gen_final!();
 }
